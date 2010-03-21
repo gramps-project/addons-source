890b7b3c7122c25414a4adc0d55cac2f20a4ebda
lcc-net <lcc-net@1b16344b-c707-4684-8e57-f14b83debe0b>
2010-03-21 20:22:18 +0000

    Lines of Descendancy Report

---
+++ contrib/LinesOfDescendency/lines-of-descendency.gpr.py
@@ -0,0 +1,16 @@
+register(REPORT,
+        id = 'LinesOfDescendency',
+        name = _('Lines of Descendency'),
+        description = _('Prints out all descendency lines '
+            'from a given ancestor to a given descendent in text.'),
+        version = '1.1.0',
+        gramps_target_version = '3.3',
+        status = STABLE,
+        fname = 'lines-of-descendency.py',
+        authors = ['lcc'],
+        authors_email = ['lcc.mailaddress@gmail.com'],
+        category = CATEGORY_TEXT,
+        reportclass = 'LinesOfDescendency',
+        optionclass = 'LODOptions',
+        report_modes = [REPORT_MODE_GUI, REPORT_MODE_CLI]
+        )
