ac85cac9da26ecf397db756d38988d479db47d2a
lcc-net <lcc-net@1b16344b-c707-4684-8e57-f14b83debe0b>
2010-03-26 20:33:35 +0000

    FamilyTree addon

---
+++ contrib/FamilyTree/FamilyTree.gpr.py
@@ -0,0 +1,15 @@
+register(REPORT,
+    id = 'FamilyTree',
+    name = _('Family Tree'),
+    description = _('Produces a graphical family tree.'),
+    version = '3.1.3',
+    gramps_target_version = '3.3',
+    status = STABLE,
+    fname = 'FamilyTree.py',
+    reportclass = 'FamilyTree',
+    optionclass = 'FamilyTreeOptions',
+    authors = ['Reinhard Mueller'],
+    authors_email = ['reinhard.mueller@bytewise.at'],
+    category = CATEGORY_DRAW,
+    report_modes = [REPORT_MODE_GUI, REPORT_MODE_BKI, REPORT_MODE_CLI]
+    )
