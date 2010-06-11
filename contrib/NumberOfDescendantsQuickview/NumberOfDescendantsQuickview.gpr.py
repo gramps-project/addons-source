51b5a9b5b392509270d310000073d983e844faf4
reinhardmueller <reinhardmueller@1b16344b-c707-4684-8e57-f14b83debe0b>
2010-06-11 22:20:18 +0000

    Added NumberOfDescendantsQuickview (formerly known as descendant_count quickview and now renamed to avoid confusion with the other quickview with the same name)

---
+++ contrib/NumberOfDescendantsQuickview/NumberOfDescendantsQuickview.gpr.py
@@ -0,0 +1,12 @@
+register(QUICKREPORT,
+         id    = 'NumberOfDescendantsQuickview',
+         name  = _("Number of descendants"),
+         description= _("Shows the number of descendants of the current person"),
+         version = '3.3',
+         gramps_target_version = '3.3',
+         status = STABLE,
+         fname = 'NumberOfDescendantsQuickview.py',
+         authors = ["Reinhard Mueller"],
+         authors_email = ["reinhard.mueller@igal.at"],
+         category = CATEGORY_QR_PERSON,
+         runfunc = 'run')
