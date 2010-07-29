b02d4302a27bf12e34aaea36a895d73351d00bfc
dsblank <dsblank@1b16344b-c707-4684-8e57-f14b83debe0b>
2010-07-29 08:02:56 +0000

    Refinements: sort cols, delete rows, double-click to edit; cleanup

--- trunk/contrib/ClipboardGramplet/ClipboardGramplet.py
+++ trunk/contrib/ClipboardGramplet/ClipboardGramplet.py
@@ -18,22 +18,195 @@
 # Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
 #
 
-# $Id: QueryGramplet.py 195 2010-02-13 22:50:57Z dsblank $
+# $Id: $
 
+#-------------------------------------------------------------------------
+#
+# Python modules
+#
+#-------------------------------------------------------------------------
+import gtk
+import cPickle as pickle
+
+#-------------------------------------------------------------------------
+#
+# Gramps modules
+#
+#-------------------------------------------------------------------------
 from TransUtils import get_addon_translator
 _ = get_addon_translator(__file__).ugettext
 from gen.plug import Gramplet
 from DdTargets import DdTargets
-import gtk
-from ScratchPad import * #ScratchPadListView, ScratchPadListModel
-import cPickle as pickle
+from ScratchPad import * 
+import Errors
 
+#-------------------------------------------------------------------------
+#
+# Local Functions
+#
+#-------------------------------------------------------------------------
 def update_rows(model, path, iter):
     """
     Update the rows of a model.
     """
     model.row_changed(path, iter)
 
+def escape(data):
+    """
+    Remove newlines from text.
+    """
+    data = data.replace(chr(10), "\\n")
+    return data
+
+def unescape(data):
+    """
+    Replace newlines with \n text.
+    """
+    return data.replace("\\n", chr(10))
+
+#-------------------------------------------------------------------------
+#
+# MultiTreeView class
+#
+#-------------------------------------------------------------------------
+class MultiTreeView(gtk.TreeView):
+    '''
+    TreeView that captures mouse events to make drag and drop work properly
+    '''
+    def __init__(self, dbstate, uistate):
+        self.dbstate = dbstate
+        self.uistate = uistate
+        super(MultiTreeView, self).__init__()
+        self.connect('button_press_event', self.on_button_press)
+        self.connect('button_release_event', self.on_button_release)
+        self.connect('key_press_event', self.key_press_event)
+        self.defer_select = False
+
+    def key_press_event(self, widget, event):
+        if event.type == gtk.gdk.KEY_PRESS:
+            if event.keyval == gtk.keysyms.Delete:
+                model, paths = self.get_selection().get_selected_rows()
+                for path in paths:
+                    try:
+                        node = model.get_iter(path)
+                    except:
+                        node = None
+                    if node:
+                        model.remove(node)
+
+    def on_button_press(self, widget, event):
+        from gui.editors import (EditPerson, EditEvent, EditFamily, EditSource,
+                                 EditPlace, EditRepository, EditNote, EditMedia)
+        # Here we intercept mouse clicks on selected items so that we can
+        # drag multiple items without the click selecting only one
+        target = self.get_path_at_pos(int(event.x), int(event.y))
+        if event.type == gtk.gdk._2BUTTON_PRESS and event.button == 1:
+            model, paths = self.get_selection().get_selected_rows()
+            for path in paths:
+                node = model.get_iter(path)
+                if node is not None:
+                    o = model.get_value(node,1)
+                    objclass = o._objclass
+                    handle = o._handle
+                    if objclass == 'Person':
+                        person = self.dbstate.db.get_person_from_handle(handle)
+                        if person:
+                            try:
+                                EditPerson(self.dbstate, 
+                                           self.uistate, [], person)
+                                return True # handled event
+                            except Errors.WindowActiveError:
+                                pass
+                    elif objclass == 'Event':
+                        event = self.dbstate.db.get_event_from_handle(handle)
+                        if event:
+                            try:
+                                EditEvent(self.dbstate, 
+                                          self.uistate, [], event)
+                                return True # handled event
+                            except Errors.WindowActiveError:
+                                pass
+                    elif objclass == 'Family':
+                        ref = self.dbstate.db.get_family_from_handle(handle)
+                        if ref:
+                            try:
+                                EditFamily(self.dbstate, 
+                                           self.uistate, [], ref)
+                                return True # handled event
+                            except Errors.WindowActiveError:
+                                pass
+                    elif objclass == 'Source':
+                        ref = self.dbstate.db.get_source_from_handle(handle)
+                        if ref:
+                            try:
+                                EditSource(self.dbstate, 
+                                           self.uistate, [], ref)
+                                return True # handled event
+                            except Errors.WindowActiveError:
+                                pass
+                    elif objclass == 'Place':
+                        ref = self.dbstate.db.get_place_from_handle(handle)
+                        if ref:
+                            try:
+                                EditPlace(self.dbstate, 
+                                           self.uistate, [], ref)
+                                return True # handled event
+                            except Errors.WindowActiveError:
+                                pass
+                    elif objclass == 'Repository':
+                        ref = self.dbstate.db.get_repository_from_handle(handle)
+                        if ref:
+                            try:
+                                EditRepository(self.dbstate, 
+                                           self.uistate, [], ref)
+                                return True # handled event
+                            except Errors.WindowActiveError:
+                                pass
+                    elif objclass == 'Note':
+                        ref = self.dbstate.db.get_note_from_handle(handle)
+                        if ref:
+                            try:
+                                EditNote(self.dbstate, 
+                                         self.uistate, [], ref)
+                                return True # handled event
+                            except Errors.WindowActiveError:
+                                pass
+                    elif objclass in ['Media', 'MediaObject']:
+                        ref = self.dbstate.db.get_object_from_handle(handle)
+                        if ref:
+                            try:
+                                EditMedia(self.dbstate, 
+                                          self.uistate, [], ref)
+                                return True # handled event
+                            except Errors.WindowActiveError:
+                                pass
+            return False
+        # otherwise:
+        if (target 
+            and event.type == gtk.gdk.BUTTON_PRESS
+            and not (event.state & (gtk.gdk.CONTROL_MASK|gtk.gdk.SHIFT_MASK))
+            and self.get_selection().path_is_selected(target[0])):
+            # disable selection
+            self.get_selection().set_select_function(lambda *ignore: False)
+            self.defer_select = target[0]
+			
+    def on_button_release(self, widget, event):
+        # re-enable selection
+        self.get_selection().set_select_function(lambda *ignore: True)
+        
+        target = self.get_path_at_pos(int(event.x), int(event.y))	
+        if (self.defer_select and target 
+            and self.defer_select == target[0]
+            and not (event.x==0 and event.y==0)): # certain drag and drop
+            self.set_cursor(target[0], target[1], False)
+            
+        self.defer_select=False
+
+#-------------------------------------------------------------------------
+#
+# ClipboardListView class
+#
+#-------------------------------------------------------------------------
 class ClipboardListView(ScratchPadListView):
     """
     Subclass ScratchPadListView to override remove_invalid_objects.
@@ -46,14 +219,22 @@ class ClipboardListView(ScratchPadListView):
         if model:
             for o in model:
                 o[1] = o[1].__class__(self.dbstate, o[1]._pickle)
+                o[3] = o[1]._type
+                o[4] = o[1]._value
             model.foreach(update_rows)
 
+#-------------------------------------------------------------------------
+#
+# ClipboardGramplet class
+#
+#-------------------------------------------------------------------------
 class ClipboardGramplet(Gramplet):
     """
     A clipboard-like gramplet.
     """
     def init(self):
-        self.object_list = ClipboardListView(self.dbstate, gtk.TreeView())
+        self.object_list = ClipboardListView(self.dbstate, 
+                                             MultiTreeView(self.dbstate, self.uistate))
         self.otree = ScratchPadListModel()
         self.object_list.set_model(self.otree)
         self.gui.get_container_widget().remove(self.gui.textview)
@@ -63,14 +244,16 @@ class ClipboardGramplet(Gramplet):
     def on_load(self):
         i = 0
         while i < len(self.gui.data):
-            data = self.gui.data[i]
+            data = unescape(self.gui.data[i])
             i += 1
-            title = self.gui.data[i]
+            title = unescape(self.gui.data[i])
             i += 1
-            value = self.gui.data[i]
+            value = unescape(self.gui.data[i])
             i += 1
-            data = data.replace("\\n", chr(10))
-            tuple_data = pickle.loads(data)
+            try:
+                tuple_data = pickle.loads(data)
+            except:
+                tuple_data = ("TEXT", data)
             drag_type = tuple_data[0]
             model = self.object_list._widget.get_model()
             class Selection(object):
@@ -95,7 +278,6 @@ class ClipboardGramplet(Gramplet):
                     Selection(data), # pickled data
                     None,            # info (not used)
                     -1, title=title, value=value)              # time
-            #model.append([drag_type, data, lambda: drag_type])
 
     def on_save(self):
         self.gui.data = [] # clear out old data: data, title, value
@@ -111,7 +293,7 @@ class ClipboardGramplet(Gramplet):
                 else:
                     # pickled: type, timestamp, handle, value
                     data = o[1].pack()
-                self.gui.data.append(data.replace(chr(10), "\\n"))
-                self.gui.data.append(o[1]._title)
-                self.gui.data.append(o[1]._value)
+                self.gui.data.append(escape(data))
+                self.gui.data.append(escape(o[1]._title))
+                self.gui.data.append(escape(o[1]._value))
 
