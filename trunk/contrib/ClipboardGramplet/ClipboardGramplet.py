65cd7aaf6074c229f975033003b688edc6264716
dsblank <dsblank@1b16344b-c707-4684-8e57-f14b83debe0b>
2010-07-30 05:14:37 +0000

    Make a filter from selected objects

--- trunk/contrib/ClipboardGramplet/ClipboardGramplet.py
+++ trunk/contrib/ClipboardGramplet/ClipboardGramplet.py
@@ -26,6 +26,7 @@
 #
 #-------------------------------------------------------------------------
 import gtk
+import time
 import cPickle as pickle
 
 #-------------------------------------------------------------------------
@@ -95,14 +96,59 @@ class MultiTreeView(gtk.TreeView):
                         node = None
                     if node:
                         model.remove(node)
+                return True
 
     def on_button_press(self, widget, event):
-        from gui.editors import (EditPerson, EditEvent, EditFamily, EditSource,
-                                 EditPlace, EditRepository, EditNote, EditMedia)
         # Here we intercept mouse clicks on selected items so that we can
         # drag multiple items without the click selecting only one
         target = self.get_path_at_pos(int(event.x), int(event.y))
-        if event.type == gtk.gdk._2BUTTON_PRESS and event.button == 1:
+        if event.button == 3: # right mouse
+            selection = widget.get_selection()
+            store, paths = selection.get_selected_rows()
+            tpath = paths[0] if len(paths) > 0 else None
+            node = store.get_iter(tpath) if tpath else None
+            o = None
+            if node:
+                o = store.get_value(node, 1)
+            popup = gtk.Menu()
+            # ---------------------------
+            if o:
+                objclass, handle = o._objclass, o._handle
+            else:
+                objclass, handle = None, None
+            if objclass in ['Person', 'Event', 'Media', 'Source',
+                            'Repository', 'Family', 'Note', 'Place']:
+                menu_item = gtk.MenuItem(_("See %s details") % objclass)
+                menu_item.connect("activate", 
+                   lambda widget: self.edit_obj(objclass, handle))
+                popup.append(menu_item)
+                menu_item.show()
+                # ---------------------------
+                menu_item = gtk.MenuItem(_("Make Active %s") % objclass)
+                menu_item.connect("activate", 
+                      lambda widget: self.uistate.set_active(handle, objclass))
+                popup.append(menu_item)
+                menu_item.show()
+                # ---------------------------
+                gids = set()
+                for path in paths:
+                    node = store.get_iter(path)
+                    if node:
+                        o = store.get_value(node, 1)
+                        if o._objclass == objclass:
+                            my_handle = o._handle
+                            obj = self.dbstate.db.get_table_metadata(objclass)["handle_func"](my_handle)
+                            if obj:
+                                gids.add(obj.gramps_id)
+                menu_item = gtk.MenuItem(_("Create Filter from selected %s...") % objclass)
+                menu_item.connect("activate", 
+                      lambda widget: self.make_filter(objclass, gids))
+                popup.append(menu_item)
+                menu_item.show()
+            # Show the popup menu:
+            popup.popup(None, None, None, 3, event.time)
+            return True        
+        elif event.type == gtk.gdk._2BUTTON_PRESS and event.button == 1:
             model, paths = self.get_selection().get_selected_rows()
             for path in paths:
                 node = model.get_iter(path)
@@ -110,70 +156,7 @@ class MultiTreeView(gtk.TreeView):
                     o = model.get_value(node,1)
                     objclass = o._objclass
                     handle = o._handle
-                    if objclass == 'Person':
-                        person = self.dbstate.db.get_person_from_handle(handle)
-                        if person:
-                            try:
-                                EditPerson(self.dbstate, 
-                                           self.uistate, [], person)
-                            except Errors.WindowActiveError:
-                                pass
-                    elif objclass == 'Event':
-                        event = self.dbstate.db.get_event_from_handle(handle)
-                        if event:
-                            try:
-                                EditEvent(self.dbstate, 
-                                          self.uistate, [], event)
-                            except Errors.WindowActiveError:
-                                pass
-                    elif objclass == 'Family':
-                        ref = self.dbstate.db.get_family_from_handle(handle)
-                        if ref:
-                            try:
-                                EditFamily(self.dbstate, 
-                                           self.uistate, [], ref)
-                            except Errors.WindowActiveError:
-                                pass
-                    elif objclass == 'Source':
-                        ref = self.dbstate.db.get_source_from_handle(handle)
-                        if ref:
-                            try:
-                                EditSource(self.dbstate, 
-                                           self.uistate, [], ref)
-                            except Errors.WindowActiveError:
-                                pass
-                    elif objclass == 'Place':
-                        ref = self.dbstate.db.get_place_from_handle(handle)
-                        if ref:
-                            try:
-                                EditPlace(self.dbstate, 
-                                           self.uistate, [], ref)
-                            except Errors.WindowActiveError:
-                                pass
-                    elif objclass == 'Repository':
-                        ref = self.dbstate.db.get_repository_from_handle(handle)
-                        if ref:
-                            try:
-                                EditRepository(self.dbstate, 
-                                           self.uistate, [], ref)
-                            except Errors.WindowActiveError:
-                                pass
-                    elif objclass == 'Note':
-                        ref = self.dbstate.db.get_note_from_handle(handle)
-                        if ref:
-                            try:
-                                EditNote(self.dbstate, 
-                                         self.uistate, [], ref)
-                            except Errors.WindowActiveError:
-                                pass
-                    elif objclass in ['Media', 'MediaObject']:
-                        ref = self.dbstate.db.get_object_from_handle(handle)
-                        if ref:
-                            try:
-                                EditMedia(self.dbstate, 
-                                          self.uistate, [], ref)
-                            except Errors.WindowActiveError:
-                                pass
+                    self.edit_obj(objclass, handle)
             return True
         # otherwise:
         if (target 
@@ -195,6 +178,104 @@ class MultiTreeView(gtk.TreeView):
             self.set_cursor(target[0], target[1], False)
             
         self.defer_select=False
+
+    def edit_obj(self, objclass, handle):
+        from gui.editors import (EditPerson, EditEvent, EditFamily, EditSource,
+                                 EditPlace, EditRepository, EditNote, EditMedia)
+        if objclass == 'Person':
+            person = self.dbstate.db.get_person_from_handle(handle)
+            if person:
+                try:
+                    EditPerson(self.dbstate, 
+                               self.uistate, [], person)
+                except Errors.WindowActiveError:
+                    pass
+        elif objclass == 'Event':
+            event = self.dbstate.db.get_event_from_handle(handle)
+            if event:
+                try:
+                    EditEvent(self.dbstate, 
+                              self.uistate, [], event)
+                except Errors.WindowActiveError:
+                    pass
+        elif objclass == 'Family':
+            ref = self.dbstate.db.get_family_from_handle(handle)
+            if ref:
+                try:
+                    EditFamily(self.dbstate, 
+                               self.uistate, [], ref)
+                except Errors.WindowActiveError:
+                    pass
+        elif objclass == 'Source':
+            ref = self.dbstate.db.get_source_from_handle(handle)
+            if ref:
+                try:
+                    EditSource(self.dbstate, 
+                               self.uistate, [], ref)
+                except Errors.WindowActiveError:
+                    pass
+        elif objclass == 'Place':
+            ref = self.dbstate.db.get_place_from_handle(handle)
+            if ref:
+                try:
+                    EditPlace(self.dbstate, 
+                               self.uistate, [], ref)
+                except Errors.WindowActiveError:
+                    pass
+        elif objclass == 'Repository':
+            ref = self.dbstate.db.get_repository_from_handle(handle)
+            if ref:
+                try:
+                    EditRepository(self.dbstate, 
+                               self.uistate, [], ref)
+                except Errors.WindowActiveError:
+                    pass
+        elif objclass == 'Note':
+            ref = self.dbstate.db.get_note_from_handle(handle)
+            if ref:
+                try:
+                    EditNote(self.dbstate, 
+                             self.uistate, [], ref)
+                except Errors.WindowActiveError:
+                    pass
+        elif objclass in ['Media', 'MediaObject']:
+            ref = self.dbstate.db.get_object_from_handle(handle)
+            if ref:
+                try:
+                    EditMedia(self.dbstate, 
+                              self.uistate, [], ref)
+                except Errors.WindowActiveError:
+                    pass
+
+    def make_filter(self, objclass, gramps_ids):
+        import Filters 
+        from gui.filtereditor import EditFilter
+        import const
+
+        FilterClass = Filters.GenericFilterFactory(objclass)
+        rule = getattr(getattr(Filters.Rules, objclass),'RegExpIdOf')
+        filter = FilterClass()
+        filter.set_name(_("Filter %s from Clipboard") % objclass)
+        struct_time = time.localtime()
+        filter.set_comment( _("Created on %4d/%02d/%02d") % 
+            (struct_time.tm_year, struct_time.tm_mon, struct_time.tm_mday))
+        re = "|".join(["^%s$" % gid for gid in gramps_ids])
+        filter.add_rule(rule([re]))
+
+        filterdb = Filters.FilterList(const.CUSTOM_FILTERS)
+        filterdb.load()
+        EditFilter(objclass, self.dbstate, self.uistate, [],
+                   filter, filterdb,
+                   lambda : self.edit_filter_save(filterdb, objclass))
+
+    def edit_filter_save(self, filterdb, objclass):
+        """
+        If a filter changed, save them all. Reloads, and also calls callback.
+        """
+        from Filters import reload_custom_filters
+        filterdb.save()
+        reload_custom_filters()
+        self.uistate.emit('filters-changed', (objclass,))
 
 #-------------------------------------------------------------------------
 #
