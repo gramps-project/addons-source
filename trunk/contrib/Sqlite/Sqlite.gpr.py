f189cbf1537a2ae2eca917e6f63af20caf88b95a
dsblank <dsblank@1b16344b-c707-4684-8e57-f14b83debe0b>
2010-08-16 02:31:34 +0000

    Moved ImportSql to Sqlite package; fixed an error with unicode

---
+++ trunk/contrib/Sqlite/Sqlite.gpr.py
@@ -0,0 +1,23 @@
+register(IMPORT,
+         id    = 'im_sqlite',
+         name  = _('SQLite Import'),
+         description =  _('SQLite is a common local database format'),
+         version = '1.0.1',
+         gramps_target_version = "3.3",
+         status = STABLE,
+         fname = 'ImportSql.py',
+         import_function = 'importData',
+         extension = "sql"
+)
+
+register(EXPORT,
+         id    = 'ex_sqlite',
+         name  = _('SQLite Export'),
+         description =  _('SQLite is a common local database format'),
+         version = '1.0.0',
+         gramps_target_version = "3.3",
+         status = STABLE,
+         fname = 'ExportSql.py',
+         export_function = 'exportData',
+         extension = "sql"
+)
