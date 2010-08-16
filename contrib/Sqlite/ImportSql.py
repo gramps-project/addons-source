b51983f8f3dc2cf8e6f761a9fa57ad1ec8b73ae6
Doug Blank <doug.blank@gmail.com>
2010-08-16 02:31:34 +0000

    Moved ImportSql to Sqlite package; fixed an error with unicode

--- contrib/Sqlite/ImportSql.py
+++ contrib/Sqlite/ImportSql.py
@@ -17,7 +17,7 @@
 # along with this program; if not, write to the Free Software
 # Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
 #
-# $Id$
+# $Id: ImportSql.py 189 2010-02-13 07:08:48Z dsblank $
 
 "Import from SQLite Database"
 
