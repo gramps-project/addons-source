71a6a0b81166e1052b843d61de817900477fd3cc
Doug Blank <doug.blank@gmail.com>
2010-08-25 13:50:19 +0000

    Split Web connections up; added plugin.depends_on='libwebconnect'

---
+++ contrib/UKWebConnectPack/UKWebPack.gpr.py
@@ -0,0 +1,18 @@
+#------------------------------------------------------------------------
+#
+# Register the Addon
+#
+#------------------------------------------------------------------------
+
+register(GENERAL,
+         category="WebConnect",
+         id="UK Web Connect Pack",
+         name=_("UK Web Connect Pack"),
+         description = _("Collection of Web sites for the UK (requires libwebconnect)"),
+         status = STABLE,
+         version = '1.0.3',
+         gramps_target_version = "3.3",
+         fname="UKWebPack.py",
+         load_on_reg = True,
+         depends_on = ["libwebconnect"]
+         )
