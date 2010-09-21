23781a8e7e2aa5e2b096079cb887209afa16650d
ldnp <ldnp@1b16344b-c707-4684-8e57-f14b83debe0b>
2010-09-21 14:55:23 +0000

    More info with balloon.

--- trunk/contrib/GoogleEarthWriteKML/GoogleEarthWriteKML.gpr.py
+++ trunk/contrib/GoogleEarthWriteKML/GoogleEarthWriteKML.gpr.py
@@ -28,7 +28,7 @@
 register(MAPSERVICE,
     id = 'GoogleEarth',
     name = _('GoogleEarth'),
-    version = '1.0.3',
+    version = '1.0.5',
     gramps_target_version="3.3",
     status = STABLE,
     fname = 'GoogleEarthWriteKML.py',
