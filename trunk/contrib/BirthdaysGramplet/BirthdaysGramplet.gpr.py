0ccba0c2c952e37121c1ee47d11f44e38ec4f311
peter17 <peter17@1b16344b-c707-4684-8e57-f14b83debe0b>
2010-11-30 11:42:34 +0000

    Updating BirthdaysGramplet

--- trunk/contrib/BirthdaysGramplet/BirthdaysGramplet.gpr.py
+++ trunk/contrib/BirthdaysGramplet/BirthdaysGramplet.gpr.py
@@ -1,13 +1,13 @@
 # File: Birthdays.gpr.py
 register(GRAMPLET,
-         id='Birthdays', 
-         name=_("Birthdays Gramplet"),
-         description = _("a gramplet that displays the birthdays of the n following days"),
-         status = STABLE,
-         version = '1.0.2',
-         fname="BirthdaysGramplet.py",
-         height = 200, 
-         gramplet = 'BirthdaysGramplet',
-         gramps_target_version="3.3",
-         gramplet_title=_("Birthdays Gramplet") 
-         )
+	id='Birthdays',
+	name=_("Birthdays Gramplet"),
+	description = _("a gramplet that displays the birthdays of the living people"),
+	status = STABLE,
+	version = '1.0.4',
+	fname="BirthdaysGramplet.py",
+	height = 200,
+	gramplet = 'BirthdaysGramplet',
+	gramps_target_version="3.2",
+	gramplet_title=_("Birthdays Gramplet")
+	)
