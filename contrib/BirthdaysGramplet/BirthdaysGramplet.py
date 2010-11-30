dc17ee56d5fb757f7810205a878eb1aecf9e9067
peter17 <peter17@1b16344b-c707-4684-8e57-f14b83debe0b>
2010-11-30 11:42:34 +0000

    Updating BirthdaysGramplet

--- contrib/BirthdaysGramplet/BirthdaysGramplet.py
+++ contrib/BirthdaysGramplet/BirthdaysGramplet.py
@@ -25,83 +25,59 @@ import DateHandler
 import time
 import datetime
 import posixpath
+import config
 
 """
-This Gramplet displays the birthdays of the next n days (n = 0 to 365).
+This Gramplet displays the incoming birthdays.
 """
 
 class BirthdaysGramplet(Gramplet):
-    def init(self):
-	self.set_text(_("No Family Tree loaded."))
-	self.num_days = 7
-	self.max_age = 110
+	def init(self):
+		self.set_text(_("No Family Tree loaded."))
+		self.max_age = config.get('behavior.max-age-prob-alive')
 
-    def post_init(self):
-	self.disconnect("active-changed")
+	def post_init(self):
+		self.disconnect("active-changed")
 
-    def on_load(self):
-	if len(self.gui.data) == 2:
-	    self.num_days = int(self.gui.data[0])
-            self.max_age = int(self.gui.data[1])
+	def db_changed(self):
+		self.dbstate.db.connect('person-add', self.update)
+		self.dbstate.db.connect('person-delete', self.update)
+		self.dbstate.db.connect('person-update', self.update)
+		self.update
 
-    def db_changed(self):
-	self.dbstate.db.connect('person-add', self.update)
-	self.dbstate.db.connect('person-delete', self.update)
-	self.dbstate.db.connect('person-update', self.update)
-	self.update
-
-    def build_options(self):
-	from gen.plug.menu import NumberOption
-        # Consider removing this; why should the user think about it?
-	self.add_option(NumberOption(_("Number of days"), self.num_days, 0, 365))
-        # Consider using the system builtin maximum age:
-        # config.get('behavior.max-age-prob-alive')
-	self.add_option(NumberOption(_("Maximum age"), self.max_age, 0, 150))
-
-    def save_options(self):
-        self.num_days = int(self.get_option(_("Number of days")).get_value())
-        self.max_age = int(self.get_option(_("Maximum age")).get_value())
-
-    def save_update_options(self, widget=None):
-        self.num_days = int(self.get_option(_("Number of days")).get_value())
-        self.max_age = int(self.get_option(_("Maximum age")).get_value())
-        self.gui.data = [self.num_days, self.max_age]
-        self.update()
-
-    def main(self):
-	self.set_text(_("Processing..."))
-	database = self.dbstate.db
-	personList = database.iter_people()
-	result = []
-	text = ''
-        today = Today()
-	for cnt, person in enumerate(personList):
-	    birth_ref = person.get_birth_ref()
-	    death_ref = person.get_death_ref()
-	    if (birth_ref and not death_ref):
-		birth = database.get_event_from_handle(birth_ref.ref)
-		birth_date = birth.get_date_object()
-		if birth_date.is_regular():
-                    birthday_this_year = Date(today.get_year(), birth_date.get_month(), birth_date.get_day())
-                    age = birthday_this_year - birth_date
-                    # returns (years, months, days), all negative if after
-                    diff = today - birth_date
-                    # about number of days since today, not counting year:
-                    diff_days = abs(diff[1]) * 30 + abs(diff[2])
-                    if abs(diff[0]) < self.max_age:
-                        if diff[0] < 0: # missed for this year, next year:
-                            result.append((diff_days + 365, age, birth_date, person))
-                        elif diff_days == 0: # today
-                            result.append((diff_days + 365, age, birth_date, person))
-                        else:
-                            result.append((diff_days, age, birth_date, person))
-        # Reverse sort on number of days from now:
-	result.sort(key=lambda item: -item[0])
-	self.clear_text()
-	for diff, age, date, person in result:
-            name = person.get_primary_name()
-            self.append_text("%s: " % DateHandler.displayer.display(date))
-            self.link(name_displayer.display_name(name), 
-                      "Person", person.handle)
-            self.append_text(" (%s)\n" % age)
-        self.append_text("", scroll_to="begin")
+	def main(self):
+		self.set_text(_("Processing..."))
+		database = self.dbstate.db
+		personList = database.iter_people()
+		result = []
+		text = ''
+		today = Today()
+		for cnt, person in enumerate(personList):
+			birth_ref = person.get_birth_ref()
+			death_ref = person.get_death_ref()
+			if (birth_ref and not death_ref):
+				birth = database.get_event_from_handle(birth_ref.ref)
+				birth_date = birth.get_date_object()
+				if birth_date.is_regular():
+					birthday_this_year = Date(today.get_year(), birth_date.get_month(), birth_date.get_day())
+					age = birthday_this_year - birth_date
+					# returns (years, months, days), all negative if after
+					diff = today - birth_date
+					# about number of days since today, not counting year:
+					diff_days = abs(diff[1]) * 30 + abs(diff[2])
+					if abs(diff[0]) < self.max_age:
+						if diff[0] < 0: # missed for this year, next year:
+							result.append((diff_days + 365, age, birth_date, person))
+						elif diff_days == 0: # today
+							result.append((diff_days + 365, age, birth_date, person))
+						else:
+							result.append((diff_days, age, birth_date, person))
+		# Reverse sort on number of days from now:
+		result.sort(key=lambda item: -item[0])
+		self.clear_text()
+		for diff, age, date, person in result:
+			name = person.get_primary_name()
+			self.append_text("%s: " % DateHandler.displayer.display(date))
+			self.link(name_displayer.display_name(name), "Person", person.handle)
+			self.append_text(" (%s)\n" % age)
+		self.append_text("", scroll_to="begin")
