4e700e0f711c4ab11726028593cdf29ec5f6470e
romjerome <romjerome@1b16344b-c707-4684-8e57-f14b83debe0b>
2010-12-01 18:13:36 +0000

    first version of french web sites collection

---
+++ trunk/contrib/FRWebConnectPack/FRWebPack.gpr.py
@@ -0,0 +1,19 @@
+#------------------------------------------------------------------------
+#
+# Register the Addon
+#
+#------------------------------------------------------------------------
+
+register(GENERAL,
+         category="WebConnect",
+         id="FR Web Connect Pack",
+         name=_("FR Web Connect Pack"),
+         description = _("Collection of Web sites for the FR (requires libwebconnect)"),
+         status = UNSTABLE,
+         version = '0.9.9',
+         gramps_target_version = "3.3",
+         fname="FRWebPack.py",
+         load_on_reg = True,
+         depends_on = ["libwebconnect"]
+         )
+
