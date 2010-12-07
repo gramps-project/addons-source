97df36d94a1dfb5d557077a5a25897187a4f7161
romjerome <romjerome@1b16344b-c707-4684-8e57-f14b83debe0b>
2010-12-07 10:05:29 +0000

    update list

--- contrib/FRWebConnectPack/FRWebPack.py
+++ contrib/FRWebConnectPack/FRWebPack.py
@@ -1,3 +1,5 @@
+# -*- coding: UTF-8 -*-
+#
 # Gramps - a GTK+/GNOME based genealogy program
 #
 # Copyright (C) 2010  Doug Blank <doug.blank@gmail.com>
@@ -25,18 +27,28 @@ from libwebconnect import *
 
 # http://www.gramps-project.org/wiki/index.php?title=Resource_and_related_sites#French_resources
 #
-# http://www.bigenet.org/index.php
-# http://www.geneabank.org/frenind.php3
-# http://www.culture.fr/fr/sections/collections/genealogie
-# http://www.cdhf.net/fr/index.php?t=bases&d=bases/moteurpat&c=moteurpat&f=selection
-# https://beta.familysearch.org/
-# http://www.geneanet.org/
-#
-# Quebec (Canada)
-# http://www.fichierorigine.com/
+# http://www.bigenet.org/index.php ?
+
 WEBSITES = [
+    ["Person", u"Ministère de la culture", _("French cultural ministry"), "http://www.culture.fr/genealogie/?action_type=search&lang=fr&search_nom=%(surname)s&search_prenom=%(given)s"],
+    ["Person", "Geneabank", _("Genealogical bank"), "http://searchgbk.geneanet.org/result.php3?name=%(surname)s&place=%(place_title)s&start=&end=&source=gbk*"],
+    #["Person", u"Centre Départemental d'Histoire des Familles (CDHF)", _("Historical Families Center Departement (CDHF)"), "http://www.cdhf.net/fr/index.php?t=bases&d=bases%2Fmoteurpat&c=moteurpat&f=selection&p=&order=&order2=&motcle=&patronyme=%(surname)s"],
+    ["Person", "Fichier Origine", _("OrigineFile (Quebec)"), "http://www.fichierorigine.com/recherche.php?nom=%(surname)s"],
+    ["Person", "Geneanet", "Geneanet", "http://search.geneanet.org/result.php?lang=fr&name=%(surname)s"],
     ["Person", "Geneanet-Favrejhas", "Geneanet, Favrejhas", "http://gw1.geneanet.org/index.php3?b=favrejhas&m=NG&n=%(surname)s&t=N&x=0&y=0"],
     ["Person", "Roglo", "Roglo", "http://roglo.eu/roglo?m=NG&n=%(given)s+%(surname)s&t=PN"],
+    ["Person", "FamilySearch-Beta", _("FamilySearch.org Beta"), "http://beta.familysearch.org/s/search/index/record-search#searchType=records&filtered=false&fed=true&collectionId=&advanced=false&givenName=%(given)s&surname=%(surname)s&birthYear=%(birth)s&birthLocation=&deathYear=%(death)s&deathLocation="],
+    ["Person", "Gallica", "Gallica", '''http://gallica.bnf.fr/Search?ArianeWireIndex=index&p=1&lang=FR&q="%(surname)s,+%(given)s"'''],
+    ["Person", "Archive.org", "Archive.org", '''http://www.archive.org/search.php?query="%(surname)s,+%(given)s"'''],
+    ["Person", "FR-Google", _("FR Google"), '''http://www.google.fr/#hl=fr&q="%(surname)s,+%(given)s"'''],
+    #["Place", "Geneanet", "Geneanet", "http://search.geneanet.org/result.php?lang=fr&place=%(city)s"],
+    #["Place", u"Centre Départemental d'Histoire des Familles (CDHF)", _("Historical Families Center Departement (CDHF)"), "http://www.cdhf.net/fr/index.php?t=villages&d=villages&c=villages&f=results&p=&page=&lieu2=%(city)s"],
+    #["Place", "Fichier Origine", _("OrigineFile (Quebec)"), "http://www.fichierorigine.com/recherche.php?commune=%(city)s&pays=%(country)s"],
+    #["Place", "Gallica", "Gallica", "http://gallica.bnf.fr/Search?ArianeWireIndex=index&p=1&lang=FR&q=%(city)s"],
+    #["Place", "Archive.org", "Archive.org", "http://www.archive.org/search.php?query=%(city)s"],
+    #["Source", "Gallica", "Gallica", "http://gallica.bnf.fr/Search?ArianeWireIndex=index&p=1&lang=FR&q=%(source_title)s"],
+    #["Source", "Archive.org", "Archive.org", "http://www.archive.org/search.php?query=%(source_title)s"],
+    #["Source", "Google books", _("Google books"), "http://www.google.fr/search?tbs=bks%3A1&q=%(source_title)s"],
     ]
 
 def load_on_reg(dbstate, uistate, pdata):
