fbb6a764a08bfa279821abede379092acc146a05
nick-h <nick-h@1b16344b-c707-4684-8e57-f14b83debe0b>
2010-12-10 14:15:00 +0000

    Update target version

--- contrib/BirthdaysGramplet/BirthdaysGramplet.gpr.py
+++ contrib/BirthdaysGramplet/BirthdaysGramplet.gpr.py
@@ -1,13 +1,13 @@
 # File: Birthdays.gpr.py
 register(GRAMPLET,
-	id='Birthdays',
-	name=_("Birthdays Gramplet"),
-	description = _("a gramplet that displays the birthdays of the living people"),
-	status = STABLE,
-	version = '1.0.6',
-	fname="BirthdaysGramplet.py",
-	height = 200,
-	gramplet = 'BirthdaysGramplet',
-	gramps_target_version="3.2",
-	gramplet_title=_("Birthdays Gramplet")
-	)
+    id='Birthdays',
+    name=_("Birthdays Gramplet"),
+    description = _("a gramplet that displays the birthdays of the living people"),
+    status = STABLE,
+    version = '1.0.7',
+    fname="BirthdaysGramplet.py",
+    height = 200,
+    gramplet = 'BirthdaysGramplet',
+    gramps_target_version="3.3",
+    gramplet_title=_("Birthdays Gramplet")
+    )
