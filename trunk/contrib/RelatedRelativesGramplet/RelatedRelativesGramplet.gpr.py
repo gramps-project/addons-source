6b1afe27694af0175582ca3a7729dee491335dc7
romjerome <romjerome@1b16344b-c707-4684-8e57-f14b83debe0b>
2011-02-14 16:11:49 +0000

    update; mailing list (Heinz)

--- trunk/contrib/RelatedRelativesGramplet/RelatedRelativesGramplet.gpr.py
+++ trunk/contrib/RelatedRelativesGramplet/RelatedRelativesGramplet.gpr.py
@@ -8,6 +8,6 @@ register(GRAMPLET,
         expand=True,
         gramplet = 'RelatedRelativesGramplet',
         gramplet_title=_("Related Relatives"),
-        version = '1.0.2',
+        version = '1.0.3',
         gramps_target_version="3.3",
         )
--- trunk/contrib/RelatedRelativesGramplet/RelatedRelativesGramplet.py
+++ trunk/contrib/RelatedRelativesGramplet/RelatedRelativesGramplet.py
@@ -1,6 +1,6 @@
 # Gramps - a GTK+/GNOME based genealogy program
 #
-# Copyright (C) 2010  Heinz Brinker <heinzbrinker@yahoo.de>
+# Copyright (C) 2011  Heinz Brinker
 #
 # This program is free software; you can redistribute it and/or modify
 # it under the terms of the GNU General Public License as published by
@@ -32,7 +32,8 @@ import posixpath
 #------------------------------------------------------------------------
 from gen.display.name import displayer as name_displayer
 from gen.plug import Gramplet
-from gen.ggettext import sgettext as _
+from TransUtils import get_addon_translator
+_ = get_addon_translator().gettext
 from Utils import media_path_full
 import Relationship
 import DateHandler
@@ -48,106 +49,118 @@ class RelatedRelativesGramplet(Gramplet):
         self.set_text(_("No Family Tree loaded."))
 #         self.set_tooltip(_("Double-click item to see matches"))
 
+#
+# Register database triggers for updates
+#
     def db_changed(self):
         self.dbstate.db.connect('person-add', self.update)
-#         self.dbstate.db.connect('person-edit', self.update)
         self.dbstate.db.connect('person-delete', self.update)
         self.dbstate.db.connect('family-add', self.update)
         self.dbstate.db.connect('family-delete', self.update)
         self.dbstate.db.connect('person-rebuild', self.update)
         self.dbstate.db.connect('family-rebuild', self.update)
+
+#
+#   Function that recursively adds all descendants of a person to the
+#   person_handle_list
 #
     def addDescendants(self, ancestorHandle):
+        # If this person is not yet in the list, add person (and its descendants
+        # to descendants list.
         if ancestorHandle not in self.person_handle_list:
             self.person_handle_list.append(ancestorHandle)
-#             self.append_text(" +")
+#           Check if person is father or mother in a family
             ancestor = self.dbstate.db.get_person_from_handle(ancestorHandle)
             if ancestor:
-#                 pname = name_displayer.display(ancestor)
-#                 self.append_text(" %s " % pname)
                 for family_handle in ancestor.get_family_handle_list():
                     if family_handle:
                         family = self.dbstate.db.get_family_from_handle(family_handle)
                         if family:
+#                           Add all children of the person  to the list by
+#                           recursively calling this function for each one.
                             childlist = family.get_child_ref_list()[:]
                             for child_ref in childlist:
                                 self.addDescendants(child_ref.ref)
-#         else:
-#             self.append_text(" .")
 
-
+#
+#   Main function, called when the gramplet is opened or updated
+#
     def main(self):
-        self.set_text("Relations of related people in your database:")
+        # Write heading text to gramplet
+        self.set_text(_("Relations of related people in your database:"))
+        # Define basic variables
         database = self.dbstate.db
         rel_calc = Relationship.get_relationship_calculator()
         family_handle_list = [] # Will keep list of families with no ancestors
-        nf = 0
-#         self.append_text("Searching base families...\n")
-        # Find the base families with no ancestors
+
+        # Find all base families with no ancestors in the database.
         flist = database.iter_family_handles()
         for familyhandle in flist:
             family = database.get_family_from_handle(familyhandle)
-            nf += 1
-#             self.append_text("Checking family %d\n" % nf)
             # Check if mother of this family is child of another family
+            # If this is the case, skip this family
             ancestorHandle = family.get_mother_handle()
             if ancestorHandle:
                 mother = database.get_person_from_handle(ancestorHandle)
                 parent_handle_list = mother.get_parent_family_handle_list()
                 if parent_handle_list:
-#                     self.append_text(" Has Ancesctors\n")
                     continue
             # Check if father of this family is child of another family
+            # If this is the case, skip this family
             ancestorHandle = family.get_father_handle()
             if ancestorHandle:
                 father = database.get_person_from_handle(ancestorHandle)
                 parent_handle_list = father.get_parent_family_handle_list()
                 if parent_handle_list:
-#                   self.append_text(" Has Ancesctors\n")
                   continue
-            # Members of this family have no ancestors. Add family
+            # Members of this family have no ancestors. Add family to base
+            # family handle list
             family_handle_list.append(familyhandle)
-#             self.append_text("Processing to be done for family %d\n" % nf)
             yield True
 
-        # Iterate found families and find all descendants for each family
-#         self.append_text("Iterating base families...\n")
-        # The family_list contains all families with no ancestors.
-        pair_p1_list = []
-        pair_p2_list = []
+        # The base family handle list now contains all families that have
+        # no ancestors. Now iterate through found families. For each family
+        # first find all descendants and then check if one person in this
+        # family tree is partner of another person in the same tree.
+        #
+        # Related relatives may be found more than once from different base
+        # families. Therefore we hold a list of all found pairs to avoid
+        # listing them more than once
+        pair_p1_list = [] # List of all 1st related partners for all families
+        pair_p2_list = [] # List of all 2nd related partners for all families
         for familyhandle in family_handle_list:
-#             self.append_text("\n***** Next family *****\n")
+            # Build list of all related persons (descendants) of this family.
             self.person_handle_list = []
             family = database.get_family_from_handle(familyhandle)
-            # Add all descendants of the father
+            # Add all descendants of the father to person list of this family
             father_handle = family.get_father_handle()
             if father_handle:
                 father = database.get_person_from_handle(father_handle)
-                fathername = name_displayer.display(father)
-#                 self.append_text("Father: %s" % fathername)
+                # Add all descendants of the father
                 self.addDescendants(father_handle)
             mother_handle = family.get_mother_handle()
-            # Add all descendants of the mother
+            # Add all descendants of the mother to person list of this family
             if mother_handle:
                 mother = database.get_person_from_handle(mother_handle)
-                mothername = name_displayer.display(mother)
-#                 self.append_text(" Mother: %s" % mothername)
+                # Add all descendants of the mother
                 self.addDescendants(mother_handle)
-#             self.append_text("\n")
-            # List of descendants for this family is complete
-#             self.append_text(" %d Persons in tree\n" % len(self.person_handle_list))
-            # Check every person in the tree for partner that is part of this tree
+
+            # The person list of all descendants for this family is complete.
+            # Now check for every person in the list if it has a partner that
+            # is also in this list
             for checkHandle in self.person_handle_list:
                 person = database.get_person_from_handle(checkHandle)
                 if person:
-                    pname = name_displayer.display(person)
-#                     self.append_text("\n Check person %s \n" % pname)
                     pfamily_handle_list = person.get_family_handle_list()
                     if pfamily_handle_list:
                         for family_handle in pfamily_handle_list:
+                            # Skip the family if it is listed in the base
+                            # families list (which should be quite short).
                             if family_handle in family_handle_list:
-#                                 self.append_text("  Skipped own family\n")
                                 continue
+
+                            # If current person is father or mother in a family
+                            # find the handle of the partner
                             family = database.get_family_from_handle(family_handle)
                             father_handle = family.get_father_handle()
                             mother_handle = family.get_mother_handle()
@@ -155,9 +168,14 @@ class RelatedRelativesGramplet(Gramplet):
                                 handlepartner = mother_handle
                             else:
                                 handlepartner = father_handle
+
+                            # If the partner is in our list of persons of this
+                            # family tree, both are related.
                             if handlepartner in self.person_handle_list:
                                 newEntry = True;
-#                                 self.append_text("\n*M*A*T*C*H*\n")
+                                # Check if this pair is already in the lists of
+                                # related partners. A and B will also appear as
+                                # B and A. So we have to cross check
                                 for ii in range(len(pair_p1_list)):
                                     if checkHandle == pair_p1_list[ii]:
                                         if  handlepartner == pair_p2_list[ii]:
@@ -165,28 +183,67 @@ class RelatedRelativesGramplet(Gramplet):
                                     if checkHandle == pair_p2_list[ii]:
                                         if handlepartner == pair_p1_list[ii]:
                                             newEntry = False
+
+                                # If this pair is not yet listed, add them to
+                                # the list and show their relationship in the
+                                # gramplet window.
                                 if newEntry:
+                                    # Add pair to list of found related relatives
                                     pair_p1_list.append(checkHandle)
                                     pair_p2_list.append(handlepartner)
-                                    partner = self.dbstate.db.get_person_from_handle(handlepartner)
-                                    p1name = name_displayer.display(person)
-#                                     self.append_text("\n\n %s " % p1name)
-                                    self.append_text("\n\n")
-                                    self.link(p1name, 'Person', checkHandle)
-                                    p2name = name_displayer.display(partner)
-#                                     self.append_text(" und %s " % p2name)
-                                    self.append_text(" & ")
-                                    self.link(p2name, 'Person', handlepartner)
-                                    self.append_text(":")
-                                    rel_strings, common_an = rel_calc.get_all_relationships(database, person, partner)
-                                    rel_strings1, common_an = rel_calc.get_all_relationships(database, partner, person)
-                                    if rel_strings:
-                                        for relstring in rel_strings:
-                                            for relstring1 in rel_strings1:
-                                                self.append_text("\n%s" % relstring)
-                                                self.append_text(" & %s" % relstring1)
+                                    partner = self.dbstate.db. \
+                                        get_person_from_handle(handlepartner)
+                                    # Find relation between the partners by use
+                                    # of the relationship calculator. Print all
+                                    # relationships A to B and B to A.
+                                    rel_strings, common_an = \
+                                        rel_calc.get_all_relationships(database,
+                                                                       person,
+                                                                       partner)
+                                    rel_strings1, common_an1 = \
+                                        rel_calc.get_all_relationships(database,
+                                                                       partner,
+                                                                       person)
+                                    if len(rel_strings) > 1:
+                                        # Output names of both partners as links
+                                        p1name = name_displayer.display(person)
+                                        self.append_text("\n\n")
+                                        self.link(p1name, 'Person', checkHandle)
+                                        p2name = name_displayer.display(partner)
+                                        self.append_text(" " + _("and") + " ")
+                                        self.link(p2name, 'Person', handlepartner)
+                                        self.append_text(" " + \
+                                                         _("are partners and") + \
+                                                         ":")
+                                        # Omit the first relationship from list
+                                        for x in range(1, len(rel_strings)):
+                                            self.append_text("\n%s" %
+                                                             rel_strings[x])
+                                            self.append_text(" & %s" %
+                                                             rel_strings1[x])
+                                            # Print list of common ancestors for
+                                            # the found relation.
+                                            # Remove duplicate ancestors
+                                            anc_list = list(set(common_an[x]))
+                                            for anc in anc_list:
+                                                person = database. \
+                                                    get_person_from_handle(anc)
+                                                if person:
+                                                    # Print ancestor as link
+                                                    pname = name_displayer. \
+                                                        display(person)
+                                                    self.append_text("\n\t" \
+                                                        + _("Common ancestor") \
+                                                        + " ")
+                                                    self.link(pname, 'Person',
+                                                              anc)
+
+                        # After the check for each persons family allow other
+                        # threads to do some work.
                         yield True
+        # If the list of related pairs is empty we did not find any related
+        # relatives in the database.
         if len(pair_p1_list) == 0:
-            self.append_text("\nNo relatives in a relation found.\n")
-        self.append_text("\n\nEND\n")
-        yield False
+            self.append_text("\n" + _("No relatives in a relation found") + ".\n")
+        self.append_text("\n\n" + _("END") + "\n")
+        return
