24cf51ae88d0e6836db646b20460361785b1bee7
ldnp <ldnp@1b16344b-c707-4684-8e57-f14b83debe0b>
2011-03-16 14:28:30 +0000

    Update #2 of Swedish translations.

--- contrib/PythonGramplet/PythonGramplet.gpr.py
+++ contrib/PythonGramplet/PythonGramplet.gpr.py
@@ -7,7 +7,7 @@ register(GRAMPLET,
          height=250,
          gramplet = 'PythonGramplet',
          gramplet_title=_("Python Shell"),
-         version = '1.0.2',
+         version = '1.0.3',
          gramps_target_version = "3.3",
          help_url="PythonGramplet",
          )
