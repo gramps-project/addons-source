877844458a3d55bd64da3869aa72e1ba0b471083
gburto01 <gburto01@1b16344b-c707-4684-8e57-f14b83debe0b>
2011-07-04 18:57:52 +0000

    Patch to open the appropriate editor on doubleclick

--- trunk/contrib/ExtendedAttributes/ExtendedAttributes.py
+++ trunk/contrib/ExtendedAttributes/ExtendedAttributes.py
@@ -1,6 +1,7 @@
 # Gramps - a GTK+/GNOME based genealogy program
 #
 # Copyright (C) 2011 Nick Hall
+# Copyright (C) 2011 Gary Burton
 #
 # This program is free software; you can redistribute it and/or modify
 # it under the terms of the GNU General Public License as published by
@@ -23,8 +24,13 @@ from ListModel import ListModel, NOSORT
 from QuickReports import run_quick_report_by_name
 from gen.plug import Gramplet
 from gen.ggettext import gettext as _
+import gen.lib
 import DateHandler
 import gtk
+from gui.editors import EditPerson, EditFamily, EditEventRef
+import Errors
+from gen.db import DbTxn
+from gen.display.name import displayer as name_displayer
 
 class Attributes(Gramplet):
     """
@@ -47,24 +53,33 @@ class Attributes(Gramplet):
         titles = [(_('Date'), 1, 100),
                   ('', 1, 100),
                   (_('Key'), 2, 100),
-                  (_('Value'), 3, 100)]
-        self.model = ListModel(top, titles, event_func=self.display_report)
+                  (_('Value'), 3, 100),
+                  ('', NOSORT, 50,)
+                 ]
+        self.model = ListModel(top, titles, event_func=self._display_editor)
         return top
 
     def add_attributes(self, obj, date_object=None):
         """
         Add the attributes of an object to the model.
         """
-        event_date = event_sort = ''
+        event_date = event_sort = handle = ''
         if date_object is not None:
             event_date = DateHandler.displayer.display(date_object)
             event_sort = '%012d' % date_object.get_sort_value()
 
+        try:
+            handle = obj.get_handle()
+        except AttributeError:
+            handle = obj.ref
+         
         for attr in obj.get_attribute_list():
             self.model.add((event_date,
                             event_sort,
                             attr.get_type(),
-                            attr.get_value()))
+                            attr.get_value(),
+                            handle
+                            ))
 
     def display_attributes(self, obj):
         """
@@ -79,17 +94,11 @@ class Attributes(Gramplet):
 
         self.set_has_data(self.model.count > 0)
         
-    def display_report(self, treeview):
+    def _display_editor(self, treeview):
         """
-        Display the quick report for matching attribute key.
+        Display the appropriate editor window for the attribute.
         """
-        model, iter_ = treeview.get_selection().get_selected()
-        if iter_:
-            key = model.get_value(iter_, 2)
-            run_quick_report_by_name(self.dbstate, 
-                                     self.uistate, 
-                                     'attribute_match', 
-                                     key)
+        pass
 
     def get_has_data(self, obj):
         """
@@ -104,6 +113,19 @@ class Attributes(Gramplet):
                 return True
         return False
 
+    def _get_event_ref(self, event):
+        """
+        Return the event reference belonging to the active person or
+        family for the given event. 
+        """
+        for event_ref in self.object_for_update.get_event_ref_list():
+            if event_ref.ref == event.get_handle():
+                return event_ref
+        return None
+
+    def _object_edited(self, ref, event):
+        pass
+
 class ExtendedPersonAttributes(Attributes):
     """
     Displays the attributes of a person.
@@ -116,15 +138,58 @@ class ExtendedPersonAttributes(Attributes):
         self.update()
 
     def update_has_data(self):
-        active_handle = self.get_active('Person')
-        active = self.dbstate.db.get_person_from_handle(active_handle)
+        active = self.get_active_object('Person')
         self.set_has_data(self.get_has_data(active))
+
+    def _object_edited(self, ref, event):
+        """
+        Callback method for committing changes to the active person after an
+        event ref has been modified.
+        """
+        with DbTxn('', self.dbstate.db) as trans:
+            self.dbstate.db.commit_person(self.object_for_update, trans)
+            msg = _("Edit Person (%s)") % \
+                    name_displayer.display(self.object_for_update)
+            trans.set_description(msg)
+
+    def _display_editor(self, treeview):
+        """
+        Display the appropriate editor - either event ref or person
+        editor depending on the which type of object the handle belongs to
+        """
+        model, iter_ = treeview.get_selection().get_selected()
+        if iter_:
+            handle = model.get_value(iter_, 4)
+
+            event = self.dbstate.db.get_event_from_handle(handle)
+            if event:
+                event_ref = self._get_event_ref(event) 
+                try:
+                    EditEventRef(self.dbstate,
+                                 self.uistate,
+                                 [],
+                                 event,
+                                 event_ref,
+                                 self._object_edited)
+                except Errors.WindowActiveError:
+                    pass
+                return
+
+            person = self.dbstate.db.get_person_from_handle(handle)
+            if person:
+                try:
+                    EditPerson(self.dbstate, self.uistate, [], person)
+                except Errors.WindowActiveError:
+                    pass
     
     def main(self):
-        active_handle = self.get_active('Person')
-        active = self.dbstate.db.get_person_from_handle(active_handle)
-
         self.model.clear()
+        active = self.get_active_object('Person')
+
+        # Keep a pointer to the active person in case the user decides to
+        # modify an event ref attribute and we need to commit the change
+        self.object_for_update = active
+
         if active:
             self.display_attributes(active)
         else:
@@ -140,15 +205,55 @@ class ExtendedFamilyAttributes(Attributes):
         self.update()
 
     def update_has_data(self):
-        active_handle = self.get_active('Family')
-        active = self.dbstate.db.get_family_from_handle(active_handle)
+        active = self.get_active_object('Family')
         self.set_has_data(self.get_has_data(active))
     
-    def main(self):
-        active_handle = self.get_active('Family')
-        active = self.dbstate.db.get_family_from_handle(active_handle)
+    def _object_edited(self, ref, event):
+        """
+        Callback method for committing changes to the active person after an
+        event ref has been modified.
+        """
+        with DbTxn(_("Edit Family"), self.dbstate.db) as trans:
+            self.dbstate.db.commit_family(self.object_for_update, trans)
 
+    def _display_editor(self, treeview):
+        """
+        Display the appropriate editor - either event ref or family
+        editor depending on the which type of object the handle belongs to
+        """
+        model, iter_ = treeview.get_selection().get_selected()
+        if iter_:
+            handle = model.get_value(iter_, 4)
+
+            event = self.dbstate.db.get_event_from_handle(handle)
+            if event:
+                event_ref = self._get_event_ref(event) 
+                try:
+                    EditEventRef(self.dbstate,
+                                 self.uistate,
+                                 [],
+                                 event,
+                                 event_ref,
+                                 self._object_edited)
+                except Errors.WindowActiveError:
+                    pass
+                return
+
+            family = self.dbstate.db.get_family_from_handle(handle)
+            if family:
+                try:
+                    EditFamily(self.dbstate, self.uistate, [], family)
+                except Errors.WindowActiveError:
+                    pass
+
+    def main(self):
         self.model.clear()
+        active = self.get_active_object('Family')
+        
+        # Keep a pointer to the active family in case the user decides to
+        # modify an event ref attribute and we need to commit the change
+        self.object_for_update = active
+
         if active:
             self.display_attributes(active)
         else:
