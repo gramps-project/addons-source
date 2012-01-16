29074b89eecbb237d0a8ed649d6b82e5a6b37b2c
lcc-net <lcc-net@1b16344b-c707-4684-8e57-f14b83debe0b>
2012-01-16 16:32:02 +0000

    License.

--- contrib/DescendantsLines/DescendantsLines.py
+++ contrib/DescendantsLines/DescendantsLines.py
@@ -17,6 +17,33 @@
 # along with this program; if not, write to the Free Software
 # Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
 #
+# This program is based on the program located at
+# http://offog.org/darcs/misccode/familytree. The license for that
+# program is found at http://offog.org/darcs/misccode/NOTES.
+# Distributed under the terms of the X11 license:
+#
+#  Copyright 1998, 1999, 2000, 2001, 2002, 2003, 2004, 2005, 2006, 2007,
+#    2008, 2009, 2010, 2011 Adam Sampson <ats@offog.org>
+#
+#  Permission is hereby granted, free of charge, to any person obtaining a
+#  copy of this software and associated documentation files (the
+#  "Software"), to deal in the Software without restriction, including
+#  without limitation the rights to use, copy, modify, merge, publish,
+#  distribute, sublicense, and/or sell copies of the Software, and to
+#  permit persons to whom the Software is furnished to do so, subject to
+#  the following conditions:
+#
+#  The above copyright notice and this permission notice shall be included
+#  in all copies or substantial portions of the Software.
+#
+#  THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS
+#  OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF
+#  MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT.
+#  IN NO EVENT SHALL ADAM SAMPSON BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+#  LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING
+#  FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER
+#  DEALINGS IN THE SOFTWARE.
+#
 """
 Print Descendants Lines (experimental migration, UNSTABLE)
 """
