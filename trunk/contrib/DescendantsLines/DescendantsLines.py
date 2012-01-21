7cb3ea88cbc6bd1376193fedb77437e23a669d2c
lcc-net <lcc-net@1b16344b-c707-4684-8e57-f14b83debe0b>
2012-01-21 08:16:02 +0000

    Maintenance. Comment out unneeded code.

--- trunk/contrib/DescendantsLines/DescendantsLines.py
+++ trunk/contrib/DescendantsLines/DescendantsLines.py
@@ -152,6 +152,8 @@ class DescendantsLinesReport(Report):
         MIN_C_WIDTH
         TEXT_PAD
         TEXT_LINE_PAD
+        inc_places - Whether to include event places in the output.
+        inc_marriages - Whether to include marriage information in the output.
         """
 
         Report.__init__(self, database, options_class)
@@ -253,7 +255,6 @@ class DescendantsLinesReport(Report):
         #PYTHONPATH
         
         input_fn = os.path.join(const.USER_PLUGINS, 'DescendantsLines', 'DescendantsLines.xml')
-        #output_fn = os.path.join(const.USER_HOME, 'DescendantsLines.png')
         
         # Pass 2  
           
@@ -274,13 +275,13 @@ class DescendantsLinesReport(Report):
         self.xml_file = codecs.getwriter("utf8")(xml_file)
         self.write_xml_head()
         
-        self.xml_file.write('<events>\n')
-        for child in ind_list:
-            person = self.database.get_person_from_handle(child)
-            for event_ref in person.get_event_ref_list():
-                if event_ref.get_role() == gen.lib.EventRoleType.PRIMARY:
-                    self.write_xml_event(event_ref)
-        self.xml_file.write('</events>\n')
+#        self.xml_file.write('<events>\n')
+#        for child in ind_list:
+#            person = self.database.get_person_from_handle(child)
+#            for event_ref in person.get_event_ref_list():
+#                if event_ref.get_role() == gen.lib.EventRoleType.PRIMARY:
+#                    self.write_xml_event(event_ref)
+#        self.xml_file.write('</events>\n')
         
         self.xml_file.write('<people>\n')
         for child in ind_list:
@@ -320,39 +321,41 @@ class DescendantsLinesReport(Report):
     def write_xml_head(self):
         """
         Writes the header part of the xml file.
+        NOTE: This xml file is not following the Gramps XML
+        specification.
         """
         self.xml_file.write('<?xml version="1.0" encoding="UTF-8"?>\n')
         self.xml_file.write('<!DOCTYPE database PUBLIC "-//GRAMPS//DTD GRAMPS XML 1.4.0//EN"\n')
         self.xml_file.write('"http://gramps-project.org/xml/1.4.0/grampsxml.dtd">\n')
         self.xml_file.write('<database xmlns="http://gramps-project.org/xml/1.4.0/">\n')
 
-    def write_xml_event(self, event_ref):
-        """
-        Writes the event part of the xml file.
-        """
-        
-        event = find_event(self.database, event_ref.ref)
-        etype = event.get_type().xml_str()
-        date = event.get_date_object()
-        if self.inc_places:
-            placeh = event.get_place_handle()
-            place_title = None
-            if placeh:
-                place_title = self.database.get_place_from_handle(placeh).get_title()
-        local_date = DateHandler.displayer.display(date)
-        
-        self.xml_file.write('<event id="%s" handle="%s">\n' % (event.get_gramps_id(), event.handle))
-        self.xml_file.write('<type>%s</type>\n' % etype)
-        if date:
-            
-            # DTD needs date object, use translated date for report 
-            
-            self.xml_file.write('<dateval val=%s/>\n' % \
-                    xml.sax.saxutils.quoteattr(local_date))
-        if self.inc_places and place_title:
-            self.xml_file.write('<placetval val=%s/>\n' % \
-                    xml.sax.saxutils.quoteattr(place_title))
-        self.xml_file.write('</event>\n')
+#    def write_xml_event(self, event_ref):
+#        """
+#        Writes the event part of the xml file.
+#        """
+#        
+#        event = find_event(self.database, event_ref.ref)
+#        etype = event.get_type().xml_str()
+#        date = event.get_date_object()
+#        if self.inc_places:
+#            placeh = event.get_place_handle()
+#            place_title = None
+#            if placeh:
+#                place_title = self.database.get_place_from_handle(placeh).get_title()
+#        local_date = DateHandler.displayer.display(date)
+#        
+#        self.xml_file.write('<event id="%s" handle="%s">\n' % (event.get_gramps_id(), event.handle))
+#        self.xml_file.write('<type>%s</type>\n' % etype)
+#        if date:
+#            
+#            # DTD needs date object, use translated date for report 
+#            
+#            self.xml_file.write('<dateval val=%s/>\n' % \
+#                    xml.sax.saxutils.quoteattr(local_date))
+#        if self.inc_places and place_title:
+#            self.xml_file.write('<placetval val=%s/>\n' % \
+#                    xml.sax.saxutils.quoteattr(place_title))
+#        self.xml_file.write('</event>\n')
         
     def write_xml_person(self, person, identifiant, child, gender, first, surname, event_list):
         """
@@ -409,7 +412,7 @@ class DescendantsLinesReport(Report):
         """
         self.xml_file.write('</database>\n')
         
-    # Method below from plugins/textreport/DescendReport.py
+    # Method below from plugins/textreport/DescendReport.py, modified
     def __date_place(self,event):
         if event:
             date = DateHandler.get_date(event)
@@ -755,8 +758,8 @@ def load_gramps(fn, start):
             self.first = None
             self.prefix = None
             self.last = None
-            self.birth = None
-            self.death = None
+#            self.birth = None
+#            self.death = None
             self.birth_s = None
             self.death_s = None
             self.marriage_s = None
@@ -838,46 +841,46 @@ def load_gramps(fn, start):
             print 'No death event information found: ' + handle
         tpeople[id] = po
 
-    events = x.getElementsByTagName('events')[0]
-    for ev in events.getElementsByTagName('event'):
-        p_id = eventtoid.get(ev.getAttribute('handle'))
-        if p_id is None:
-            continue
-        po = tpeople[p_id]
-        etype = get_text(ev.getElementsByTagName('type'))
-        dvs = ev.getElementsByTagName('dateval')
-        date = None
-        if len(dvs) > 0:
-            date = ev.getElementsByTagName('dateval')[0].getAttribute('val')
-        else:
-            print 'Undated event: ' + ev.getAttribute('handle')
-
-        if INC_PLACES:
-            ptv = ev.getElementsByTagName('placetval')
-            placet = None
-            if len(ptv) > 0:
-                placet = ev.getElementsByTagName('placetval')[0].getAttribute('val')
-            else:
-                print 'Unplacetd event: ' + ev.getAttribute('handle')
-
-            if len(dvs) == 0 and len(ptv) == 0:
-                continue
-
-        elif len(dvs) == 0:
-            continue
-
-        if etype == 'Birth':
-            po.birth = date
-            if INC_PLACES:
-                if placet:
-                    po.birth += ' - ' + placet
-        elif etype == 'Death':
-            po.death = date
-            if INC_PLACES:
-                if placet:
-                    po.death += ' - ' + placet
-        else:
-            print 'Unknown event type: ' + etype
+#    events = x.getElementsByTagName('events')[0]
+#    for ev in events.getElementsByTagName('event'):
+#        p_id = eventtoid.get(ev.getAttribute('handle'))
+#        if p_id is None:
+#            continue
+#        po = tpeople[p_id]
+#        etype = get_text(ev.getElementsByTagName('type'))
+#        dvs = ev.getElementsByTagName('dateval')
+#        date = None
+#        if len(dvs) > 0:
+#            date = ev.getElementsByTagName('dateval')[0].getAttribute('val')
+#        else:
+#            print 'Undated event: ' + ev.getAttribute('handle')
+#
+#        if INC_PLACES:
+#            ptv = ev.getElementsByTagName('placetval')
+#            placet = None
+#            if len(ptv) > 0:
+#                placet = ev.getElementsByTagName('placetval')[0].getAttribute('val')
+#            else:
+#                print 'Unplacetd event: ' + ev.getAttribute('handle')
+#
+#            if len(dvs) == 0 and len(ptv) == 0:
+#                continue
+#
+#        elif len(dvs) == 0:
+#            continue
+#
+#        if etype == 'Birth':
+#            po.birth = date
+#            if INC_PLACES:
+#                if placet:
+#                    po.birth += ' - ' + placet
+#        elif etype == 'Death':
+#            po.death = date
+#            if INC_PLACES:
+#                if placet:
+#                    po.death += ' - ' + placet
+#        else:
+#            print 'Unknown event type: ' + etype
 
 
     class InFamily:
