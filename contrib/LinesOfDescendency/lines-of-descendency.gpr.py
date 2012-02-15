47847858b27935ebefbaaea6fc402e57f2e731c8
lcc-net <lcc-net@1b16344b-c707-4684-8e57-f14b83debe0b>
2012-02-15 14:02:35 +0000

    LinesOfDescendency - Maintenance, Licence, build, listing.

--- contrib/LinesOfDescendency/lines-of-descendency.gpr.py
+++ contrib/LinesOfDescendency/lines-of-descendency.gpr.py
@@ -1,9 +1,33 @@
+#
+# Lines of Descendency Report - a plugin for Gramps, the GTK+/GNOME based
+#                               genealogy program.
+#
+# Copyright (c) 2010, 2012 lcc <lcc.mailaddres@gmail.com>
+# 
+# Permission is hereby granted, free of charge, to any person obtaining a copy
+# of this software and associated documentation files (the "Software"), to deal
+# in the Software without restriction, including without limitation the rights
+# to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+# copies of the Software, and to permit persons to whom the Software is
+# furnished to do so, subject to the following conditions:
+# 
+# The above copyright notice and this permission notice shall be included in
+# all copies or substantial portions of the Software.
+# 
+# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+# IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+# FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+# AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+# LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+# OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+# SOFTWARE.
+#
 register(REPORT,
         id = 'LinesOfDescendency',
-        name = _('Lines of Descendency'),
+        name = _('Lines of Descendency Report'),
         description = _('Prints out all descendency lines '
             'from a given ancestor to a given descendent in text.'),
-        version = '1.1.7',
+        version = '1.1.8',
         gramps_target_version = '3.4',
         status = STABLE,
         fname = 'lines-of-descendency.py',
