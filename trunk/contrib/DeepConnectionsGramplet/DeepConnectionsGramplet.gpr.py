d3df35407905d1d42b88ad3f2effebe540ea20c6
dsblank <dsblank@1b16344b-c707-4684-8e57-f14b83debe0b>
2012-04-29 14:38:18 +0000

    5702: 'Fadder' in DeepConnectionsGramplet; added association text

--- trunk/contrib/DeepConnectionsGramplet/DeepConnectionsGramplet.gpr.py
+++ trunk/contrib/DeepConnectionsGramplet/DeepConnectionsGramplet.gpr.py
@@ -10,7 +10,7 @@ register(GRAMPLET,
          gramplet_title=_("Deep Connections"),
          detached_width = 510,
          detached_height = 480,
-         version = '1.0.10',
+         version = '1.0.11',
          gramps_target_version = "3.5",
          )
 
