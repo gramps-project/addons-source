3e9ae22c82e0666cdb85634f59a8715138c90666
romjerome <romjerome@1b16344b-c707-4684-8e57-f14b83debe0b>
2012-05-21 10:40:44 +0000

    5253: experimental translation (Gramps XML parsing) for using ElementTree (default) with python 2.6 only; update french translation

---
+++ contrib/lxml/etreeGramplet.gpr.py
@@ -0,0 +1,19 @@
+#------------------------------------------------------------------------
+#
+# Register the Gramplet
+#
+#------------------------------------------------------------------------
+
+register(GRAMPLET, 
+         id="etree Gramplet", 
+         name=_("etree Gramplet"), 
+         description = _("Gramplet for testing etree with Gramps XML"),
+         status = STABLE,
+         version = '0.0.3',
+         gramps_target_version = "3.5",
+         include_in_listing = False,
+         height = 300,
+         gramplet = "etreeGramplet",
+         fname ="etreeGramplet.py",
+         gramplet_title =_("etree"),
+         )
