a33aab73f95f327d8e23022afccc0eb45d1bb1fc
romjerome <romjerome@1b16344b-c707-4684-8e57-f14b83debe0b>
2012-11-15 06:40:22 +0000

    start to fix some import (gramps modules)

--- trunk/contrib/BirthdaysGramplet/BirthdaysGramplet.gpr.py
+++ trunk/contrib/BirthdaysGramplet/BirthdaysGramplet.gpr.py
@@ -8,7 +8,7 @@ register(GRAMPLET,
 	fname="BirthdaysGramplet.py",
 	height = 200,
 	gramplet = 'BirthdaysGramplet',
-	gramps_target_version = "3.5",
+	gramps_target_version = "4.0",
 	gramplet_title = _("Birthdays Gramplet"),
 	help_url = "BirthdaysGramplet",
 	)
