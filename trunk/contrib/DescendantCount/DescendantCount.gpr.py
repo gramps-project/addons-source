2b20a52a86a7ccd0dee31ab13c17b9d80bac72dd
romjerome <romjerome@1b16344b-c707-4684-8e57-f14b83debe0b>
2012-11-17 16:46:49 +0000

    update some import (gramps modules)

--- trunk/contrib/DescendantCount/DescendantCount.gpr.py
+++ trunk/contrib/DescendantCount/DescendantCount.gpr.py
@@ -9,7 +9,7 @@ register(QUICKREPORT,
          authors=["Douglas S. Blank"],
          authors_email=["doug.blank@gmail.com"],
          version = '1.0.8',
-         gramps_target_version = "3.5",
+         gramps_target_version = "4.0",
          )
 
 register(GRAMPLET, 
@@ -25,6 +25,6 @@ register(GRAMPLET,
          detached_width = 600,
          detached_height = 400,
          version = '1.0.9',
-         gramps_target_version = "3.5",
+         gramps_target_version = "4.0",
          )
 
