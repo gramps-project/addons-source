01d56e7d9386835bf3042620f111fdb03ff4a3c8
romjerome <romjerome@1b16344b-c707-4684-8e57-f14b83debe0b>
2012-11-25 14:05:41 +0000

    ugettext support for python 2.7

--- contrib/DescendantsLines/DescendantsLines.gpr.py
+++ contrib/DescendantsLines/DescendantsLines.gpr.py
@@ -25,7 +25,7 @@ register(REPORT,
          description =  _("Produces descendants lines of a person"),
          version = '0.1.13',
          gramps_target_version = '4.0',
-         status = STABLE, # not yet tested with python 3
+         status = STABLE, # not yet tested with python 3, reduce() not supported in 3.x; use functools.reduce()
          fname = 'DescendantsLines.py',
          category = CATEGORY_DRAW,
          reportclass = 'DescendantsLinesReport',
