1f284ea988c177504fdec15f75e120e6540be10d
romjerome <romjerome@1b16344b-c707-4684-8e57-f14b83debe0b>
2012-11-25 16:01:27 +0000

    update comment

--- trunk/contrib/NumberOfDescendantsQuickview/NumberOfDescendantsQuickview.gpr.py
+++ trunk/contrib/NumberOfDescendantsQuickview/NumberOfDescendantsQuickview.gpr.py
@@ -4,7 +4,7 @@ register(QUICKREPORT,
          description= _("Shows the number of descendants of the current person"),
          version = '3.4.11',
          gramps_target_version = '4.0',
-         status = STABLE, # not yet tested with python 3
+         status = UNSTABLE, # not yet tested with python 3, error on unicode row
          fname = 'NumberOfDescendantsQuickview.py',
          authors = ["Reinhard Mueller"],
          authors_email = ["reinhard.mueller@igal.at"],
