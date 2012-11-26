131fe5dd59a884ec8d4dcf15e95325e576d9d4b9
romjerome <romjerome@1b16344b-c707-4684-8e57-f14b83debe0b>
2012-11-26 15:24:30 +0000

    update comment

--- trunk/contrib/SetAttributeTool/SetAttributeTool.gpr.py
+++ trunk/contrib/SetAttributeTool/SetAttributeTool.gpr.py
@@ -26,7 +26,7 @@ register(TOOL,
          description =  _("Set an attribute to a given value."),
          version = '0.0.11',
          gramps_target_version = '4.0',
-         status = UNSTABLE, # not yet tested with python 3
+         status = STABLE, # not yet tested with python 3
          fname = 'SetAttributeTool.py',
          authors = ["Douglas S. Blank"],
          authors_email = ["doug.blank@gmail.com"],
