82d585b9095b3551b070014caee7445a95620a36
uli22 <uli22@1b16344b-c707-4684-8e57-f14b83debe0b>
2013-01-29 20:08:37 +0000

    New Source Report

---
+++ contrib/SourcesCitationsReport/SourcesCitationsReport.gpr.py
@@ -0,0 +1,22 @@
+#------------------------------------------------------------------------
+#
+# SourcesCitations Report
+#
+#------------------------------------------------------------------------
+
+plg = newplugin()
+plg.id    = 'SourcesCitationsReport'
+plg.name  = _("Sources and Citations Report")
+plg.description =  _("Provides a source and Citations with notes")
+plg.version = '1.0'
+plg.gramps_target_version = '3.4'
+plg.status = STABLE
+plg.fname = 'SourcesCitationsReport.py'
+plg.ptype = REPORT
+plg.authors = ["Uli22"]
+plg.authors_email = ["hansulrich.frink@gmail.com"]
+plg.category = CATEGORY_TEXT
+plg.reportclass = 'SourcesCitationsReport'
+plg.optionclass = 'SourcesCitationsOptions'
+plg.report_modes = [REPORT_MODE_GUI, REPORT_MODE_BKI, REPORT_MODE_CLI]
+plg.require_active = False
