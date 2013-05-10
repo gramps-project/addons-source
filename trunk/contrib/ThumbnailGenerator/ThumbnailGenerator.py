1d5e9a20440cbb5385d6a1b706d28330968db451
nick-h <nick-h@1b16344b-c707-4684-8e57-f14b83debe0b>
2013-05-10 16:04:55 +0000

    6710: Add missing translation code

--- trunk/contrib/ThumbnailGenerator/ThumbnailGenerator.py
+++ trunk/contrib/ThumbnailGenerator/ThumbnailGenerator.py
@@ -31,6 +31,13 @@ from gramps.gui.utils import ProgressMeter
 from gramps.gui.thumbnails import get_thumbnail_image, SIZE_NORMAL, SIZE_LARGE
 from gramps.gen.utils.file import media_path_full
 
+from gramps.gen.const import GRAMPS_LOCALE as glocale
+try:
+    _trans = glocale.get_addon_translator(__file__)
+except ValueError:
+    _trans = glocale.translation
+_ = _trans.gettext
+
 #-------------------------------------------------------------------------
 #
 # Thumbnail Generator
