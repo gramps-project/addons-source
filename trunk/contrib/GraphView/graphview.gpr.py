554716c529783f6207363d4c2eb230bb905ef0f0
nick-h <nick-h@1b16344b-c707-4684-8e57-f14b83debe0b>
2013-05-14 20:08:32 +0000

    python3 fixes for GraphView

--- trunk/contrib/GraphView/graphview.gpr.py
+++ trunk/contrib/GraphView/graphview.gpr.py
@@ -1,11 +1,11 @@
 register(VIEW, 
          id    = 'graphview',
          name  = _("Graph View"),
-         category = ("Ancestry", _("Ancestry")),
+         category = ("Ancestry", _("Charts")),
          description =  _("Dynamic graph of relations"),
          version = '1.0.37',
          gramps_target_version = '4.1',
-         status = UNSTABLE, # ImportError: No module named 'cStringIO'
+         status = STABLE
          fname = 'graphview.py',
          authors = ["Gary Burton"],
          authors_email = ["gary.burton@zen.co.uk"],
