2e66c7a0ac24e52ec734a72e7748a418cbddc4a3
sam888 <sam888@1b16344b-c707-4684-8e57-f14b83debe0b>
2015-01-09 00:19:21 +0000

    Update HeadlineNews tgz

--- trunk/contrib/HeadlineNewsGramplet/HeadlineNewsGramplet.gpr.py
+++ trunk/contrib/HeadlineNewsGramplet/HeadlineNewsGramplet.gpr.py
@@ -8,7 +8,7 @@ register(GRAMPLET,
          expand=True,
          gramplet = 'HeadlineNewsGramplet',
          gramplet_title=_("Headline News"),
-         version = '1.0.25',
+         version = '1.0.26',
          gramps_target_version="4.2",
          help_url="HeadlineNewsGramplet"
          )
