99443b2e13fd6b71b943e12cdb00f6197d35ff5a
sam888 <sam888@1b16344b-c707-4684-8e57-f14b83debe0b>
2015-03-17 05:15:01 +0000

    DenominoVisio: Replace cuni with str

--- contrib/DenominoViso/DenominoViso.gpr.py
+++ contrib/DenominoViso/DenominoViso.gpr.py
@@ -18,6 +18,6 @@ register(REPORT,
                     'representation of ancestors/descendants (SVG) '
                     'where details about individuals become visible '
                     'upon mouse-events.'),
-    version = '2.3.22',
+    version = '2.3.23',
     gramps_target_version = '4.2',
 )
