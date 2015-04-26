9f8ba9366c8ad123e86d2336e74f1cac20e012b2
sam888 <sam888@1b16344b-c707-4684-8e57-f14b83debe0b>
2015-04-26 00:53:45 +0000

    Regenerate Sqlite addon listing as cuni error already fixed

--- contrib/Sqlite/Sqlite.gpr.py
+++ contrib/Sqlite/Sqlite.gpr.py
@@ -2,9 +2,9 @@ register(IMPORT,
          id    = 'im_sqlite',
          name  = _('SQLite Import'),
          description =  _('SQLite is a common local database format'),
-         version = '1.0.25',
+         version = '1.0.27',
          gramps_target_version = "4.2",
-         status = STABLE, # not yet tested with python 3, need to review unicode usage 
+         status = STABLE, # tested with python 3, need to review unicode usage 
          fname = 'ImportSql.py',
          import_function = 'importData',
          extension = "sql"
@@ -14,9 +14,9 @@ register(EXPORT,
          id    = 'ex_sqlite',
          name  = _('SQLite Export'),
          description =  _('SQLite is a common local database format'),
-         version = '1.0.24',
+         version = '1.0.26',
          gramps_target_version = "4.2",
-         status = STABLE, # not yet tested with python 3
+         status = STABLE, # tested with python 3 but still gives errors
          fname = 'ExportSql.py',
          export_function = 'exportData',
          extension = "sql",
