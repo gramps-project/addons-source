acb499b4d0ce8fa7d6f1f62ee0b06ba36b364456
Sam Manzi <manzi.sam@gmail.com>
2015-08-29 11:34:38 +1000

    HeadnewsGramplet - rebuild

--- HeadlineNewsGramplet/HeadlineNewsGramplet.gpr.py
+++ HeadlineNewsGramplet/HeadlineNewsGramplet.gpr.py
@@ -8,7 +8,7 @@ register(GRAMPLET,
          expand=True,
          gramplet = 'HeadlineNewsGramplet',
          gramplet_title=_("Headline News"),
-         version = '1.0.28',
+         version = '1.0.29',
          gramps_target_version="5.0",
          help_url="HeadlineNewsGramplet"
          )
