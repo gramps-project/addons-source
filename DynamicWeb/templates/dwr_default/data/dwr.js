fc6263a0af9396dc5c6c95ea5fc98af7be1a9615
Pierre Bélissent <pierre.belissent@gmail.com>
2015-09-03 23:45:05 +0200

    DynamicWeb report: statistics initial test

--- DynamicWeb/templates/dwr_default/data/dwr.js
+++ DynamicWeb/templates/dwr_default/data/dwr.js
@@ -1334,7 +1334,6 @@ function printMediaRefArea(bk_table, fref, fname)
 		}
 		if (!isNaN(rect[0]) && rect.join(',') != '0,0,100,100')
 		{
-		
 			html += '<a href="' + fref(idx) + '"';
 			html += ' title="' + fname(idx) + '" class="dwr-imgmap" style="';
 			html += 'left: ' + rect[0] + '%;';
@@ -1765,26 +1764,31 @@ function printFamiliesIndex()
 {
 	document.write(htmlFamiliesIndex());
 }
-function htmlFamiliesIndex()
+function htmlFamiliesIndex(data)
 {
 	var html = '';
-	html += '<h2 class="page-header">' + _('Families Index') + '</h2>';
+	if (typeof(data) == 'undefined')
+	{
+		html += '<h2 class="page-header">' + _('Families Index') + '</h2>';
+		data = [];
+		for (var x = 0; x < F.length; x++) data.push(x);
+	}
 	var columns = [{
 		title: _('Father'),
-		ftext: printIndexSpouseText,
-		fhref: famHref,
-		fsort: printIndexSpouseIdx
+		ftext: function(x, col) {return(printIndexSpouseText(data[x], col));},
+		fhref: function(x) {return(famHref(data[x]));},
+		fsort: function(x, col) {return(printIndexSpouseIdx(data[x], col));}
 	}, {
 		title: _('Mother'),
-		ftext: printIndexSpouseText,
-		fhref: famHref,
-		fsort: printIndexSpouseIdx
+		ftext: function(x, col) {return(printIndexSpouseText(data[x], col));},
+		fhref: function(x) {return(famHref(data[x]));},
+		fsort: function(x, col) {return(printIndexSpouseIdx(data[x], col));}
 	}];
 	if (INDEX_SHOW_MARRIAGE) columns.push({
 		title: _('Marriage'),
-		ftext: function(fdx, col) {return(F[fdx].marr_year);}
+		ftext: function(x, col) {return(F[data[x]].marr_year);}
 	});
-	html += printIndex(F, [0, 'asc'], columns);
+	html += printIndex(data, [0, 'asc'], columns);
 	return(html);
 }
 
