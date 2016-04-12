920f2c1eaec452986dbea4e8c26b3d18abf7cdba
système <romjerome@yahoo.fr>
2016-04-12 13:25:42 +0200

    7955: provide an utility tool: a Relationships map
    overview of kekule/sosa and custom Rel_ids
    
    resume all informations on a tab (rows/columns) with ability to sort

---
+++ RelID/Relation_tab.gpr.py
@@ -0,0 +1,49 @@
+#
+# Gramps - a GTK+/GNOME based genealogy program
+#
+# Copyright (C) 2009 Benny Malengier
+#
+# This program is free software; you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation; either version 2 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program; if not, write to the Free Software
+# Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
+#
+
+"""
+GRAMPS registration file
+"""
+
+MODULE_VERSION="4.2"
+
+#------------------------------------------------------------------------
+#
+# Association State
+#
+#------------------------------------------------------------------------
+
+register(TOOL,
+id    = 'relationtab',
+name  = "Display Relation with home person",
+description =  ("Will display relational informations with the home person"),
+version = '1.0',
+gramps_target_version = MODULE_VERSION,
+status = STABLE,
+fname = 'Relation_tab.py',
+authors = ["Jérôme Rapinat"],
+authors_email = ["romjerome@yahoo.fr"],
+category = TOOL_UTILS,
+toolclass = 'RelationTab',
+optionclass = 'RelationTabOptions',
+tool_modes = [TOOL_MODE_GUI, TOOL_MODE_CLI]
+  )
+
+
---
+++ RelID/Relation_tab.py
@@ -0,0 +1,156 @@
+#
+# Gramps - a GTK+/GNOME based genealogy program
+#
+# Copyright (C) 2000-2006  Donald N. Allingham
+#
+# This program is free software; you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation; either version 2 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program; if not, write to the Free Software
+# Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
+#
+
+"""
+Relations tab.
+
+"""
+from gramps.gen.const import GRAMPS_LOCALE as glocale
+_ = glocale.translation.gettext
+from gi.repository import Gtk
+from gramps.gui.listmodel import ListModel, INTEGER
+from gramps.gui.managedwindow import ManagedWindow
+from gramps.gui.utils import ProgressMeter
+
+from gramps.gui.plug import tool
+from gen.display.name import displayer as name_displayer
+#from gramps.gen.lib.handle import Handle
+from gramps.gen.relationship import get_relationship_calculator
+from gramps.gen.config import config
+import number
+
+#-------------------------------------------------------------------------
+#
+#
+#
+#-------------------------------------------------------------------------
+class RelationTab(tool.Tool, ManagedWindow):
+
+    def __init__(self, dbstate, user, options_class, name, callback=None):
+        uistate = user.uistate
+        self.label = _("Relation and distances with root")
+        tool.Tool.__init__(self, dbstate, options_class, name)
+        if uistate:
+            ManagedWindow.__init__(self,uistate,[],
+                                                 self.__class__)
+            titles = [
+                (_('Rel_id'), 0, 75, INTEGER), # would be INTEGER
+                (_('Relation'), 1, 300),
+                (_('Name'), 2, 200),
+                (_('up'), 3, 35, INTEGER),
+                (_('down'), 4, 35, INTEGER),
+                (_('Common MRA'), 5, 75, INTEGER),
+                (_('Rank'), 6, 75, INTEGER),
+                ]
+
+            treeview = Gtk.TreeView()
+            model = ListModel(treeview, titles)
+            window = Gtk.Window()
+            window.set_default_size(880, 600)
+            s = Gtk.ScrolledWindow()
+            s.add(treeview)
+            window.add(s)
+
+        stats_list = []
+        max_level = config.get('behavior.generation-depth')
+
+        default_person = dbstate.db.get_default_person()
+
+        if default_person:
+            plist = dbstate.db.get_person_handles(sort_handles=True)
+            relationship = get_relationship_calculator()
+            progress = ProgressMeter(self.label, can_cancel=True,
+                                 parent=window)
+            if progress.get_cancelled():
+                return
+            length = len(plist)
+            progress.set_pass(_('Generating Relation tab'), length)
+            for handle in plist:
+                progress.step()
+                person = dbstate.db.get_person_from_handle(handle)
+                rel = relationship.get_one_relationship(
+                                            dbstate.db, default_person, person)
+                dist = relationship.get_relationship_distance_new(
+                          dbstate.db, default_person, person, only_birth=True)
+                rel_a = dist[0][2]
+                Ga = len(rel_a)
+                rel_b = dist[0][4]
+                Gb = len(rel_b)
+                mra = 1
+                rank = dist[0][0]
+
+                if rank == -1: # not related people
+                    continue
+
+                for letter in rel_a:
+                    if letter == 'f':
+                        mra = mra * 2
+                    if letter == 'm':
+                        mra = mra * 2 + 1
+
+                name = name_displayer.display(person)
+                kekule = number.get_number(Ga, Gb, rel_a, rel_b)
+
+                # work-around
+                if kekule == "u": # cousin(e)s need a key
+                   kekule = 0
+                if kekule == "nb": # non-birth
+                   kekule = -1
+                try:
+                   test = int(kekule)
+                except: # 1: related to mother; 0.x : no more girls lineage
+                   kekule = 1
+
+                stats_list.append((int(kekule), rel, name, int(Ga), 
+                                    int(Gb), int(mra), int(rank)))
+
+        for entry in stats_list:
+            model.add(entry, entry[0])
+        window.show_all()
+        self.set_window(window, None, self.label)
+        self.show()
+        progress.close()
+
+        for handle in plist:
+            person = dbstate.db.get_person_from_handle(handle)
+            name1 = name_displayer.display(person)
+            refs = person.get_person_ref_list()
+            if refs:
+                for ref in person.serialize()[-1]:
+                    (a, b, c, two, value) = ref
+                    person2 = dbstate.db.get_person_from_handle(two)
+                    name2 = name_displayer.display(person2)
+                    #stats_list.append((name1, value, name2))
+
+    def build_menu_names(self, obj):
+        return (self.label,None)
+
+#------------------------------------------------------------------------
+#
+#
+#
+#------------------------------------------------------------------------
+class RelationTabOptions(tool.ToolOptions):
+    """
+    Defines options and provides handling interface.
+    """
+
+    def __init__(self, name, person_id=None):
+        tool.ToolOptions.__init__(self, name, person_id)
