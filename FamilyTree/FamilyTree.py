4e9e3e19f6753585d586cfa08563e2e20d5b2361
système <romjerome@yahoo.fr>
2016-07-12 18:16:34 +0200

    9573: Family Tree doesn't display location
    fix a possible typo on StyledText strings

--- FamilyTree/FamilyTree.py
+++ FamilyTree/FamilyTree.py
@@ -99,7 +99,7 @@ class FamilyTree(gramps.gen.plug.report.Report):
 
         if not self.title:
             name = self.__family_get_display_name(self.center_family)
-            self.title = StyledText(_("Family Tree for %s")) % name
+            self.title = StyledText(_("Family Tree for %s") % name)
 
         style_sheet = self.doc.get_style_sheet()
         self.line_width = pt2cm(style_sheet.get_draw_style("FTR-box").get_line_width())
@@ -233,7 +233,7 @@ class FamilyTree(gramps.gen.plug.report.Report):
         else:
             father = None
         if father:
-            if kekule: 
+            if kekule:
                 father_text = [('FTR-name', StyledText(str(kekule) + " ") + self.__person_get_display_name(father))] + [('FTR-data', p) for p in self.__person_get_display_data(father)]
             else:
                 father_text = [('FTR-name', self.__person_get_display_name(father))] + [('FTR-data', p) for p in self.__person_get_display_data(father)]
@@ -252,7 +252,7 @@ class FamilyTree(gramps.gen.plug.report.Report):
         else:
             mother = None
         if mother:
-            if kekule > 1: 
+            if kekule > 1:
                 mother_text = [('FTR-name', StyledText(str(kekule+1) + " ") + self.__person_get_display_name(mother))] + [('FTR-data', p) for p in self.__person_get_display_data(mother)]
             else:
                 mother_text = [('FTR-name', self.__person_get_display_name(mother))] + [('FTR-data', p) for p in self.__person_get_display_data(mother)]
@@ -432,7 +432,7 @@ class FamilyTree(gramps.gen.plug.report.Report):
                     spouse_number = StyledText("")
                 if spouse_handle is not None:
                     spouse = self.database.get_person_from_handle(spouse_handle)
-                    
+
                     family_node['text'] += [('FTR-name', spouse_number + self.__person_get_display_name(spouse))] + [('FTR-data', p) for p in self.__person_get_display_data(spouse)]
                 elif spouse_number:
                     family_node['text'] += [('FTR-name', spouse_number)]
@@ -546,7 +546,7 @@ class FamilyTree(gramps.gen.plug.report.Report):
                     self.__draw_box(family_node['text'], family_node['color'], self.descendant_max_color + 1, generation, self.yoffset_d + family_node['top'], family_node['height'])
                     if family_node['child_list']:
                         self.__print_descendants_tree(
-                                family_node['child_list'], 
+                                family_node['child_list'],
                                 self.yoffset_d + family_node['top'] + family_node['height'] / 2,
                                 generation + 1)
             else:
@@ -672,9 +672,9 @@ class FamilyTree(gramps.gen.plug.report.Report):
         else:
             mother_name = _("Unknown")
 
-        return StyledText(_("%(father)s and %(mother)s")) % {
+        return StyledText(_("%(father)s and %(mother)s") % {
                 'father': father_name,
-                'mother': mother_name}
+                'mother': mother_name})
 
 
     def __person_get_display_name(self, person):
@@ -821,7 +821,7 @@ class FamilyTree(gramps.gen.plug.report.Report):
         # cut "," from end of the string and limit length of note to 50 characters
         note_len = len(note)
         if note_len > 50:
-            note = note[:48] 
+            note = note[:48]
             note += "..."
         else:
             note_len -= 2
@@ -945,7 +945,7 @@ class FamilyTree(gramps.gen.plug.report.Report):
             if place.private and self.protect_private:
                 place_text = ""
             else:
-                place_text = place_displayer.display(self.database, place)
+                place_text = place_displayer.display_event(self.database, event)
         elif self.missinginfo:
             place_text = "____________"
         else:
@@ -1058,7 +1058,7 @@ class FamilyTreeOptions(gramps.gen.plug.report.MenuReportOptions):
         kekule_start_number = gramps.gen.plug.menu.NumberOption(_("Kekule number of husband"), 0, 0, 16384)
         kekule_start_number.set_help(_("The Kekule number of the husband (central family). Set 0 to not show Kekule numbers"))
         menu.add_option(category_name, "kekule_start_number", kekule_start_number)
-        
+
         fit_on_page = gramps.gen.plug.menu.BooleanOption(_("Scale to fit on a single page"), True)
         fit_on_page.set_help(_("Whether to scale to fit on a single page."))
         menu.add_option(category_name, 'fit_on_page', fit_on_page)
