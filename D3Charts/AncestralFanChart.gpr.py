38b317b0565389938f11b89815ce894c4528a385
Sam Manzi <manzi.sam@gmail.com>
2016-12-07 20:11:32 +1100

    D3Charts rebuild

--- D3Charts/AncestralFanChart.gpr.py
+++ D3Charts/AncestralFanChart.gpr.py
@@ -32,6 +32,6 @@ register(REPORT,
                     'representation of ancestors (SVG) '
                     'represented as a Fan Chart from the D3.js '
                     'JavaScript library.'),
-    version = '1.0.18',
+    version = '1.0.19',
     gramps_target_version = "5.0",
 )
