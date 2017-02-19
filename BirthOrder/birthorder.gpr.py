8036e291606b68b1014db9db739cbfb9e184be3f
prculley <paulr2787@gmail.com>
2017-02-19 14:05:53 -0600

    A new Birth Order plugin Tool

---
+++ BirthOrder/birthorder.gpr.py
@@ -0,0 +1,25 @@
+#------------------------------------------------------------------------
+#
+# Birth Order
+#
+#------------------------------------------------------------------------
+
+register(TOOL,
+id    = 'birthorder',
+name  = _("Sort Children in Birth order"),
+description =  _("Looks through families, looking for children that are not "
+                 "in birth order.  User can accept individual suggestions to "
+                 "correct, edit the birth order manually, accept all those "
+                 "families that have all children with proper birth dates, or "
+                 "accept all."),
+version = '0.0.1',
+gramps_target_version = '4.2',
+status = STABLE,
+fname = 'birthorder.py',
+authors = ["Paul R. Culley"],
+authors_email = ["paulr2787@gmail.com"],
+category = TOOL_DBPROC,
+toolclass = 'BirthOrder',
+optionclass = 'BirthOrderOptions',
+tool_modes = [TOOL_MODE_GUI]
+  )
