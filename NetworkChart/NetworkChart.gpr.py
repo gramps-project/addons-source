e3ca71d6acaee0876e274e37fc7b5d490e9ae2f9
zorkmark <zorkmark@gmail.com>
2017-03-19 18:28:02 -0500

    Added dependency dialog with link to wiki
    Fixed highlight bug related to center_person
    
    All fixes from 0.0.3

--- NetworkChart/NetworkChart.gpr.py
+++ NetworkChart/NetworkChart.gpr.py
@@ -17,11 +17,13 @@
 # Foundation, Inc., 51 Franklin Street, Fifth Floor, 
 # Boston, MA 02110-1301  USA
 #
-# version 0.0.2
+# version 0.0.4
 #-------------------------------------------------------------------------
 import importlib
-module = importlib.find_loader("networkx")
-if module is not None:
+from gramps.gen.const import URL_WIKISTRING
+module1 = importlib.find_loader("networkx")
+module2 = importlib.find_loader("pygraphviz")
+if module1 is not None and module2 is not None:
     register(REPORT,
         id = 'networkchart',
         name = _('Network Chart'),
@@ -33,12 +35,36 @@ if module is not None:
         report_modes = [REPORT_MODE_GUI, REPORT_MODE_CLI],
         authors = ['Mark B.'],
         authors_email = ['familynetworkchart@gmail.com'],
+        #help_url = URL_WIKISTRING+'NetworkChart',
         description = _('Generates a family network chart.'),
-        version = '0.0.2',
+        version = '0.0.4',
         gramps_target_version = '4.2',
         include_in_listing = True,
     )
 else:
+    from gramps.gen.config import config
+    from gramps.gui.dialog import QuestionDialog2
     from gramps.gen.config import logging
-    warn_msg = _("Failure to load Family NetworkChart.  Networkx python module not found.")
-    logging.log(logging.WARNING, warn_msg)
+    if module1 is None:
+        warn_msg = _("NetworkChart Warning:  Python networkx module not found.")
+        logging.log(logging.WARNING, warn_msg)
+    if module2 is None:
+        warn_msg = _("NetworkChart Warning:  Python pygraphviz module not found.")
+        logging.log(logging.WARNING, warn_msg)
+    inifile = config.register_manager("networkchartwarn")
+    inifile.load()
+    sects = inifile.get_sections()
+    if 'networkchartwarn' not in sects:
+        yes_no = QuestionDialog2(_("NetworkChart Failed to Load"),
+            _("\n\nNetworkChart is missing python modules.  Both networkx \n" 
+              "and pygraphviz must be installed.  For now, you can install \n"
+              "the files manually.  See \n\n"
+              "https://gramps-project.org/wiki/index.php?title=NetworkChart \n\n"
+              "To dismiss all future NetworkChart warnings click Dismiss."),
+            _(" Dismiss "),
+            _("Continue"))
+        prompt = yes_no.run()
+        if prompt is True:
+            inifile.register('networkchartwarn.MissingModules', "")
+            inifile.set('networkchartwarn.MissingModules', "True")
+            inifile.save()
