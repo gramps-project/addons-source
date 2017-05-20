b0d159cc98f7ca52b0bb082b355b06efc044fa73
Sam Manzi <manzi.sam@gmail.com>
2017-05-20 11:09:20 +1000

    ChangeGivenNames rebuild fix10044

--- ChangeGivenNames/ChangeGivenNames.gpr.py
+++ ChangeGivenNames/ChangeGivenNames.gpr.py
@@ -36,7 +36,7 @@ id    = 'chgivenname',
 name  = _("Fix Capitalization of Given Names"),
 description =  _("Searches the entire database and attempts to "
                     "fix capitalization of the given names."),
-version = '1.0.24',
+version = '1.0.25',
 gramps_target_version = '4.2',
 status = STABLE,
 fname = 'ChangeGivenNames.py',
