f3f396cdb6f6213690030e4e402353dee2ace0d9
prculley <paulr2787@gmail.com>
2017-08-11 15:31:29 -0500

    Add French translation

--- BirthOrder/birthorder.gpr.py
+++ BirthOrder/birthorder.gpr.py
@@ -12,7 +12,7 @@ description =  _("Looks through families, looking for children that are not "
                  "correct, edit the birth order manually, accept all those "
                  "families that have all children with proper birth dates, or "
                  "accept all."),
-version = '0.0.2',
+version = '0.0.4',
 gramps_target_version = '4.2',
 status = STABLE,
 fname = 'birthorder.py',
