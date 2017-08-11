1cbea7019fc25eab8970b2acf6015d9098268714
prculley <paulr2787@gmail.com>
2017-08-11 15:31:29 -0500

    Add French translation

---
+++ BirthOrder/birthorder.glade.h
@@ -0,0 +1,16 @@
+char *s = N_("Select the family you wish to examine.");
+char *s = N_("Family ID");
+char *s = N_("Father");
+char *s = N_("Mother");
+char *s = N_("Help");
+char *s = N_("Accept All");
+char *s = N_("This button will sort all of the families listed.");
+char *s = N_("Accept All *");
+char *s = N_("This button will sort all the families with '*'.  In these families, all the children have valid dates, so the sort is unambiguous.");
+char *s = N_("Accept");
+char *s = N_("This button will accept only the family selected and save it as shown in lower right pane.");
+char *s = N_("Done");
+char *s = N_("This button will exit, with remaining families unchanged.");
+char *s = N_("Current sort");
+char *s = N_("Proposed sort");
+char *s = N_("When satisfied with the sort, use 'Accept' button below to accept the changes.  If you wish to adjust the order, select the child you wish to move, and use 'Up' or 'Down' buttons below to move them.");
