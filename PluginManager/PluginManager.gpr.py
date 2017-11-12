c4e2d4e926b5b1aacfc0a0945cc4db6865118271
Sam Manzi <manzi.sam@gmail.com>
2017-11-12 12:18:07 +1100

    [PluginManager]Rebuild-avoid GUI loads during CLI bug9981[gramps50]

--- PluginManager/PluginManager.gpr.py
+++ PluginManager/PluginManager.gpr.py
@@ -29,7 +29,7 @@ id    = 'PluginManager',
 name  = _("Plugin Manager Enhanced"),
 description =  _("An Addon/Plugin Manager with several additional "
                  "capabilities"),
-version = '1.0.4',
+version = '1.0.5',
 gramps_target_version = '5.0',
 fname = "PluginManagerLoad.py",
 authors = ["Paul Culley"],
