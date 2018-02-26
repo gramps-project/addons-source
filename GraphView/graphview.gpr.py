c196b51e09b84027c21308867ebce4611b9d26f7
Paul Culley <paulr2787@gmail.com>
2018-02-25 20:12:00 -0600

    Add View specific icons to four addons[gramps50](#115)
    Different Icons for the various addon charts:
     - GraphView
     - TimelinePedigreeView
     - QuiltView
     - HtreePedigreeView
    
    Previously all third party chart views had the same icon as the builtin PedigreeView making it frustrating to select the correct view each time.

--- GraphView/graphview.gpr.py
+++ GraphView/graphview.gpr.py
@@ -1,3 +1,15 @@
+if locals().get('uistate'):  # don't start GUI if in CLI mode, just ignore
+    from gi.repository import Gtk, GdkPixbuf
+    import os
+    from gramps.gen.const import USER_PLUGINS
+    fname = os.path.join(USER_PLUGINS, 'GraphView',
+                        'gramps-graph.svg')
+    factory = Gtk.IconFactory()
+    pixbuf = GdkPixbuf.Pixbuf.new_from_file(fname)
+    iconset = Gtk.IconSet.new_from_pixbuf(pixbuf)
+    factory.add('gramps-graph', iconset)
+    factory.add_default()
+
 register(VIEW,
      id    = 'graphview',
      name  = _("Graph View"),
@@ -10,4 +22,5 @@ register(VIEW,
      authors = ["Gary Burton"],
      authors_email = ["gary.burton@zen.co.uk"],
      viewclass = 'GraphView',
+     stock_icon = 'gramps-graph',
 )
--- QuiltView/QuiltView.gpr.py
+++ QuiltView/QuiltView.gpr.py
@@ -17,17 +17,29 @@
 # along with this program; if not, write to the Free Software
 # Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
 #
+if locals().get('uistate'):  # don't start GUI if in CLI mode, just ignore
+    from gi.repository import Gtk, GdkPixbuf
+    import os
+    from gramps.gen.const import USER_PLUGINS
+    fname = os.path.join(USER_PLUGINS, 'QuiltView',
+                        'gramps-quilt.svg')
+    factory = Gtk.IconFactory()
+    pixbuf = GdkPixbuf.Pixbuf.new_from_file(fname)
+    iconset = Gtk.IconSet.new_from_pixbuf(pixbuf)
+    factory.add('gramps-quilt', iconset)
+    factory.add_default()
 
 register(VIEW,
-id = 'QuiltView',
-name = _('Quilt Chart'),
-category = ('Ancestry', _('Charts')),
-description =  _('The view shows a quilt chart visualisation of a family tree'),
-version = '1.0.3',
-gramps_target_version = '5.0',
-status = STABLE,
-fname = 'QuiltView.py',
-authors = ['Nick Hall', 'Serge Noiraud'],
-authors_email = ['nick__hall@hotmail.com', 'serge.noiraud@free.fr'],
-viewclass = 'QuiltView',
+    id = 'QuiltView',
+    name = _('Quilt Chart'),
+    category = ('Ancestry', _('Charts')),
+    description =  _('The view shows a quilt chart visualisation of a family tree'),
+    version = '1.0.3',
+    gramps_target_version = '5.0',
+    status = STABLE,
+    fname = 'QuiltView.py',
+    authors = ['Nick Hall', 'Serge Noiraud'],
+    authors_email = ['nick__hall@hotmail.com', 'serge.noiraud@free.fr'],
+    viewclass = 'QuiltView',
+    stock_icon = 'gramps-quilt',
 )
