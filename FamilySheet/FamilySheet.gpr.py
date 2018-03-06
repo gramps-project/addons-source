fa960aa47fb2d6ee8edf1b63d4dc4cf113889e7c
Sam Manzi <manzi.sam@gmail.com>
2018-03-07 08:58:56 +1100

    [FamilySheet]Fix Report exception on missing spouse(#123)[gramps50]rebuild

--- FamilySheet/FamilySheet.gpr.py
+++ FamilySheet/FamilySheet.gpr.py
@@ -3,7 +3,7 @@ register(REPORT,
     name = _('Family Sheet'),
     description = _("Produces a family sheet showing full information "
                     "about a person and his/her partners and children."),
-    version = '3.4.30',
+    version = '3.4.31',
     gramps_target_version = "5.0",
     status = STABLE,
     fname = 'FamilySheet.py',
