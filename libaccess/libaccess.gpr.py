903b6f70fac6cef8d82287272344d5cd4d776de9
Sam Manzi <manzi.sam@gmail.com>
2018-04-27 08:48:57 +1000

    Update or Add initial German translation[gramps50]rebuild

--- libaccess/libaccess.gpr.py
+++ libaccess/libaccess.gpr.py
@@ -31,7 +31,7 @@ id    = 'libaccess',
 name  = "Generic DB Access lib",
 description =  _("Provides a library for generic access to "
                  "the database and gen.lib."),
-version = '1.0.22',
+version = '1.0.23',
 gramps_target_version = "5.0",
 status = STABLE, # not yet tested with python 3
 fname = 'libaccess.py',
