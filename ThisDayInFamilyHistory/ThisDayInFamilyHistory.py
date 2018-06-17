f559f5232df0ffee86b8ba7a37eed334786cbd13
ThariusDawnbreaker <swadams1976@gmail.com>
2018-06-16 22:02:46 -0400

    Initial seed of graphlet source files.

---
+++ ThisDayInFamilyHistory/ThisDayInFamilyHistory.py
@@ -0,0 +1,28 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+# ------------------------------------------------------------
+#
+# This Day In Family History Gramplet
+# File: ThisDayInFamilyHistory.py
+# Author: Stephen Adams
+#
+# This program is free software; you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation; either version 2 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# Initial creation: 2018-05-26
+# Version: 0.1.0
+#
+# Purpose: Generate short reminders of events that have occurred
+# within the current family tree.  Allow flexibility to filter
+# events between living and deceased tree members and to customize
+# the level of detail received.
+#
+# PEP8 check by http://pep8online.com
+# ------------------------------------------------------------
