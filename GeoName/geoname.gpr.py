bb930ea0fc2555b7158f609094214732c985c487
GaryGriffin <genealogy@garygriffin.net>
2018-08-29 18:09:35 -0700

    Initial checkin for GeoName plugin

---
+++ GeoName/geoname.gpr.py
@@ -0,0 +1,44 @@
+#
+# Gramps - a GTK+/GNOME based genealogy program
+#
+# Copyright (C) 2015      Nick Hall
+#
+# This program is free software; you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation; either version 2 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program; if not, write to the Free Software
+# Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+#
+#
+
+#------------------------------------------------------------------------
+#
+# GetGOV Gramplet
+#
+#------------------------------------------------------------------------
+
+register(GRAMPLET,
+         id = "GeoName Gramplet",
+         name = _("GeoName"),
+         description = _("Gramplet to get places from the GeoNames database"),
+         status = STABLE,
+         version = '1.0.0',
+         gramps_target_version = '5.0',
+         fname = "geoname.py",
+         gramplet = 'GeoName',
+         height = 375,
+         detached_width = 510,
+         detached_height = 480,
+         expand = True,
+         gramplet_title = _("GeoName"),
+         help_url="GeoName Gramplet",
+         include_in_listing = True,
+        )
