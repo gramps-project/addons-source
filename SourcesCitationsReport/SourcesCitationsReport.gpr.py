57c29515fbdfbd8ba675b4a77196d93687c4c125
uli22 <uli22@users.noreply.github.com>
2018-08-31 13:37:00 +0200

    new version of Report
    omitted German texts, user defined citation date instead of "anno", new gpr.py file

--- SourcesCitationsReport/SourcesCitationsReport.gpr.py
+++ SourcesCitationsReport/SourcesCitationsReport.gpr.py
@@ -4,19 +4,19 @@
 #
 #------------------------------------------------------------------------
 
-register(REPORT,
-    id    = 'SourcesCitationsReport',
-    name  = _("Sources and Citations Report"),
-    description =  _("Provides a source and Citations with notes"),
-    version = '1.0.3',
-    gramps_target_version = "5.1",
-    status = STABLE,
-    fname = 'SourcesCitationsReport.py',
-    authors = ["Uli22"],
-    authors_email = ["hansulrich.frink@gmail.com"],
-    category = CATEGORY_TEXT,
-    reportclass = 'SourcesCitationsReport',
-    optionclass = 'SourcesCitationsOptions',
-    report_modes = [REPORT_MODE_GUI, REPORT_MODE_BKI, REPORT_MODE_CLI],
-    require_active = False
-    )
+plg = newplugin()
+plg.id    = 'SourcesCitationsReport'
+plg.name  = _("Sources and Citations Report")
+plg.description =  _("Provides a source and Citations Report with notes")
+plg.version = '3.6.0'
+plg.gramps_target_version = '5.0'
+plg.status = STABLE
+plg.fname = 'SourcesCitationsReport.py'
+plg.ptype = REPORT
+plg.authors = ["Uli22"]
+plg.authors_email = ["hansulrich.frink@gmail.com"]
+plg.category = CATEGORY_TEXT
+plg.reportclass = 'SourcesCitationsReport'
+plg.optionclass = 'SourcesCitationsOptions'
+plg.report_modes = [REPORT_MODE_GUI, REPORT_MODE_BKI, REPORT_MODE_CLI]
+plg.require_active = False
