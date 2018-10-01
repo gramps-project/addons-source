cfff447cc2e757e5c8cee0e8e8f1daf05ff30c1c
prculley <paulr2787@gmail.com>
2018-09-30 19:20:08 -0500

    PlaceCleanup, a new addon

---
+++ PlaceCleanup/placecleanup.glade.h
@@ -0,0 +1,72 @@
+char *s = N_("City, County, State, Country");
+char *s = N_("Primary Name");
+char *s = N_("Postal Code");
+char *s = N_("Type");
+char *s = N_("The current Primary name.");
+char *s = N_("The postal code for the place, if any.");
+char *s = N_("What type of place this is. Eg 'Country', 'City', ... .");
+char *s = N_("Orig");
+char *s = N_("Check this box if you want to keep the original Postal code.");
+char *s = N_("Check this box if you want to keep the original Place Type.");
+char *s = N_("Longitude");
+char *s = N_("Latitude");
+char *s = N_("Keep Original");
+char *s = N_("Check this box if you want to keep the original Latitude and Longitude.");
+char *s = N_("The Gramps ID..");
+char *s = N_("ID");
+char *s = N_("Check this box if you want to keep the original Gramps ID.");
+char *s = N_("Alterative Names\n"
+             "Select one or more rows and press 'Keep' button to toggle the 'include into place' status .");
+char *s = N_("Inc");
+char *s = N_("Name");
+char *s = N_("Lang");
+char *s = N_("Date");
+char *s = N_("Keep");
+char *s = N_("Select one or more rows and use this button to keep the alternative names.");
+char *s = N_("Primary");
+char *s = N_("Select one row and use this button to make the row the primary name.");
+char *s = N_("Discard");
+char *s = N_("Select one or more rows and use this button to discard the alternative names.");
+char *s = N_("Cancel the operation, no changes are made to your Place.");
+char *s = N_("Store the values in your Place.");
+char *s = N_("Ok");
+char *s = N_("Help");
+char *s = N_("Keep Web Links");
+char *s = N_("Puts web links (typically wikipedia) into your place.");
+char *s = N_("Add citation and source to Place");
+char *s = N_("This Adds a Citation to your place, citing GeoNames.");
+char *s = N_("GeoNames User ID");
+char *s = N_("Enter you Geonames user ID here.");
+char *s = N_("Alternative Names Languages to keep");
+char *s = N_("Enter the two or three letter codes for the languages you wish to keep, seperated by spaces.\n"
+             "For example \"en fr ru\".\n"
+             "These languages will initially be checked off on the Names result display.\n"
+             "If you often see an un-checked name in a specific language you want, add the language code here.");
+char *s = N_("Currently Enclosed Places");
+char *s = N_("When cleaning a place that is already enclosed, this affects what is done.\n"
+             "If '<b>Keep current Enclosure</b>' is selected, no changes to the enclosing place is made.\n"
+             "If '<b>Replace with GeoNames Enclosure</b>' is selected, the enclosed place is changed to the next level GeoNames place.  This may result in some unused places remaining in your database.");
+char *s = N_("Keep current Enclosure");
+char *s = N_("Replace with GeoNames Enclosure");
+char *s = N_("Title");
+char *s = N_("Current Place title.\n"
+             "You can edit this to help find other places, particularly to reduce the number of results.");
+char *s = N_("Find");
+char *s = N_("Search you local places for a match.");
+char *s = N_("Select");
+char *s = N_("Continue to merge (for local places), or to complete a place (for GeoNames).");
+char *s = N_("Edit");
+char *s = N_("Split up Title, Edit manually, and enclose with next level of hierarchy.  For use when the initial component of the Title is too detailed to be found in the gazetteer.");
+char *s = N_("Next Place");
+char *s = N_("Find an incomplete place.");
+char *s = N_("Set preferences");
+char *s = N_("Select one of these places to continue the cleanup process.");
+char *s = N_("Search result places");
+char *s = N_("No\n"
+             "Matches");
+char *s = N_("Checking the 'Populated' checkbox causes the GeoNames search to include 'Populted places', places where people live and work.");
+char *s = N_("Populated");
+char *s = N_("Checking the 'Admin' checkbox cause the GeoNames search to include adminstrative subdivisions in the search (countries, states, counties etc.).");
+char *s = N_("Checking the 'Spot' checkbox cause the GeoNames search to include a wide variety of other places in the search (churches, burial grounds, buildings etc.).");
+char *s = N_("Admin");
+char *s = N_("Spot");
