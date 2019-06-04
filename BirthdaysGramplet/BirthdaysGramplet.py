4a8037832675e37a48aee73f2a01a95e0a90cb31
Mattkmmr <49940207+Mattkmmr@users.noreply.github.com>
2019-06-04 23:17:49 +0200

    Revert "Add ignore birthday functionality to [BirthdayGramplet]"
    This reverts commit 269d9befd1f8ea028323e96a32af8cb52b215d43.

--- BirthdaysGramplet/BirthdaysGramplet.py
+++ BirthdaysGramplet/BirthdaysGramplet.py
@@ -1,8 +1,6 @@
-#
 # Gramps - a GTK+/GNOME based genealogy program
 #
 # Copyright (C) 2010  Peter Potrowl <peter017@gmail.com>
-# Copyright (C) 2019  Matthias Kemmer <matt.familienforschung@gmail.com>
 #
 # This program is free software; you can redistribute it and/or modify
 # it under the terms of the GNU General Public License as published by
@@ -17,125 +15,68 @@
 # You should have received a copy of the GNU General Public License
 # along with this program; if not, write to the Free Software
 # Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
-#
 
 from gramps.gen.plug import Gramplet
 from gramps.gen.const import GRAMPS_LOCALE as glocale
-from gramps.gen.display.name import displayer as name_displayer
-from gramps.gen.lib.date import Today, Date
-import gramps.gen.datehandler
-from gramps.gen.config import config
-from gramps.gen.plug.menu import EnumeratedListOption
 try:
     _trans = glocale.get_addon_translator(__file__)
 except ValueError:
     _trans = glocale.translation
 _ = _trans.gettext
 
+from gramps.gen.display.name import displayer as name_displayer
+from gramps.gen.lib.date import Today, Date
+import gramps.gen.datehandler
+import time
+import datetime
+import posixpath
+from gramps.gen.config import config
+
+"""
+This Gramplet displays the incoming birthdays.
+"""
 
 class BirthdaysGramplet(Gramplet):
     def init(self):
         self.set_text(_("No Family Tree loaded."))
         self.max_age = config.get('behavior.max-age-prob-alive')
 
-    def build_options(self):
-        """Build the configuration options"""
-        db = self.dbstate.db
-
-        name = _("Ignore birthdays with tag")
-        self.option = EnumeratedListOption(name, self.ignore_tag)
-
-        self.option.add_item('', '')  # No ignore tag
-        if db.is_open():
-            for tag_handle in db.get_tag_handles(sort_handles=True):
-                tag = db.get_tag_from_handle(tag_handle)
-                tag_name = tag.get_name()
-                self.option.add_item(tag_name, tag_name)
-
-        self.add_option(self.option)
-
-    def save_options(self):
-        """Save gramplet configuration data"""
-        self.ignore_tag = self.option.get_value()
-
-    def save_update_options(self, obj):
-        """Save a gramplet's options to file"""
-        self.save_options()
-        self.gui.data = [self.ignore_tag]
-        self.update()
-
-    def on_load(self):
-        """Load stored configuration data"""
-        if len(self.gui.data) == 1:
-            self.ignore_tag = self.gui.data[0]
-        else:
-            self.ignore_tag = ''
-
     def db_changed(self):
-        """Update gramplet when database was changed"""
         self.connect(self.dbstate.db, 'person-add', self.update)
         self.connect(self.dbstate.db, 'person-delete', self.update)
         self.connect(self.dbstate.db, 'person-update', self.update)
 
     def main(self):
-        """Main function of the Birthdays gramplet"""
         self.set_text(_("Processing..."))
         database = self.dbstate.db
         personList = database.iter_people()
-        self.result = []
-
-        # get ignore tag handle
-        tag_name = self.option.get_value()
-        tag_handles = database.get_tag_handles()
-        for handle in tag_handles:
-            tag = database.get_tag_from_handle(handle)
-            if tag_name == tag.get_name():
-                ignore_tag_handle = tag.get_handle()
-
-        for cnt, person in enumerate(personList):
-            person_tag_handles = person.get_tag_list()
-            if ignore_tag_handle in person_tag_handles:
-                pass  # ignore person
-            else:  # calculate age and days until birthday:
-                self.__calculate(database, person)
-
-        # Reverse sort on number of days from now:
-        self.result.sort(key=lambda item: -item[0])
-        self.clear_text()
-
-        # handle text shown in gramplet
-        for diff, age, date, person in self.result:
-            name = person.get_primary_name()
-            displayer = gramps.gen.datehandler.displayer
-            self.append_text("{}: ".format(displayer.display(date)))
-            self.link(name_displayer.display_name(name), "Person",
-                      person.handle)
-            self.append_text(" ({})\n".format(age))
-        self.append_text("", scroll_to="begin")
-
-    def __calculate(self, database, person):
-        """Calculate the age and days until birthday"""
+        result = []
+        text = ''
         today = Today()
-        birth_ref = person.get_birth_ref()
-        death_ref = person.get_death_ref()
-        if (birth_ref and not death_ref):
-            birth = database.get_event_from_handle(birth_ref.ref)
-            birth_date = birth.get_date_object()
-            if birth_date.is_regular():
-                birthday_this_year = Date(today.get_year(),
-                                          birth_date.get_month(),
-                                          birth_date.get_day())
-                next_age = birthday_this_year - birth_date
-                # (0 year, months, days) between now and birthday of this
-                # year (positive if passed):
-                diff = today - birthday_this_year
-                # about number of days the diff is:
-                diff_days = diff[1] * 30 + diff[2]
-                if next_age[0] < self.max_age:
-                    if diff_days <= 0:  # not yet passed
-                        self.result.append((diff_days, next_age, birth_date,
-                                           person))
-                    else:  # passed; add it for next year's birthday
-                        self.result.append((diff_days - 365,
-                                            next_age[0] + 1,
-                                            birth_date, person))
+        for cnt, person in enumerate(personList):
+            birth_ref = person.get_birth_ref()
+            death_ref = person.get_death_ref()
+            if (birth_ref and not death_ref):
+                birth = database.get_event_from_handle(birth_ref.ref)
+                birth_date = birth.get_date_object()
+                if birth_date.is_regular():
+                    birthday_this_year = Date(today.get_year(), birth_date.get_month(), birth_date.get_day())
+                    next_age = birthday_this_year - birth_date
+                    # (0 year, months, days) between now and birthday of this year (positive if passed):
+                    diff = today - birthday_this_year
+                    # about number of days the diff is:
+                    diff_days = diff[1] * 30 + diff[2]
+                    if next_age[0] < self.max_age:
+                        if diff_days <= 0: #not yet passed
+                            result.append((diff_days, next_age, birth_date, person))
+                        else: #passed; add it for next year's birthday
+                            result.append((diff_days - 365, next_age[0] + 1, birth_date, person))
+        # Reverse sort on number of days from now:
+        result.sort(key=lambda item: -item[0])
+        self.clear_text()
+        for diff, age, date, person in result:
+            name = person.get_primary_name()
+            self.append_text("%s: " % gramps.gen.datehandler.displayer.display(date))
+            self.link(name_displayer.display_name(name), "Person", person.handle)
+            self.append_text(" (%s)\n" % age)
+        self.append_text("", scroll_to="begin")
