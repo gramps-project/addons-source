4aee0448e0ebf4b2f5bb15fd68c9c34bc985cba6
Sam Manzi <manzi.sam@gmail.com>
2019-08-26 15:00:13 +1000

    [PreReq]Update Gramps version[gramps50]

--- PrerequisitesCheckerGramplet/PrerequisitesCheckerGramplet.py
+++ PrerequisitesCheckerGramplet/PrerequisitesCheckerGramplet.py
@@ -245,8 +245,8 @@ class PrerequisitesCheckerGramplet(Gramplet):
         '''
         self.append_text("\n")
         # Start check
-        LATEST_GRAMPS_VERSION = (5, 0, 1)
-        LATEST_GRAMPS_DATE = "2018-12-20"
+        LATEST_GRAMPS_VERSION = (5, 1, 0)
+        LATEST_GRAMPS_DATE = "2019-08-20"
         latest_release_message = ("Gramps " + verstr(LATEST_GRAMPS_VERSION) +
                                   "  released " + LATEST_GRAMPS_DATE +
                                   " is the most current version.\n")
