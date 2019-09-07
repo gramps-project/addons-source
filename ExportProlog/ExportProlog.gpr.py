123dbb819d03fc67e762cb0964e48abf75293cff
prculley <paulr2787@gmail.com>
2019-09-07 16:27:08 -0500

    Fix crash when exporting Prolog format
    Issue #11316

--- ExportProlog/ExportProlog.gpr.py
+++ ExportProlog/ExportProlog.gpr.py
@@ -1,7 +1,8 @@
 register(EXPORT,
          id    = 'Prolog Export',
          name  = _('Prolog Export'),
-         description =  _('Exports data into a Prolog fact format'),
+         description =  _('Exports data into a Prolog fact format.  '
+                          'Data is in Unicode (utf-8)'),
          version = '1.0.9',
          gramps_target_version = "5.1",
          status = STABLE,
