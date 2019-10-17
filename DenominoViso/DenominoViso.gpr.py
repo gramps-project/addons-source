78293c42e232814c3a973ed86a9663a90cb79800
Sam Manzi <manzi.sam@gmail.com>
2019-10-17 16:38:37 +1100

    [DenominoViso]Converted to standard HTML[v51] Converted addon to use straight HTML5, XHTML looked like it was going to replace HTML, but pretty much is unused and HTML5 is the standard. * Stop addon declaring it is a Dutch (nl) web page * Change default file name extension (*.html) * Fix some spelling

--- DenominoViso/DenominoViso.gpr.py
+++ DenominoViso/DenominoViso.gpr.py
@@ -7,17 +7,17 @@ register(REPORT,
     id = 'denominoviso',
     name = _('DenominoViso'),
     category = CATEGORY_WEB,
-    status = STABLE, # number of arguments and widget
+    status = STABLE,
     fname = 'DenominoViso.py',
     reportclass = 'DenominoVisoReport',
     optionclass = 'DenominoVisoOptions',
     report_modes = [REPORT_MODE_GUI, REPORT_MODE_CLI],
     authors = ['M.D. Nauta'],
     authors_email = ['m.d.nauta@hetnet.nl'],
-    description = _('Generates a web (XHTML) page with a graphical '
+    description = _('Generates a web (HTML) page with a graphical '
                     'representation of ancestors/descendants (SVG) '
                     'where details about individuals become visible '
                     'upon mouse-events.'),
-    version = '2.3.38',
+    version = '2.3.39',
     gramps_target_version = "5.1",
 )
