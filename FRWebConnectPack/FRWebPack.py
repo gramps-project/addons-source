604e1bec6292c4d26a39d32323612fbe0c18f169
romjerome <romjerome@users.noreply.github.com>
2019-11-27 17:30:44 +0100

    Update FRWebPack.py (#258)
    * Update FRWebPack.py
    
    * update doc (widget for 'culture.fr' web site)
    
    * Add Belgian's Archives
    
    * filtering by french language
    
    * revert one filter for french language
    
    * add retronews
    
    newspapers from Bnf (French National library)
    
    * typo
    
    * add 'Filae' search form
    
    * country or lang mark on translation strings

--- FRWebConnectPack/FRWebPack.py
+++ FRWebConnectPack/FRWebPack.py
@@ -33,26 +33,30 @@ _ = _trans.gettext
 
 # http://www.gramps-project.org/wiki/index.php?title=Resource_and_related_sites#French_resources
 #
+# http://www.culture.fr/widgets/genealogie/1/0/bigenet&amp;ecfa&amp;mayenne&amp;vendome&amp;leonore&amp;ain&amp;memoire_des_hommes&amp;vendee
 # http://www.bigenet.org/index.php ?
 # http://www.cybergenealogie.fr/cailgeo/us/screch.php
 
 WEBSITES = [
-    ["Person", "Ministère de la culture", _("French cultural ministry"), "http://www.culture.fr/genealogie/?action_type=search&lang=fr&search_nom=%(surname)s&search_prenom=%(given)s"],
-    ["Person", "Geneabank", _("Genealogical bank"), "http://gbkcouples.geneabank.org/nom/?name=%(surname)s&place=&start=&end=&source=gbk*"],
+    ["Person", "Ministère de la culture", _("Cultural ministry (France)"), "http://www.culture.fr/bases_genealogie/resultats?lastname=%(surname)s&firstname=%(given)s&search=Lancer+la+recherche&bases[]=bigenet&bases[]=ecfa&bases[]=mayenne&bases[]=vendome&bases[]=leonore&bases[]=ain&bases[]=memoire_des_hommes&bases[]=vendee"],
+    ["Person", "Geneabank", _("Genealogical bank"), "https://gbkcouples.geneabank.org/nom/?name=%(surname)s&place=&start=&end=&source=gbk*"],
     #["Person", "Centre Départemental d'Histoire des Familles (CDHF)", _("Historical Families Center Departement (CDHF)"), "http://www.cdhf.net/fr/index.php?t=bases&d=bases%2Fmoteurpat&c=moteurpat&f=selection&p=&order=&order2=&motcle=&patronyme=%(surname)s"],
-    ["Person", "Fichier Origine", _("OrigineFile (Quebec)"), "http://www.fichierorigine.com/recherche.php?nom=%(surname)s"],
-    ["Person", "Geneanet", "Geneanet", "http://search.geneanet.org/result.php?lang=fr&name=%(surname)s"],
-    ["Person", "Geneanet-Favrejhas", "Geneanet, Favrejhas", "http://gw1.geneanet.org/index.php3?b=favrejhas&m=NG&n=%(surname)s&t=N&x=0&y=0"],
+    ["Person", "Fichier Origine", _("OrigineFile (Quebec)"), "https://www.fichierorigine.com/recherche?nom=%(surname)s"],
+    ["Person", "Geneanet", "Geneanet", "https://search.geneanet.org/result.php?lang=fr&name=%(surname)s"],
+    ["Person", "Filae", "Filae", "https://www.filae.com/v4/genealogie/Search.mvc/SearchForm?ln=%(surname)s&fn=%(given)s"],
+    #["Person", "Geneanet-Favrejhas", "Geneanet, Favrejhas", "http://gw1.geneanet.org/index.php3?b=favrejhas&m=NG&n=%(surname)s&t=N&x=0&y=0"],
     ["Person", "Roglo", "Roglo", "http://roglo.eu/roglo?m=NG&n=%(given)s+%(surname)s&t=PN"],
-    ["Person", "FamilySearch", _("FamilySearch.org"), "https://familysearch.org/search/record/results?count=20&query=%%2Bgivenname%%3A%(given)s~ %%2Bsurname%%3A%(surname)s~ %%2Bbirth_year%%3A%(birth)s-%(birth)s~ %%2Bdeath_year%%3A%(death)s-%(death)s~"],
-    ["Person", "CAILGEO", _("Cyber genealogy"), "http://www.cybergenealogie.fr/cailgeo/fr/cail01n.php?nomr=%(surname)s"],
-    ["Person", "Gallica", "Gallica", '''http://gallica.bnf.fr/Search?ArianeWireIndex=index&p=1&lang=FR&q="%(surname)s,+%(given)s"'''],
-    ["Person", "Archive.org", "Archive.org", '''http://www.archive.org/search.php?query="%(surname)s,+%(given)s"'''],
-    ["Person", "GeneaBook", "GeneaBook", "http://www.geneabook.org/genealogie/1/ouvrages.php?nom=%(surname)s&x=20&y=1"],
+    #["Person", "FamilySearch", _("FamilySearch.org"), "https://familysearch.org/search/record/results?count=20&query=%%2Bgivenname%%3A%(given)s~ %%2Bsurname%%3A%(surname)s~ %%2Bbirth_year%%3A%(birth)s-%(birth)s~ %%2Bdeath_year%%3A%(death)s-%(death)s~"],
+    ["Person", "CAILGEO", _("Cyber genealogy"), "https://www.cybergenealogie.fr/cailgeo/fr/cail01n.php?nomr=%(surname)s"],
+    ["Person", "Gallica", "Gallica", '''https://gallica.bnf.fr/Search?ArianeWireIndex=index&p=1&lang=FR&q="%(surname)s,+%(given)s"'''],
+    ["Person", "Retronews", _("RetroNews (Bnf)"), "https://www.retronews.fr/search#allTerms=%(surname)s&sort=score&page=1&searchIn=all"],
+    ["Person", "GeneaBook", "GeneaBook", "https://www.geneanet.org/fonds/bibliotheque/?&nom=%(surname)s&prenom=&q=%(given)s&go=1"],
     #["Person", "Google Archives", _("Google Archives"), "http://news.google.fr/archivesearch?q=%(surname)s&btnGt=Afficher+la+chronologie"],
     #["Person", "FR-Google", _("FR Google"), u'''http://www.google.fr/#hl=fr&q="%(surname)s,+%(given)s"'''],
-    ["Person", "Hathi Trust Digital Library", _("Hathi Trust Digital Library"), "http://babel.hathitrust.org/cgi/ls?q1=%(surname)s+%(given)s+&lmt=ft&a=srchls"],
-    ["Person", "Open Library", _("Open Library"), "http://openlibrary.org/search?q=%(surname)s, %(given)s"],
+    ["Person", "Archive.org", "Archive.org", '''https://www.archive.org/search.php?query="%(surname)s+%(given)s"&sin=TXT'''],
+    ["Person", "Hathi Trust Digital Library", _("Hathi Trust Digital Library (fr)"), '''https://babel.hathitrust.org/cgi/ls?q1=%(surname)s+%(given)s+&lmt=ft&a=srchls&facet=language008_full:"French"'''],
+    ["Person", "Open Library", _("Open Library (fr)"), '''https://openlibrary.org/search?q="%(surname)s, %(given)s"&language=fre'''],
+    ["Person", "Archives de Belgique", _("Belgium's Archives"),"https://search.arch.be/fr/rechercher-des-personnes/resultats/q/zoekwijze/s?text=%(surname)s"],
     #["Place", "Geneanet", "Geneanet", "http://search.geneanet.org/result.php?lang=fr&place=%(city)s"],
     #["Place", "Centre Départemental d'Histoire des Familles (CDHF)", _("Historical Families Center Departement (CDHF)"), "http://www.cdhf.net/fr/index.php?t=villages&d=villages&c=villages&f=results&p=&page=&lieu2=%(city)s"],
     #["Place", "Fichier Origine", _("OrigineFile (Quebec)"), "http://www.fichierorigine.com/recherche.php?commune=%(city)s&pays=%(country)s"],
