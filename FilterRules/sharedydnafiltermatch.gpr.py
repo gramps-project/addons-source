590db69cd218c8455612c38d620965015015e281
Mattkmmr <49940207+Mattkmmr@users.noreply.github.com>
2020-04-08 18:51:11 +0200

    New filter rule [SharedYDNAFilterMatch]

---
+++ FilterRules/sharedydnafiltermatch.gpr.py
@@ -0,0 +1,34 @@
+#
+# Gramps - a GTK+/GNOME based genealogy program
+#
+# Copyright (C) 2020    Matthias Kemmer
+#
+# This program is free software; you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation; either version 2 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program; if not, write to the Free Software
+# Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
+#
+"""Filter rule matching people sharing same yDNA with filter match."""
+
+register(RULE,
+  id = 'sharedydnafiltermatch',
+  name = _("Descendants of <filter> sharing yDNA with each other"),
+  description = _("Matches descendants of a <filter> sharing yDNA"),
+  version = '1.0.0',
+  authors = ["Matthias Kemmer"],
+  authors_email = ["matt.familienforschung@gmail.com"],
+  gramps_target_version = '5.1',
+  status = STABLE,
+  fname = "sharedydnafiltermatch.py",
+  ruleclass = 'SharedYDNAFilterMatch',  # must be rule class name
+  namespace = 'Person',  # one of the primary object classes
+  )
---
+++ FilterRules/sharedydnafiltermatch.py
@@ -0,0 +1,116 @@
+#
+# Gramps - a GTK+/GNOME based genealogy program
+#
+# Copyright (C) 2020    Matthias Kemmer
+#
+# This program is free software; you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation; either version 2 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program; if not, write to the Free Software
+# Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
+#
+"""Filter rule matching people sharing same yDNA with filter match."""
+
+# -------------------------------------------------------------------------
+#
+# Gramps modules
+#
+# -------------------------------------------------------------------------
+from gramps.gen.filters.rules._rule import Rule
+from gramps.gen.filters.rules.person._matchesfilter import MatchesFilter
+from gramps.gen.const import GRAMPS_LOCALE as glocale
+try:
+    _trans = glocale.get_addon_translator(__file__)
+except ValueError:
+    _trans = glocale.translation
+_ = _trans.gettext
+
+
+# -------------------------------------------------------------------------
+#
+# SharedYDNA
+#
+# -------------------------------------------------------------------------
+class SharedYDNAFilterMatch(Rule):
+    """Filter rule matching people sharing same yDNA with filter match."""
+
+    labels = [_('Filter name:')]
+    name = _("Descendants of <filter> sharing yDNA with each other")
+    description = _("Matches descendants of a <filter> sharing yDNA")
+    category = _("General filters")
+
+    def prepare(self, db, user):
+        """Prepare a reference list for the filter."""
+        self.db = db
+        self.persons = set()
+        self.current_children = set()
+        self.next_children = set()
+
+        self.matchfilt = MatchesFilter(self.list)
+        self.matchfilt.requestprepare(db, user)
+        for person in db.iter_people():
+            if self.matchfilt.apply(db, person) and person.get_gender() == 1:
+                self.root_father = person
+                self.setup()
+                self.get_male_desc()
+
+    def setup(self):
+        """Get the first round of male descendants."""
+        self.persons.add(self.root_father.get_handle())
+        children = self.get_male_children(self.root_father)
+        if children:
+            for child_handle in children:
+                self.current_children.update(children)
+        else:
+            return False
+
+    def get_male_desc(self):
+        """Get all male descendant lines of root father recursively."""
+        if self.current_children and (False not in self.current_children):
+            for child_handle in self.current_children:
+                if child_handle not in self.persons:
+                    self.persons.add(child_handle)
+                    child = self.db.get_person_from_handle(child_handle)
+                    has_children = self.get_male_children(child)
+                    if has_children:
+                        self.next_children.update(has_children)
+            self.current_children.clear()
+            self.get_male_desc()
+        elif len(self.current_children) == 0 and len(self.next_children) > 0:
+            self.current_children.update(self.next_children)
+            self.next_children.clear()
+            self.get_male_desc()
+
+    def get_male_children(self, person):
+        """
+        Get all male children of a person.
+
+        This function returns a set of male children if a person has male
+        children and their relationship type to the father is birth.
+        """
+        male_children = set()
+        family_list = person.get_family_handle_list()
+
+        if len(family_list) == 0:
+            return False
+
+        for family_handle in family_list:
+            family = self.db.get_family_from_handle(family_handle)
+            for child_ref in family.get_child_ref_list():
+                child = self.db.get_person_from_handle(child_ref.ref)
+                if (child_ref.get_father_relation() == _("Birth") and
+                        child.get_gender() == 1):
+                    male_children.add(child_ref.ref)
+        return male_children
+
+    def apply(self, db, obj):
+        """Check if the filter applies to a person."""
+        return obj.get_handle() in self.persons
