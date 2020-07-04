c7c28705673acd2f3501b47f98f7c391a1fd517f
prculley <paulr2787@gmail.com>
2020-07-04 11:56:21 -0500

    Rebuild D3charts for bugs, CombinedView and DynamicWeb for translations

--- D3Charts/AncestralFanChart.gpr.py
+++ D3Charts/AncestralFanChart.gpr.py
@@ -32,6 +32,6 @@ register(REPORT,
                     'representation of ancestors (SVG) '
                     'represented as a Fan Chart from the D3.js '
                     'JavaScript library.'),
-    version = '1.0.27',
+    version = '1.0.28',
     gramps_target_version = "5.1",
 )
