0ea4423f2bfc22b7c2b6ddcff4f5563c21134abf
Jan Sparreboom <jan@sparreboom.net>
2020-07-17 18:35:36 +0200

    Removed old internationalisation lines.

--- Differences/differences.py
+++ Differences/differences.py
@@ -35,9 +35,7 @@ import os
 # GRAMPS modules
 #
 #------------------------------------------------------------------------
-#from gramps.gen.const import GRAMPS_LOCALE as glocale
-#_ = glocale.translation.gettext
-#ngettext = glocale.translation.ngettext
+#
 from gramps.gen.display.name import displayer as global_name_display
 from gramps.gen.plug.docgen import (FontStyle, ParagraphStyle, GraphicsStyle,
                              FONT_SERIF, PARA_ALIGN_RIGHT,
