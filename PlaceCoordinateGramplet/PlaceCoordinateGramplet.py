a0621654ca80d251760525a15da070299adb3217
Christian Schulze <c.w.schulze@gmail.com>
2020-08-02 18:23:36 +0200

    load geopy, move register call to one file
    use geopy
    
    align version numbers
    
    enable calling gpr content with exec
    
    build version
    
    remerge files...
    
    build version
    
    update copyright

--- PlaceCoordinateGramplet/PlaceCoordinateGramplet.py
+++ PlaceCoordinateGramplet/PlaceCoordinateGramplet.py
@@ -1,6 +1,6 @@
 # Gramps - a GTK+/GNOME based genealogy program
 #
-# Copyright (C) 2015       Christian Schulze
+# Copyright (C) 2020 Christian Schulze
 #
 # This program is free software; you can redistribute it and/or modify
 # it under the terms of the GNU General Public License as published by
