234f1e7d6c38ce24ed39bffcf329745782cc5ea2
Christian Schulze <c.w.schulze@gmail.com>
2020-09-04 13:02:30 +0200

    PlaceCoordinateGramplet: do not show warning every second time   Do not check connectivity.

--- PlaceCoordinateGramplet/PlaceCoordinateGramplet.gpr.py
+++ PlaceCoordinateGramplet/PlaceCoordinateGramplet.gpr.py
@@ -24,6 +24,11 @@
 #
 #------------------------------------------------------------------------
 
+import os
+import sys
+import importlib
+import traceback
+
 from gi import Repository
 from gramps.gen.const import USER_PLUGINS
 from gramps.gen.config import logging
@@ -33,6 +38,13 @@ inifile = config.register_manager("placecoordinategramplet_warn")
 inifile.load()
 sects = inifile.get_sections()
 
+from gramps.gen.const import GRAMPS_LOCALE as glocale
+try:
+    _trans = glocale.get_addon_translator(os.path.join(USER_PLUGINS, 'PlaceCoordinateGramplet', 'PlaceCoordinateGramplet.gpr.py'))
+except ValueError:
+    _trans = glocale.translation
+_ = _trans.gettext
+
 #-------------------------------------------------------------------------
 #
 # set up logging
@@ -41,17 +53,12 @@ sects = inifile.get_sections()
 _LOG = logging.getLogger("PlaceCoordinateGramplet")
 
 
-import os
-import sys
-import importlib
-import traceback
-
 # we should always try to see if prerequisites are there, in case they
 # appear at a later time than first run.  But we still don't need to do
 # this if there is no GUI, as Gramplet and view are not necessary.
 _uistate = locals().get('uistate')
 some_import_error = False
-while _uistate:
+if _uistate:
     plugin_name = _("Place Coordinate Gramplet")
     try:
         import gi
@@ -71,7 +78,7 @@ while _uistate:
             warn_dialog = QuestionDialog2(
                 plugin_name + ' Plugin',
                 message,
-                "Don't show again", "OK",
+                _("Don't show again"), _("OK"),
                 parent=_uistate.window)
             if warn_dialog.run():
                 inifile.register(
@@ -80,41 +87,43 @@ while _uistate:
                     'placecoordinategramplet_warn.missingmodules', "False")
                 inifile.save()
         some_import_error = True
-        break
+    #    break
 
-    # now test to make sure that we can actually perform a search.  If there
-    # is a problem with connectivity this will fail
-    try:
-        location_ = GeocodeGlib.Forward.new_for_string("Berlin")
-        result = location_.search()
-        break
-    except Exception as e:
-        result = None
+    # # now test to make sure that we can actually perform a search.  If there
+    # # is a problem with connectivity this will fail
+    # try:
+    #     location_ = GeocodeGlib.Forward.new_for_string("Berlin")
+    #     result = location_.search()
+    #     inifile.register('placecoordinategramplet_warn.connectivity', "")
+    #     inifile.set('placecoordinategramplet_warn.connectivity', "True")
+    #     break
+    # except Exception as e:
+    #     result = None
 
-        if('placecoordinategramplet_warn' not in sects or not
-           inifile.is_set('placecoordinategramplet_warn.connectivity') or
-           inifile.get('placecoordinategramplet_warn.connectivity') !=
-           'False'):
-            message = _(
-                "Internet connectivity test failed for {module_name}.").format(
-                    module_name="gi.repository.GeocodeGlib") + "\n\n" + str(e)
-            logging.warning(plugin_name + ': ' + message)
+    #     if('placecoordinategramplet_warn' not in sects or not
+    #        inifile.is_set('placecoordinategramplet_warn.connectivity') or
+    #        inifile.get('placecoordinategramplet_warn.connectivity') !=
+    #        'False'):
+    #         message = _(
+    #             "Internet connectivity test failed for {module_name}.").format(
+    #                 module_name="gi.repository.GeocodeGlib") + "\n\n" + str(e)
+    #         logging.warning(plugin_name + ': ' + message)
 
-            from gramps.gui.dialog import QuestionDialog2
-            warn_dialog = QuestionDialog2(
-                plugin_name + ' Plugin',
-                message,
-                "Don't show again", "OK",
-                parent=_uistate.window)
-            if warn_dialog.run():
-                logging.warning(plugin_name + ': ' + _('Warning disabled.'))
-                inifile.register(
-                    'placecoordinategramplet_warn.connectivity', "")
-                inifile.set(
-                    'placecoordinategramplet_warn.connectivity', "False")
-                inifile.save()
-        some_import_error = True
-        break
+    #         from gramps.gui.dialog import QuestionDialog2
+    #         warn_dialog = QuestionDialog2(
+    #             plugin_name + ' Plugin',
+    #             message,
+    #             "Don't show again", "OK",
+    #             parent=_uistate.window)
+    #         if warn_dialog.run():
+    #             logging.warning(plugin_name + ': ' + _('Warning disabled.'))
+    #             inifile.register(
+    #                 'placecoordinategramplet_warn.connectivity', "")
+    #             inifile.set(
+    #                 'placecoordinategramplet_warn.connectivity', "False")
+    #             inifile.save()
+    #     some_import_error = True
+    #     break
 
 if locals().get('uistate') is None or not some_import_error:
     # Right after the download the plugin is loaded without uistate
@@ -128,7 +137,7 @@ if locals().get('uistate') is None or not some_import_error:
             id='geoIDplaceCoordinateGramplet',
             name=_("Place Coordinate Gramplet view"),
             description=_("View for the place coordinate gramplet."),
-            version = '1.1.13',
+            version = '1.1.14',
             gramps_target_version="5.1",
             status=STABLE,
             fname='PlaceCoordinateGeoView.py',
@@ -145,7 +154,7 @@ if locals().get('uistate') is None or not some_import_error:
             name=_("Place and Coordinates"),
             description=_(
                 "Gramplet that simplifies setting the coordinates of a place"),
-            version = '1.1.13',
+            version = '1.1.14',
             gramps_target_version="5.1",
             status=STABLE,
             fname="PlaceCoordinateGramplet.py",
@@ -161,6 +170,4 @@ if locals().get('uistate') is None or not some_import_error:
 if not some_import_error:
     inifile.register('placecoordinategramplet_warn.missingmodules', "")
     inifile.set('placecoordinategramplet_warn.missingmodules', "True")
-    inifile.register('placecoordinategramplet_warn.connectivity', "")
-    inifile.set('placecoordinategramplet_warn.connectivity', "True")
     inifile.save()
