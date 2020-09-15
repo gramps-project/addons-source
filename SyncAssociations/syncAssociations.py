87f9022d5914abeceddc27e2f7c9af2590196ce2
GaryGriffin <genealogy@garygriffin.net>
2020-09-14 17:37:07 -0700

    Update syncAssociations.py
    Added OK dialog for actions taken.

--- SyncAssociations/syncAssociations.py
+++ SyncAssociations/syncAssociations.py
@@ -39,6 +39,7 @@ from gramps.gen.display.name import displayer as _nd
 from gramps.gui.managedwindow import ManagedWindow
 from gramps.gen.db import DbTxn
 from gramps.gen.lib import PersonRef
+from gramps.gui.dialog import OkDialog
 # -------------------------------------------------------------------------
 # Internationalization
 # -------------------------------------------------------------------------
@@ -61,6 +62,7 @@ ASSOC_LOOKUP = {
 "Godmother": "Godchild",
 "Landlord": "Tenant"
 }
+WIKI_HELP_PAGE = 'https://gramps-project.org/wiki/index.php/Addon:SyncAssociation'
 
 #------------------------------------------------------------------------
 #
@@ -72,11 +74,15 @@ class syncAssociations(ManagedWindow):
     Sync Associations by adding an Association for the person mentioned in another Association.
     """
     def __init__(self, dbstate, user, options_class, name, callback=None):
+        uistate = user.uistate
         self.dbstate = dbstate
         self.db = dbstate.db
+        count = 0
+        has_assoc = False
         for person_handle in self.db.get_person_handles():
             person = self.db.get_person_from_handle(person_handle)
             for assoc in person.get_person_ref_list():
+                has_assoc = True
                 oldRel = assoc.get_relation()
                 if oldRel in ASSOC_LOOKUP:
                     associate = self.db.get_person_from_handle(assoc.ref)
@@ -93,7 +99,22 @@ class syncAssociations(ManagedWindow):
                         with DbTxn (_('Add %s reciprocal association' ) % _nd.display(associate), self.db) as self.trans:
                             associate.add_person_ref(personRef)
                             self.db.commit_person(associate, self.trans)
-
+                        count += 1
+        if uistate:
+            if count > 0:
+                OkDialog(_("Sync Associations"),
+                         _("{} Reciprocal associations created".format(count)),
+                         parent = uistate.window)
+            elif has_assoc:
+                OkDialog(_("Sync Associations"),
+                         _("All reciprocal associations exist, none created"),
+                         parent = uistate.window)
+            else:
+                OkDialog(_("Sync Associations"),
+                         _("No existing associations, so no reciprocal ones needed"),
+                         parent = uistate.window)
+        else:
+            print("{} Reciprocal associations created".format(count))
     def main(self):
         pass
 
@@ -109,4 +130,3 @@ class syncAssociationsOptions(tool.ToolOptions):
     def __init__(self, name, person_id=None):
         """ Initialize the options class """
         tool.ToolOptions.__init__(self, name, person_id)
-
