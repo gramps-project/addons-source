d50c62e9ddc2f9587ca0377e286ef4e41da209bf
vantu5z <vantu5z@mail.ru>
2020-10-20 10:37:43 +0300

    Clean set_busy_cursor this is not needed any more (we set/unset it in Populate)

--- GraphView/graphview.py
+++ GraphView/graphview.py
@@ -1522,7 +1522,6 @@ class GraphWidget(object):
 
         # perform double click on node by left mouse button
         if event.type == getattr(Gdk.EventType, "DOUBLE_BUTTON_PRESS"):
-            self.uistate.set_busy_cursor(False)
             # Remove all single click events
             for click_item in self.click_events:
                 if not click_item.is_destroyed():
@@ -1539,7 +1538,6 @@ class GraphWidget(object):
             return False
 
         if button == 1 and node_class == 'node':            # left mouse
-            self.uistate.set_busy_cursor(True)
             if handle == self.active_person_handle:
                 # Find a parent of the active person so that they can become
                 # the active person, if no parents then leave as the current
@@ -1548,8 +1546,6 @@ class GraphWidget(object):
                 if parent_handle:
                     handle = parent_handle
                 else:
-                    # unset busy cursor as we don't change active person
-                    self.uistate.set_busy_cursor(False)
                     return True
 
             # redraw the graph based on the selected person
