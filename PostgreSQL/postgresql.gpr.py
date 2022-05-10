c6e97dc6e8b00106ccd06b8108f42647bfd3be42
prculley <paulr2787@gmail.com>
2022-05-10 10:15:58 -0500

    Rebuild some addons for language updates

--- PostgreSQL/postgresql.gpr.py
+++ PostgreSQL/postgresql.gpr.py
@@ -26,7 +26,7 @@ if module1 or locals().get('build_script'):
              name=_('PostgreSQL'),
              name_accell=_('_PostgreSQL Database'),
              description=_('PostgreSQL Database'),
-             version = '1.0.12',
+             version = '1.0.13',
              gramps_target_version='5.1',
              status=STABLE,
              fname='postgresql.py',
