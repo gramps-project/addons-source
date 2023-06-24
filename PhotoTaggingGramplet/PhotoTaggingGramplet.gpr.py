e1ff717471a3d671c6c117f3c35f2f1a7c49ac92
Bruce Jackson <bruce@the-jacksons.ca>
2023-06-24 12:29:02 -0400

    Add support for XMP-mwg-rs Regions in Image Files - Maintenance/gramps51 (#478)
    * Add support XMP-mwg-rs regions in PhotoTaggingGramplet
    
    Allow the PhotoTaggingGramplet to show Face Regions in the XMP metadata of a file.
    Modify the columns to show the name of the XMP Face region
    Allow resizing and ordering of columns
    
    * Enable Search Bar in Person Selector
    
    Search bar provides more options for searching for persons when tagging
    
    * Clean up trailing spaces
    
    Fix errors after travis-ci build
    
    * Remove Trailing Spaces
    
    Remove Trailing Spaces found by travis-ci
    
    * Changes to resolve conflict from origin
    
    - a different PR was merged resolving conflicts
    
    * Update Tool Bar buttons
    
    - remove deprecated methods to create toolbar buttons.
    
    * Update PhotoTaggingGramplet.gpr.py
    
    - add license 
    - update registration parameters
    
    * Update PhotoTaggingGramplet.py
    
    - update license to reflect contributors
    - resolve conflict with Tool Buttons from gramps-project
    
    * Set xmp_person value if rectangle added manually
    
    - set a value for region.xmp_person after a user manually adds a rectangle

--- PhotoTaggingGramplet/PhotoTaggingGramplet.gpr.py
+++ PhotoTaggingGramplet/PhotoTaggingGramplet.gpr.py
@@ -1,8 +1,34 @@
+#
+# Gramps - a GTK+/GNOME based genealogy program
+# http://gramps-project.org
+# Gramplet registration - plug-in/add-on to extend Gramps
+#
+# Copyright (C) 2013    Artem Glebov <artem.glebov@gmail.com>
+# Copyright (C) 2014    Nick Hall
+# Copyright (C) 2021    Paul Culley
+# Copyright (C) 2021    Bruce Jackson
+#
+# This program is free software; you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation; either version 2 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program; if not, write to the Free Software
+# Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
+#
+
 register(GRAMPLET,
          id="Photo Tagging",
          name=_("Photo Tagging"),
          description = _("Gramplet for tagging people in photos"),
-         version = '1.0.39',
+         authors = ["Artem Glebov", "Nick Hall", "Paul Culley", "Bruce Jackson"],
+         version = '1.0.40',
          gramps_target_version="5.1",
          status = STABLE,
          fname="PhotoTaggingGramplet.py",
@@ -10,4 +36,6 @@ register(GRAMPLET,
          gramplet = 'PhotoTaggingGramplet',
          gramplet_title=_("Photo Tagging"),
          navtypes=["Media"],
+         help_url="Addon:Photo_Tagging_Gramplet",
+         include_in_listing = True,
          )
