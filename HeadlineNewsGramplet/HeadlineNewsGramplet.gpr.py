ff79e9acf0ea184fa0b1651eaa6afe81413759e7
prculley <paulr2787@gmail.com>
2023-06-24 09:36:53 -0500

    rebuild Combinedview, HeadlineNews, Quiltview, RepositoriesReport for latest commits

--- HeadlineNewsGramplet/HeadlineNewsGramplet.gpr.py
+++ HeadlineNewsGramplet/HeadlineNewsGramplet.gpr.py
@@ -8,7 +8,7 @@ register(GRAMPLET,
          expand=True,
          gramplet = 'HeadlineNewsGramplet',
          gramplet_title=_("Headline News"),
-         version = '1.0.43',
+         version = '1.0.44',
          gramps_target_version="5.1",
          help_url="HeadlineNewsGramplet"
          )
