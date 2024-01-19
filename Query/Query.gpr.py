f3ddbf2ad3a86b3d9b9d85817d14efebc4aeeed3
Doug Blank <doug.blank@gmail.com>
2024-01-19 08:20:30 -0500

    Update my email address

--- Query/Query.gpr.py
+++ Query/Query.gpr.py
@@ -35,7 +35,7 @@ register(QUICKREPORT,
          include_in_listing = False,
          fname="QueryQuickview.py",
          authors="Douglas Blank",
-         authors_email="dblank@cs.brynmawr.edu",
+         authors_email="doug.blank@gmail.com",
          version = '1.0.38',
          gramps_target_version = "5.2",
          )
