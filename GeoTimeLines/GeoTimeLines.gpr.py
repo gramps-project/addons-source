2c40ccb9c84444275615ea259ef7d70a1129ec99
tbmapman <tboretta@gmail.com>
2024-10-07 19:51:52 -0700

    "TimeLines Map" - Addressed some issues on GitHub and Discourse (#608)
    * Added GeoTimeLines plugin
    
    * Addressed some issues on GitHub and discourse
    
    * Update GeoTimeLines.gpr.py
    
    Added GPL License section
    
    * Fixed crash when starting Gramps directly into the TimeLines Map view

--- GeoTimeLines/GeoTimeLines.gpr.py
+++ GeoTimeLines/GeoTimeLines.gpr.py
@@ -1,11 +1,33 @@
-
+# -*- python -*-
+# -*- coding: utf-8 -*-
+#
+# Gramps - a GTK+/GNOME based genealogy program
+#
+# Copyright (C) 2011-2016  Serge Noiraud
+#
+# This program is free software; you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation; either version 2 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program; if not, write to the Free Software
+# Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
+#
 register(VIEW,
          id='geotimelines',
          name=_("TimeLines Map"),
          description=_("A view showing all the places visited by a person or persons on various date."),
          version = '1.0.0',
          gramps_target_version='5.2',
-         status=STABLE,
+         status=BETA,
+         audience = EXPERT,
+         help_url="https://gramps.discourse.group/t/timelines-map-addon-beta-testing/6130",
          fname='GeoTimeLines.py',
          authors=["Thomas B"],
          authors_email=[""],
