6f2877302b8ba69709f3def57ca5350cb70346ea
Kaj <kmi@vgdata.dk>
2024-11-05 07:57:01 +0100

    Changed the help_url after discussion in the discourse forum

--- HistContext/HistContext.gpr.py
+++ HistContext/HistContext.gpr.py
@@ -12,7 +12,8 @@ register(GRAMPLET,
          gramplet = "HistContext",
          gramplet_title=_("Historical Context"),
          gramps_target_version="5.2",
-         help_url="https://github.com/kajmikkelsen/TimeLineGramplet",
+#         help_url="https://github.com/kajmikkelsen/TimeLineGramplet",
+         help_url="Addon:Historical_Context"
          navtypes=["Person","Dashboard"],
          include_in_listing = True,
          )
