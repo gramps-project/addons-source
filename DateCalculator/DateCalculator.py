88e8b57a316a68dc1e9d5f5790759169b3c9d17c
stevenyoungs <steve@youngs.cc>
2024-12-22 21:15:46 +0000

    Date Calculator Enhancements (#621)
    * Improve wording of labels
    
    * Add tooltips to the text_view widgets
    
    * Shorten message now that we have tooltips
    
    * Use Entry widget instead of TextView
    
    We only need a single line of input and this allows a more compact display with less mouse movement
    
    * Make the result widget read-only
    
    * Change buttonbox layout for consistency
    
    Changed to be consistent with the standard filter sidebar
    
    * Add Copy button
    
    Copies the current result to the OS clipboard
    
    * refactor code
    
    * Set result date quality to QUAL_CALCULATED
    
    * Use displayer to display the result date
    
    * Run black formatter
    
    * Reword error messages and simplify labels
    
    * Update comment to reflect code changes

--- DateCalculator/DateCalculator.py
+++ DateCalculator/DateCalculator.py
@@ -3,6 +3,7 @@
 # Copyright (C) 2010       Jakim Friant
 # Copyright (c) 2015       Douglas S. Blank <doug.blank@gmail.com>
 # Copyright (c) 2020       Jan Sparreboom <jan@sparreboom.net>
+# Copyright (c) 2024       Steve Youngs <steve@youngs.cc>
 #
 # This program is free software; you can redistribute it and/or modify
 # it under the terms of the GNU General Public License as published by
@@ -22,27 +23,30 @@
 DateCalculator does date math.
 """
 
-#------------------------------------------------------------------------
+# ------------------------------------------------------------------------
 #
 # Python modules
 #
-#------------------------------------------------------------------------
+# ------------------------------------------------------------------------
 from gi.repository import Gtk
 
-#------------------------------------------------------------------------
+# ------------------------------------------------------------------------
 #
 # GRAMPS modules
 #
-#------------------------------------------------------------------------
+# ------------------------------------------------------------------------
 from gramps.gen.plug import Gramplet
+from gramps.gen.datehandler import displayer
 from gramps.gen.lib import Date
+from gramps.gui.utils import text_to_clipboard
 
-#------------------------------------------------------------------------
+# ------------------------------------------------------------------------
 #
 # Internationalisation
 #
-#------------------------------------------------------------------------
+# ------------------------------------------------------------------------
 from gramps.gen.const import GRAMPS_LOCALE as glocale
+
 try:
     trans = glocale.get_addon_translator(__file__)
 except ValueError:
@@ -52,15 +56,17 @@ ngettext = trans.ngettext
 
 from gramps.gen.datehandler import parser
 
-#------------------------------------------------------------------------
+
+# ------------------------------------------------------------------------
 #
 # Classes
 #
-#------------------------------------------------------------------------
+# ------------------------------------------------------------------------
 class DateCalculator(Gramplet):
     """
     Gramplet that computes date math.
     """
+
     def init(self):
         self.gui.WIDGET = self.build_gui()
         self.gui.get_container_widget().remove(self.gui.textview)
@@ -73,45 +79,56 @@ class DateCalculator(Gramplet):
         self.top = Gtk.Box(orientation=Gtk.Orientation.VERTICAL)
         self.top.set_border_width(6)
 
-        self.entry1 = self.__add_text_view(_("Expression 1"))
-        self.entry2 = self.__add_text_view(_("Expression 2"))
-        self.result = self.__add_text_view(_("Result"))
+        self.entry1 = self.__add_entry(
+            _("Reference Date or Date Range"), _("a valid Gramps date")
+        )
+        self.entry2 = self.__add_entry(
+            _("Date or offset ±y or ±y, m, d"),
+            _(
+                "1. a Date\n2. a positive or negative number, representing years\n3. a positive or negative list of values, representing years, months, days"
+            ),
+        )
+        self.result = self.__add_entry(_("Result"))
+        self.result.set_editable(False)
 
-        bbox = Gtk.ButtonBox()
+        button_box = Gtk.ButtonBox()
+        button_box.set_layout(Gtk.ButtonBoxStyle.START)
+        button_box.set_spacing(6)
+        button_box.set_border_width(12)
+
         apply_button = Gtk.Button(label=_("Calculate"))
-        apply_button.connect('clicked', self.apply_clicked)
-        bbox.pack_start(apply_button, False, False, 6)
+        apply_button.connect("clicked", self.apply_clicked)
+        button_box.add(apply_button)
+        copy_button = Gtk.Button(label=_("Copy"))
+        copy_button.connect("clicked", self.copy_clicked)
+        button_box.add(copy_button)
         clear_button = Gtk.Button(label=_("Clear"))
-        clear_button.connect('clicked', self.clear_clicked)
-        bbox.pack_start(clear_button, False, False, 6)
-        self.top.pack_start(bbox, False, False, 6)
+        clear_button.connect("clicked", self.clear_clicked)
+        button_box.add(clear_button)
+        self.top.pack_start(button_box, False, False, 6)
 
         self.top.show_all()
         self.clear_clicked(None)
         return self.top
 
-    def __add_text_view(self, name):
+    def __add_entry(self, name, tooltip=""):
         """
-        Add a text view to the interface.
+        Add a Entry widget to the interface.
         """
         label = Gtk.Label(halign=Gtk.Align.START)
-        label.set_markup('<b>%s</b>' % name)
+        label.set_markup("<b>%s</b>" % name)
         self.top.pack_start(label, False, False, 6)
-        swin = Gtk.ScrolledWindow()
-        swin.set_shadow_type(Gtk.ShadowType.IN)
-        tview = Gtk.TextView()
-        swin.add(tview)
-        self.top.pack_start(swin, True, True, 6)
-        return tview.get_buffer()
+        entry = Gtk.Entry()
+        entry.set_tooltip_text(tooltip)
+        self.top.pack_start(entry, False, False, 6)
+        return entry
 
     def apply_clicked(self, obj):
         """
         Method that is run when you click the Calculate button.
         """
-        text1 = str(self.entry1.get_text(self.entry1.get_start_iter(),
-                                         self.entry1.get_end_iter(), False))
-        text2 = str(self.entry2.get_text(self.entry2.get_start_iter(),
-                                         self.entry2.get_end_iter(), False))
+        text1 = str(self.entry1.get_text())
+        text2 = str(self.entry2.get_text())
         neg1 = False
         neg2 = False
         if text1.startswith("-"):
@@ -128,7 +145,7 @@ class DateCalculator(Gramplet):
                 if not val1.is_valid():
                     raise Exception()
             except:
-                self.result.set_text(_("Error: invalid entry for expression 1"))
+                self.result.set_text(_("Error: invalid date for first expression"))
                 return
         try:
             val2 = eval(text2)
@@ -138,53 +155,57 @@ class DateCalculator(Gramplet):
                 if not val2.is_valid():
                     raise Exception()
             except:
-                self.result.set_text(_("Error: invalid entry for expression 2"))
+                self.result.set_text(_("Error: invalid offset for second expression"))
                 return
+        result = None
         if isinstance(val1, Date):
             if isinstance(val2, Date):
                 if neg1:
-                    self.result.set_text(str(val2 - val1))
+                    result = val2 - val1
                 else:
-                    self.result.set_text(str(val1 - val2))
+                    result = val1 - val2
             elif isinstance(val2, tuple):
                 if neg2:
-                    self.result.set_text(str(val1 - val2))
+                    result = val1 - val2
                 else:
-                    self.result.set_text(str(val1 + val2))
+                    result = val1 + val2
             elif isinstance(val2, int):
                 if neg2:
-                    self.result.set_text(str(val1 - val2))
+                    result = val1 - val2
                 else:
-                    self.result.set_text(str(val1 + val2))
+                    result = val1 + val2
         elif isinstance(val1, tuple):
             if isinstance(val2, Date):
                 if neg1:
-                    self.result.set_text(str(val2 - val1))
+                    result = val2 - val1
                 else:
-                    self.result.set_text(str(val2 + val1))
+                    result = val2 + val1
             elif isinstance(val2, tuple):
-                self.result.set_text(_("Error: at least one expression must be a date"))
+                result = _("Error: at least one expression must be a date")
             elif isinstance(val2, int):
-                self.result.set_text(_("Error: at least one expression must be a date"))
+                result = _("Error: at least one expression must be a date")
         elif isinstance(val1, int):
             if isinstance(val2, Date):
                 if neg1:
-                    self.result.set_text(str(val2 - val1))
+                    result = val2 - val1
                 else:
-                    self.result.set_text(str(val2 + val1))
+                    result = val2 + val1
             elif isinstance(val2, tuple):
-                self.result.set_text(_("Error: at least one expression must be a date"))
+                result = _("Error: at least one expression must be a date")
             elif isinstance(val2, int):
-                self.result.set_text(_("Error: at least one expression must be a date"))
+                result = _("Error: at least one expression must be a date")
+        if result:
+            if isinstance(result, Date):
+                result.set_quality(Date.QUAL_CALCULATED)
+                result = displayer.display(result)
+            self.result.set_text(str(result))
+
+    def copy_clicked(self, obj):
+        text_to_clipboard(self.result.get_text())
 
     def clear_clicked(self, obj):
         self.entry1.set_text("")
         self.entry2.set_text("")
         self.result.set_text(
-            _("Enter an expression in the entries above and click Calculate.\n\n"
-              "An expression can be:\n\n"
-              "1. a valid Gramps date\n"
-              "2. a positive or negative number, representing years\n"
-              "3. a positive or negative tuple, representing (years, months, days)\n\n"
-              "Note that at least one expression must be a date."
-          ))
+            _("Enter an expression in the entries above and click Calculate.")
+        )
