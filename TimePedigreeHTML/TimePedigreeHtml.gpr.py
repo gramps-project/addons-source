d9fe1d11cf9b58ef9c7044a83c0d6e2556c4cc68
GaryGriffin <genealogy@garygriffin.net>
2024-12-27 10:40:18 -0800

    Update TimePedigreeHtml.gpr.py

--- TimePedigreeHTML/TimePedigreeHtml.gpr.py
+++ TimePedigreeHTML/TimePedigreeHtml.gpr.py
@@ -40,7 +40,7 @@ register(REPORT,
          name = _("Timeline Pedigree Report"),
          description = _("Creates an HTML webpage that uses JavaScript to "
             "show a pedigree block diagram arranged vertically by birth date"),
-         version = '0.0.6',
+         version = '0.0.7',
          gramps_target_version = MODULE_VERSION,
          status = STABLE,
          fname = 'TimePedigreeHtml.py',
