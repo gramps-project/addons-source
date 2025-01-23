d484f94f68a0efa61df99c31eea346e6a22f62b3
Steve Youngs <steve@youngs.cc>
2025-01-23 22:09:53 +0000

    Bump version number

--- DateCalculator/DateCalculator.gpr.py
+++ DateCalculator/DateCalculator.gpr.py
@@ -22,7 +22,7 @@ register(
     name=_("Date Calculator"),
     description=_("Perform date math calculations"),
     status=STABLE,
-    version="0.0.19",
+    version="0.0.20",
     fname="DateCalculator.py",
     authors=["Doug Blank", "Steve Youngs"],
     authors_email=["doug.blank@gmail.com", "steve@youngs.cc"],
