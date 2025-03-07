69221d2269d8ca72be1c7c549b0adbbf4d958f52
GaryGriffin <genealogy@garygriffin.net>
2025-03-07 08:27:54 -0800

    Merge Gramps6 uawebpack #662

--- UAWebConnectPack/UAWebPack.gpr.py
+++ UAWebConnectPack/UAWebPack.gpr.py
@@ -11,7 +11,7 @@ register(
     name=_("UA Web Connect Pack"),
     description=_("Collection of Web sites for the UA (requires libwebconnect)"),
     status=STABLE,
-    version = '1.0.0',
+    version = '1.0.1',
     gramps_target_version="6.0",
     fname="UAWebPack.py",
     load_on_reg=True,
