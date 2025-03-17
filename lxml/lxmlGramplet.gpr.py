c615899bd18b4291af560b4a2f8b08f51eb07232
GaryGriffin <genealogy@garygriffin.net>
2025-03-17 13:16:20 -0700

    Merge update etree and lxml gramplets for gramps60 #679

--- lxml/lxmlGramplet.gpr.py
+++ lxml/lxmlGramplet.gpr.py
@@ -5,16 +5,16 @@
 # ------------------------------------------------------------------------
 
 register(
-    GRAMPLET,
-    id="lxml Gramplet",
-    name=_("lxml"),
-    description=_("Gramplet for testing lxml and XSLT"),
-    status=STABLE,
-    version = '1.0.22',
-    gramps_target_version="6.0",
-    include_in_listing=False,
-    height=300,
-    gramplet="lxmlGramplet",
-    fname="lxmlGramplet.py",
-    gramplet_title=_("lxml"),
+GRAMPLET,
+id="lxml Gramplet",
+name=_("lxml"),
+description=_("Gramplet for testing lxml and XSLT"),
+status=STABLE,
+version = '1.0.23',
+gramps_target_version="6.0",
+include_in_listing=False,
+height=300,
+gramplet="lxmlGramplet",
+fname="lxmlGramplet.py",
+gramplet_title=_("lxml"),
 )
