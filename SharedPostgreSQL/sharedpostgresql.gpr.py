023af6b3c911a257e8a40846e3ff76397b73d211
GaryGriffin <genealogy@garygriffin.net>
2025-03-19 16:10:00 -0700

    Merge  Update SharedPostgreSQL for Gramps 6.0 #639

--- SharedPostgreSQL/sharedpostgresql.gpr.py
+++ SharedPostgreSQL/sharedpostgresql.gpr.py
@@ -24,7 +24,7 @@ register(
     name=_("SharedPostgreSQL"),
     name_accell=_("Shared _PostgreSQL Database"),
     description=_("Shared PostgreSQL Database"),
-    version = '0.1.11',
+    version = '0.1.12',
     gramps_target_version="6.0",
     status=STABLE,
     fname="sharedpostgresql.py",
