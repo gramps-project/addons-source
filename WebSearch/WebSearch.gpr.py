ebc1ad79ab6d20ebe8015b6f87809bdbd4772060
GaryGriffin <genealogy@garygriffin.net>
2025-03-23 13:37:35 -0700

    Merge Add WebSearch Gramplet for Gramps 6.0 #688

--- WebSearch/WebSearch.gpr.py
+++ WebSearch/WebSearch.gpr.py
@@ -37,7 +37,7 @@ register(
         "Person, Place, Family, or Source record"
     ),
     status=STABLE,
-    version="0.43.27",
+    version = '0.43.28',
     fname="WebSearch.py",
     height=20,
     detached_width=400,
