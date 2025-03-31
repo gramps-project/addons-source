0fa5aa8f06de0c6a120b809432bc119b33f8df3e
GaryGriffin <genealogy@garygriffin.net>
2025-03-31 15:11:09 -0700

    Merge Fix extra Error Dialogs and add a section for xmltodict library #698

--- lxml/etreeGramplet.gpr.py
+++ lxml/etreeGramplet.gpr.py
@@ -11,7 +11,7 @@ register(
     description=_("Gramplet for testing etree with Gramps XML"),
     status=EXPERIMENTAL,
     audience = DEVELOPER,
-    version = '1.0.24',
+    version = '1.0.25',
     gramps_target_version="6.0",
     include_in_listing=True,
     height=400,
