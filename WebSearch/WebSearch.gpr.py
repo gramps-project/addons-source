25d18e4bda523777e2a7744f76a8e0a53203d9ce
GaryGriffin <genealogy@garygriffin.net>
2025-04-04 13:41:44 -0700

    Merge [WebSearch] 6.0 updates including issue https://gramps-project.org/bugs/view.php?id=13751 #703

--- WebSearch/WebSearch.gpr.py
+++ WebSearch/WebSearch.gpr.py
@@ -37,7 +37,7 @@ register(
         "Person, Place, Family, or Source record"
     ),
     status=STABLE,
-    version="0.60.41",
+    version = '0.60.42',
     fname="WebSearch.py",
     height=20,
     detached_width=400,
