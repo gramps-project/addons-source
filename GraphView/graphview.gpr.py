24242bd3c7f259e29af1d0426bedfd2c18b2a469
GaryGriffin <genealogy@garygriffin.net>
2025-04-10 10:26:12 -0700

    Merge gramps60 Bug 13399: change lower case names graphview-show-id keys #705

--- GraphView/graphview.gpr.py
+++ GraphView/graphview.gpr.py
@@ -4,7 +4,7 @@ register(
     name=_("Graph View"),
     category=("Ancestry", _("Charts")),
     description=_("Dynamic and interactive graph of relations"),
-    version = '1.0.144',
+    version = '1.0.145',
     gramps_target_version="6.0",
     status=STABLE,
     fname="graphview.py",
