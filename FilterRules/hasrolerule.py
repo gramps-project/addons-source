ec54c03b4bba4d3c1faf3ef433cecc384f4906a4
Steve Youngs <steve@youngs.cc>
2025-04-27 16:49:20 +0100

    Update the HasPersonEventRole and HasFamilyEventRole rules to be compatible with the filter optimizer introduced in gramps 6.0 Refactor code to move common code into a new super class HasEventRole

--- FilterRules/hasrolerule.py
+++ FilterRules/hasrolerule.py
@@ -2,6 +2,7 @@
 # Gramps - a GTK+/GNOME based genealogy program
 #
 # Copyright (C) 2018  Paul Culley
+# Copyright (C) 2025  Steve Youngs
 #
 # This program is free software; you can redistribute it and/or modify
 # it under the terms of the GNU General Public License as published by
@@ -21,21 +22,34 @@
 """
 Filter rule to match persons with a particular event.
 """
-#-------------------------------------------------------------------------
+# -------------------------------------------------------------------------
 #
 # Standard Python modules
 #
-#-------------------------------------------------------------------------
+# -------------------------------------------------------------------------
+from __future__ import annotations
+from operator import eq, ne
 
-#-------------------------------------------------------------------------
+# -------------------------------------------------------------------------
 #
 # Gramps modules
 #
-#-------------------------------------------------------------------------
+# -------------------------------------------------------------------------
 from gramps.gen.lib.eventroletype import EventRoleType
 from gramps.gui.editors.filtereditor import MySelect, MyBoolean
 from gramps.gen.filters.rules import Rule
 from gramps.gen.const import GRAMPS_LOCALE as glocale
+
+# -------------------------------------------------------------------------
+#
+# Typing modules
+#
+# -------------------------------------------------------------------------
+from typing import Set
+from gramps.gen.types import PersonHandle
+from gramps.gen.lib import Family, Person
+from gramps.gen.db import Database
+
 try:
     _trans = glocale.get_addon_translator(__file__)
 except ValueError:
@@ -44,65 +58,64 @@ _ = _trans.gettext
 
 
 class Roletype(MySelect):
-    """ Provide a Role type selector """
+    """Provide a Role type selector"""
+
     def __init__(self, db):
         MySelect.__init__(self, EventRoleType, db.get_event_roles())
 
 
 class NoMatch(MyBoolean):
-    """ Provide a negation switch """
+    """Provide a negation switch"""
+
     def __init__(self, db):
         MyBoolean.__init__(self, _("Does NOT match with selected Role"))
-        self.set_tooltip_text(_("Finds the items that don't have event Roles "
-                                "of the selected type."))
-#-------------------------------------------------------------------------
+        self.set_tooltip_text(
+            _("Finds the items that don't have event Roles " "of the selected type.")
+        )
+
+
+class HasEventRole(Rule):
+    def _prepare(self, db: Database, user, obj_generator):
+        self.selected_handles: Set[PersonHandle] = set()
+        if self.list[0]:  # was a role specified
+            cmp = eq if self.list[1] == "0" else ne
+            for obj in obj_generator():
+                for event_ref in obj.get_event_ref_list():
+                    if cmp(event_ref.role.xml_str(), self.list[0]):
+                        self.selected_handles.add(obj.handle)
+                        continue
+
+
+# -------------------------------------------------------------------------
 #
 # HasEvent
 #
-#-------------------------------------------------------------------------
-class HasPersonEventRole(Rule):
+# -------------------------------------------------------------------------
+class HasPersonEventRole(HasEventRole):
     """Rule that checks for a person with a selected event role"""
 
-    labels = [(_('Role'), Roletype),
-              (_('Inverse'), NoMatch)]
-    name = _('People with events with the <role>')
+    labels = [(_("Role"), Roletype), (_("Inverse"), NoMatch)]
+    name = _("People with events with the <role>")
     description = _("Matches people with an event with a selected role")
-    category = _('Event filters')
+    category = _("Event filters")
 
-    def apply_to_one(self, dbase, person):
-        if not self.list[0]:
-            return False
-        for event_ref in person.get_event_ref_list():
-            if not event_ref:
-                continue
-            if self.list[1] == '1':
-                if event_ref.role.xml_str() != self.list[0]:
-                    return True
-            else:
-                if event_ref.role.xml_str() == self.list[0]:
-                    return True
-        return False
+    def prepare(self, db: Database, user):
+        self._prepare(db, user, db.iter_people)
+
+    def apply_to_one(self, dbase: Database, person: Person) -> bool:
+        return person.handle in self.selected_handles
 
 
-class HasFamilyEventRole(Rule):
+class HasFamilyEventRole(HasEventRole):
     """Rule that checks for a family with a selected event role"""
 
-    labels = [(_('Role'), Roletype),
-              (_('Inverse'), NoMatch)]
-    name = _('Families with events with the <role>')
+    labels = [(_("Role"), Roletype), (_("Inverse"), NoMatch)]
+    name = _("Families with events with the <role>")
     description = _("Matches families with an event with a selected role")
-    category = _('Event filters')
+    category = _("Event filters")
 
-    def apply_to_one(self, dbase, family):
-        if not self.list[0]:
-            return False
-        for event_ref in family.get_event_ref_list():
-            if not event_ref:
-                continue
-            if self.list[1] == '1':
-                if event_ref.role.xml_str() != self.list[0]:
-                    return True
-            else:
-                if event_ref.role.xml_str() == self.list[0]:
-                    return True
-        return False
+    def prepare(self, db: Database, user):
+        self._prepare(db, user, db.iter_families)
+
+    def apply_to_one(self, dbase: Database, family: Family) -> bool:
+        return family.handle in self.selected_handles
