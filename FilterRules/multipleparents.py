71bc20937b897b25da48f38700e0c78ee525334a
Steve Youngs <steve@youngs.cc>
2025-04-27 21:56:11 +0100

    Update the MultipleParents rule to be compatible with the filter optimizer introduced in gramps 6.0

--- FilterRules/multipleparents.py
+++ FilterRules/multipleparents.py
@@ -2,6 +2,7 @@
 # Gramps - a GTK+/GNOME based genealogy program
 #
 # Copyright (C) 2002-2006  Donald N. Allingham
+# Copyright (C) 2025       Steve Youngs
 #
 # This program is free software; you can redistribute it and/or modify
 # it under the terms of the GNU General Public License as published by
@@ -18,30 +19,48 @@
 # Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
 #
 
-#-------------------------------------------------------------------------
+# -------------------------------------------------------------------------
 #
 # Standard Python modules
 #
-#-------------------------------------------------------------------------
+# -------------------------------------------------------------------------
+from __future__ import annotations
 from gramps.gen.const import GRAMPS_LOCALE as glocale
+
 _ = glocale.translation.gettext
 
-#-------------------------------------------------------------------------
+# -------------------------------------------------------------------------
 #
 # Gramps modules
 #
-#-------------------------------------------------------------------------
+# -------------------------------------------------------------------------
 from gramps.gen.filters.rules import Rule
 
-#-------------------------------------------------------------------------
+# -------------------------------------------------------------------------
+#
+# Typing modules
+#
+# -------------------------------------------------------------------------
+from typing import Set
+from gramps.gen.types import PersonHandle
+from gramps.gen.lib import Person
+from gramps.gen.db import Database
+
+# -------------------------------------------------------------------------
 # "People with multiple parent records"
-#-------------------------------------------------------------------------
+# -------------------------------------------------------------------------
 class MultipleParents(Rule):
     """People with multiple parent records"""
 
-    name = _('People with multiple parent records')
+    name = _("People with multiple parent records")
     description = _("Matches people who have more than one set of parents")
-    category = _('Family filters')
+    category = _("Family filters")
 
-    def apply_to_one(self,db,person):
-        return len(person.get_parent_family_handle_list()) > 1
+    def prepare(self, db: Database, user):
+        self.selected_handles: Set[PersonHandle] = set()
+        for person in db.iter_people():
+            if len(person.get_parent_family_handle_list()) > 1:
+                self.selected_handles.add(person.handle)
+
+    def apply_to_one(self, db: Database, person: Person) -> bool:
+        return person.handle in self.selected_handles
