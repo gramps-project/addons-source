7b89f3b7e6e979742680c1bee42549b05c5632b0
GaryGriffin <genealogy@garygriffin.net>
2025-04-29 14:48:41 -0700

    Merge "Timeline Pedigree Report" : changes for gramps 6.0, and bug correction for background #720

--- TimePedigreeHTML/TimePedigreeHtml.gpr.py
+++ TimePedigreeHTML/TimePedigreeHtml.gpr.py
@@ -50,7 +50,7 @@ register(
         "Creates an HTML webpage that uses JavaScript to "
         "show a pedigree block diagram arranged vertically by birth date"
     ),
-    version = '0.0.10',
+    version = '0.0.11',
     gramps_target_version=major_version,
     status=STABLE,
     fname="TimePedigreeHtml.py",
