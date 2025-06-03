09df5923bd8c86562151a96da1c92b8f6737fe01
GaryGriffin <genealogy@garygriffin.net>
2025-06-03 09:19:58 -0700

    Merge Update WebSearch to version 1.10.7 for Gramps 5.2 #737

--- WebSearch/WebSearch.gpr.py
+++ WebSearch/WebSearch.gpr.py
@@ -37,7 +37,7 @@ register(
         "Person, Place, Family, or Source record"
     ),
     status=STABLE,
-    version="1.10.7",
+    version = '1.10.8',
     fname="WebSearch.py",
     height=20,
     detached_width=400,
