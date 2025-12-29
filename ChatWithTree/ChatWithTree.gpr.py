40e24a7f40d6a22315c780421dc82b68c6000428
Melle <mellekoning@gmail.com>
2025-12-29 10:37:35 +0100

    reset version number

--- ChatWithTree/ChatWithTree.gpr.py
+++ ChatWithTree/ChatWithTree.gpr.py
@@ -8,17 +8,17 @@ register(
     id="ChatWithTree",  # Unique ID for your addon
     name=_("Chat With Tree Interactive Addon"),  # Display name in Gramps, translatable
     description=_("Chat With Tree with the help of AI Large Language Model, needs litellm module"),
-    version = '0.0.28',
+    version = '0.0.27',
     gramps_target_version="6.0",  # Specify the Gramps version you are targeting
     status=STABLE,
-    audience = EVERYONE,
+    audience=EVERYONE,
     fname="ChatWithTree.py",  # The main Python file for your Gramplet
     # The 'gramplet' argument points to the class name in your main file
     gramplet="ChatWithTreeClass",
     gramplet_title=_("Chat With Tree"),
     authors = ["Melle Koning"],
     authors_email = ["mellekoning@gmail.com"],
-    height=18,
+    height=500,
      # addon needs litellm python module
     requires_mod=['litellm'],
     navtypes=["Dashboard"],
