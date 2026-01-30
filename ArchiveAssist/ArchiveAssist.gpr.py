e09bd3967b1c21fd0b41e563b22de596b71cac2a
Ludwig-T <ludwigtiston@gmail.com>
2026-01-30 17:00:16 +0100

    Init commit

---
+++ ArchiveAssist/ArchiveAssist.gpr.py
@@ -0,0 +1,14 @@
+register(
+    GRAMPLET,
+    gramps_target_version = '6.0',
+    version='1.0.0',
+    id="ArchiveAssist",
+    name="Archive Assist",
+    description=_("Parses strings from Riksarkivet and ArkivDigital to create sources and citations."),
+    status=STABLE,
+    fname="ArchiveAssist.py",
+    gramplet="ArchiveAssist",
+    gramplet_title=_("Archive Assist"),
+    authors = ["Ludwig Tiston"],
+    authors_email = ["ludwig.tiston@proton.me"],
+)
