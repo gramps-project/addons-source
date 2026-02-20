92e04196d16bbf895d8f6824d386690fc8f819a5
Ludwig-T <ludwigtiston@gmail.com>
2026-02-20 18:14:17 +0100

    Add license and AI use

--- ArchiveAssist/ArchiveAssist.py
+++ ArchiveAssist/ArchiveAssist.py
@@ -1,4 +1,20 @@
-# encoding:utf-8
+# Copyright (C) 2026  Ludwig Tiston <ludwig.tiston@proton.me>
+#
+# This program is free software; you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation; either version 2 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program; if not, see <https://www.gnu.org/licenses/>.
+
+# Contains AI-generated code. Mostly used for regex and GUI.
+
 from gi.repository import Gtk
 from gramps.gen.plug import Gramplet
 from gramps.gen.lib import Source, Citation, Attribute, Span, Date, Repository, RepoRef
