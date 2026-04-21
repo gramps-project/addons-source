77350465fbb0de339f23b431ca1f554909cc6941
GaryGriffin <genealogy@garygriffin.net>
2026-04-21 09:24:27 -0700

    Cherrypick TimelinePedigreeVIew PR 819, 823

--- TimelinePedigreeView/TimelinePedigreeView.gpr.py
+++ TimelinePedigreeView/TimelinePedigreeView.gpr.py
@@ -36,7 +36,7 @@ register(
         "The view shows a timeline pedigree with ancestors and "
         "descendants of the selected person"
     ),
-    version = '0.1.67',
+    version = '0.1.68',
     gramps_target_version="6.1",
     status=STABLE,
     fname="TimelinePedigreeView.py",
