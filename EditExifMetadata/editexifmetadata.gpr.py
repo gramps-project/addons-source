d1017d29896f599366eda6fc186afbf98ab05593
GaryGriffin <genealogy@garygriffin.net>
2026-04-28 11:11:00 -0700

    Merge EditExifMetdata: Allow newer versions of GExiv2. #829

--- EditExifMetadata/editexifmetadata.gpr.py
+++ EditExifMetadata/editexifmetadata.gpr.py
@@ -35,7 +35,7 @@ register(
     gramplet_title=_("Edit Exif Metadata"),
     detached_width=550,
     detached_height=550,
-    version = '2.0.17',
+    version = '2.0.18',
     gramps_target_version="6.1",
     status=STABLE,
     include_in_listing=True,
