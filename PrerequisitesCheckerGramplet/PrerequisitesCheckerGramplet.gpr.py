0b9abc4d06cdfec57277f456ccef1a80ca0603d7
GaryGriffin <genealogy@garygriffin.net>
2026-05-10 10:45:08 -0700

    Merge PR844 and PR845 PrerequisitesChecker
    Update PrerequisitesChecker. Since strings change, update template.pot and addons.pot for Weblate.

--- PrerequisitesCheckerGramplet/PrerequisitesCheckerGramplet.gpr.py
+++ PrerequisitesCheckerGramplet/PrerequisitesCheckerGramplet.gpr.py
@@ -28,7 +28,7 @@ register(
     id="Prerequisites Checker Gramplet",
     name=_("Prerequisites Checker"),
     description=_("Prerequisites Checker Gramplet"),
-    version = '1.2.11',
+    version = '1.2.12',
     gramps_target_version="6.1",
     status=STABLE,
     fname="PrerequisitesCheckerGramplet.py",
