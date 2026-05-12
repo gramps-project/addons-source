d448875bd8fd306742890adbcecf74096c8f27b0
GaryGriffin <genealogy@garygriffin.net>
2026-05-12 08:30:26 -0700

    Merge GrampsWebSync: add DbTxn and typing.Any imports#867

--- GrampsWebSync/grampswebsync.gpr.py
+++ GrampsWebSync/grampswebsync.gpr.py
@@ -28,7 +28,7 @@ register(
     id="gramps_web_sync",
     name=_("Gramps Web Sync"),
     description=_("Synchronizes a local database with a Gramps Web instance."),
-    version = '1.3.10',
+    version = '1.3.11',
     gramps_target_version="6.1",
     status=STABLE,
     fname="grampswebsync.py",
