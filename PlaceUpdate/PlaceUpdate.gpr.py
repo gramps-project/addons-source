d9949957f21bc549b34904df0e0eeaac78051437
GaryGriffin <genealogy@garygriffin.net>
2026-05-14 12:29:55 -0700

    Merge PlaceUpdate: import EditDate#855

--- PlaceUpdate/PlaceUpdate.gpr.py
+++ PlaceUpdate/PlaceUpdate.gpr.py
@@ -4,7 +4,7 @@ register(
     name=_("PlaceUpdate"),
     description=_("Gramplet to manipulate multiple places"),
     status=STABLE,
-    version = '1.0.13',
+    version = '1.0.14',
     gramps_target_version="6.1",
     fname="PlaceUpdate.py",
     gramplet="PlaceUpdate",
