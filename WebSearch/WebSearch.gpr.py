268ea1822d97df36fa2774f1123f14a225d74927
GaryGriffin <genealogy@garygriffin.net>
2026-06-07 12:29:43 -0700

    Merge WebSearch: add FinalNotes obituary research guide#940

--- WebSearch/WebSearch.gpr.py
+++ WebSearch/WebSearch.gpr.py
@@ -37,7 +37,7 @@ register(
         "Person, Place, Family, or Source record"
     ),
     status=STABLE,
-    version = '1.10.18',
+    version = '1.10.19',
     fname="WebSearch.py",
     height=20,
     detached_width=400,
