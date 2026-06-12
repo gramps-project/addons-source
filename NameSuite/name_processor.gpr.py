5befd9f37e4a38c172497017ed53fe02ba31b40d
bryndin <1129396+bryndin@users.noreply.github.com>
2026-06-12 02:04:53 -0700

    updated version after build

--- NameSuite/name_processor.gpr.py
+++ NameSuite/name_processor.gpr.py
@@ -29,7 +29,7 @@ register(
     description=_(
         "Tools to rename given name, audit and infer patronymic (East Slavic) names."
     ),
-    version="1.0",
+    version = '1.0.1',
     gramps_target_version="6.0",
     status=STABLE,
     fname="names_tool.py",
@@ -47,7 +47,7 @@ register(
     description=_(
         "Suggests (East Slavic) patronymic names in real-time as you navigate."
     ),
-    version="1.0",
+    version = '1.0.1',
     gramps_target_version="6.0",
     status=STABLE,
     fname="patronymics_gramplet.py",
