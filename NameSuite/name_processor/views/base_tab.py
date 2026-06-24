9a29d5823d0d5be295ec57c7484500f97764e562
bryndin <1129396+bryndin@users.noreply.github.com>
2026-06-24 01:23:48 -0700

    NameSuite: use Addon specific i18n translatior ; add manually generted .pot

--- NameSuite/name_processor/views/base_tab.py
+++ NameSuite/name_processor/views/base_tab.py
@@ -38,7 +38,11 @@ if TYPE_CHECKING:
     from name_processor.controllers.tool import ToolController
 
 
-_ = glocale.translation.gettext
+try:
+    _trans = glocale.get_addon_translator(__file__)
+except ValueError:
+    _trans = glocale.translation
+_ = _trans.gettext
 
 
 class BaseTab:
--- NameSuite/name_processor/views/tool_audit_tab.py
+++ NameSuite/name_processor/views/tool_audit_tab.py
@@ -41,7 +41,11 @@ if TYPE_CHECKING:
     from name_processor.controllers.tool import ToolController
 
 
-_ = glocale.translation.gettext
+try:
+    _trans = glocale.get_addon_translator(__file__)
+except ValueError:
+    _trans = glocale.translation
+_ = _trans.gettext
 
 
 class AuditTab(BaseTab):
