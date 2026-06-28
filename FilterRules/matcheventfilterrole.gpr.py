a3c6f5adc9be543f94d1a927553299d41c567933
Jérémi DUPIN <jeremi+fg@dupin.fdn.fr>
2026-06-28 18:14:43 +0200

    add role to person event filter

---
+++ FilterRules/matcheventfilterrole.gpr.py
@@ -0,0 +1,19 @@
+"""
+Filter rule to match person of event filter with role.
+"""
+register(
+    RULE,
+    id="MatchEventFilterRole",
+    name=_("People from event with role"),
+    description=_("Matches people of event filter with role"),
+    version = '0.0.1',
+    authors=["jjdup"],
+    authors_email=["jeremi+gramps@dupin.fdn.fr"],
+    gramps_target_version="6.0",
+    status=STABLE,
+    fname="matcheventfilterrole.py",
+    ruleclass="MatchesEventFilterRole",  # must be rule class name
+    namespace="Person",  # one of the primary object classes
+    help_url="Addon:Rule_expansions",
+)
+
