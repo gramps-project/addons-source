d65f23666cfc8fb21033fc6c505a4f681a5ed402
GaryGriffin <genealogy@garygriffin.net>
2026-06-29 10:46:06 -0700

    Merge ArchiveAssist PR#961

--- ArchiveAssist/ArchiveAssist.gpr.py
+++ ArchiveAssist/ArchiveAssist.gpr.py
@@ -16,7 +16,7 @@
 register(
     GRAMPLET,
     gramps_target_version = '6.1',
-    version = '1.0.3',
+    version = '1.0.4',
     id="ArchiveAssist",
     name="Archive Assist",
     description=_("Parses strings from Riksarkivet and ArkivDigital to create sources and citations."),
