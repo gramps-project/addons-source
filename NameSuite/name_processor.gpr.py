397dcf11ee27f0e6726c3ecbb39daaa86325b63d
GaryGriffin <genealogy@garygriffin.net>
2026-07-03 07:37:25 -0700

    Merge Add NameSuite Addon (tool + gramplet) to fix, infer and audit given and patronymic names#941

--- NameSuite/name_processor.gpr.py
+++ NameSuite/name_processor.gpr.py
@@ -28,7 +28,7 @@ register(
     description=_(
         "Tools to rename given name, audit and infer patronymic (East Slavic) names."
     ),
-    version="1.0",
+    version = '1.0.1',
     gramps_target_version="6.0",
     status=STABLE,
     fname="names_tool.py",
@@ -46,7 +46,7 @@ register(
     description=_(
         "Suggests (East Slavic) patronymic names in real-time as you navigate."
     ),
-    version="1.0",
+    version = '1.0.1',
     gramps_target_version="6.0",
     status=STABLE,
     fname="patronymics_gramplet.py",
