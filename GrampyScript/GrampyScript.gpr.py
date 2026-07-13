2f19f7df73d223216eaf9e7e8ad5cac006575382
GaryGriffin <genealogy@garygriffin.net>
2026-07-13 08:43:15 -0700

    Merge GrampyScript: fix histogram IndexError, add editor conveniences

--- GrampyScript/GrampyScript.gpr.py
+++ GrampyScript/GrampyScript.gpr.py
@@ -23,7 +23,7 @@ register(
     name=_("Gram.py Script"),
     description=_("Run a special Gramps Python script"),
     status=STABLE,
-    version = '0.0.8',
+    version = '0.0.9',
     fname="GrampyScript.py",
     authors=["Doug Blank"],
     authors_email=["doug.blank@gmail.com"],
