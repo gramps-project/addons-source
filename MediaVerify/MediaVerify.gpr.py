6a56efe127ae8fc915aa93dbe195bb46410f7a47
GaryGriffin <genealogy@garygriffin.net>
2026-07-24 09:29:22 -0700

    Merge MediaVerify auto update media object names #12610 #997

--- MediaVerify/MediaVerify.gpr.py
+++ MediaVerify/MediaVerify.gpr.py
@@ -30,7 +30,7 @@ register(
     id="mediaverify",
     name=_("Media Verify"),
     description=_("Verify that media is present in the correct path"),
-    version = '1.0.34',
+    version = '1.0.35',
     gramps_target_version="6.1",
     status=STABLE,
     fname="MediaVerify.py",
