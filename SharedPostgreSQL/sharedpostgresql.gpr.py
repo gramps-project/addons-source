7ecfe4b3a2128cf6372d90f60af8e660c7d5e87e
GaryGriffin <genealogy@garygriffin.net>
2026-08-03 13:32:09 -0700

    Merge SharedPostgreSQL: prevent race on init; case insensitive like- #1008

--- SharedPostgreSQL/sharedpostgresql.gpr.py
+++ SharedPostgreSQL/sharedpostgresql.gpr.py
@@ -24,7 +24,7 @@ register(
     name=_("SharedPostgreSQL"),
     name_accell=_("Shared _PostgreSQL Database"),
     description=_("Shared PostgreSQL Database"),
-    version = '0.1.17',
+    version = '0.1.18',
     gramps_target_version="6.1",
     status=STABLE,
     fname="sharedpostgresql.py",
