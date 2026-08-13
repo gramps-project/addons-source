00031a9dd1b1045dccb63014ca0532c5f84bdb1e
GaryGriffin <genealogy@garygriffin.net>
2026-08-13 10:20:20 -0700

    Merge GrampsWebApiDb: sync media files, fix silent write-side drift#1014

--- GrampsWebApiDb/mintapikeytool.gpr.py
+++ GrampsWebApiDb/mintapikeytool.gpr.py
@@ -28,7 +28,7 @@ register(
         "for it."
     ),
     status=BETA,
-    version = '0.1.1',
+    version = '0.1.2',
     gramps_target_version="6.0",
     fname="mintapikeytool.py",
     authors=["Doug Blank"],
