c5a8a44ca738ca2e2e15cb02ec115dee62e012a4
GaryGriffin <genealogy@garygriffin.net>
2026-09-24 10:09:10 -0700

    Merge Context View- #1029
    Initial upload of ContextView addon

--- ContextView/ContextView.gpr.py
+++ ContextView/ContextView.gpr.py
@@ -35,7 +35,7 @@ register(
         "on that person; the name in bold marks the way back to the home "
         "person."
     ),
-    version = '1.0.1',
+    version = '1.0.2',
     gramps_target_version=MODULE_VERSION,
     status=STABLE,
     audience=EVERYONE,
